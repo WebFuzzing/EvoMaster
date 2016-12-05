033105c28b25c645e78f8bec337ce8fa89870aa2
arcuri82 <arcuri82@gmail.com>
2016-12-05 18:51:59 +0100

    fixed tests and Guice

--- core/src/test/kotlin/org/evomaster/core/search/RandomSearchTest.kt
+++ core/src/test/kotlin/org/evomaster/core/search/RandomSearchTest.kt
@@ -1,9 +1,11 @@
 package org.evomaster.core.search
 
 import com.google.inject.*
+import org.evomaster.core.search.algorithms.RandomAlgorithm
 import org.evomaster.core.search.onemax.OneMaxFitness
 import org.evomaster.core.search.onemax.OneMaxIndividual
 import org.evomaster.core.search.onemax.OneMaxSampler
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 
@@ -11,29 +13,30 @@ class RandomSearchTest {
 
     class RandomOneMax : AbstractModule(){
         override fun configure() {
+            bind(Key.get(object : TypeLiteral<Sampler<OneMaxIndividual>>() {}))
+                    .to(OneMaxSampler::class.java)
+
+            bind(Key.get(object : TypeLiteral<FitnessFunction<OneMaxIndividual>>() {}))
+                    .to(OneMaxFitness::class.java)
         }
 
         @Provides @Singleton
         fun randomnessProvider() : Randomness{
             return Randomness(42)
         }
-
-        @Provides
-        fun samplerProvider() : Sampler<OneMaxIndividual>{
-            return OneMaxSampler()
-        }
-
-        @Provides
-        fun fitnessFunctionProvider() : FitnessFunction<OneMaxIndividual>{
-            return OneMaxFitness()
-        }
     }
 
-    val modules = arrayOf<com.google.inject.Module>(RandomOneMax())
-    //val injector = Guice::createInjector(*modules)
+    val injector: Injector = Guice.createInjector(* arrayOf<Module>(RandomOneMax()))
 
     @Test
     fun testRandomSearch(){
 
+        val rs = injector.getInstance(Key.get(
+                object : TypeLiteral<RandomAlgorithm<OneMaxIndividual>>() {}))
+
+        val solution = rs.search(1000)
+
+        assertEquals(3.0, solution.overall.computeFitnessScore(), 0.001);
+        assertEquals(1, solution.individuals.size)
     }
 }
--- core/src/test/kotlin/org/evomaster/core/search/onemax/OneMaxSampler.kt
+++ core/src/test/kotlin/org/evomaster/core/search/onemax/OneMaxSampler.kt
@@ -7,7 +7,7 @@ class OneMaxSampler : Sampler<OneMaxIndividual>(){
 
     override fun sampleAtRandom(): OneMaxIndividual {
 
-        val n = 5
+        val n = 3
         val sampled = OneMaxIndividual(n)
         (0 until n).forEach {
             sampled.setValue(it, randomness.choose(listOf(0.0, 0.5, 1.0)))
