ea56422751d152a1fe1a7f9189927e09a03a49b0
Bogdan Marculescu <bogdan.marculescu@bth.se>
2018-11-06 14:52:04 +0100

    Initial stages:
    1. Generate random objects based on swagger definitions collected from the SUT
    
    TODO:
    - ensure it doesn't crash if the swagger definitions are null
    - recover from some faults (e.g. proxyprint's file/null problem)

---
+++ experiments/src/main/kotlin/org/evomaster/experiments/objects/param/PathParam.kt
@@ -0,0 +1,15 @@
+package org.evomaster.experiments.objects.param
+
+import org.evomaster.core.search.gene.DisruptiveGene
+
+
+class PathParam (name: String, gene: DisruptiveGene<*>) : Param(name, gene){
+
+    override fun copy(): Param {
+        return PathParam(name, gene.copy() as DisruptiveGene<*>)
+    }
+
+    fun preventMutation(){
+        (gene as DisruptiveGene<*>).probability = 0.0
+    }
+}
