929100690cf022bb0bea572cdcdc653d1ab36cbd
Bogdan Marculescu <bogdan.marculescu@bth.se>
2018-11-06 14:52:04 +0100

    Initial stages:
    1. Generate random objects based on swagger definitions collected from the SUT
    
    TODO:
    - ensure it doesn't crash if the swagger definitions are null
    - recover from some faults (e.g. proxyprint's file/null problem)

---
+++ experiments/src/main/kotlin/org/evomaster/experiments/objects/Main.kt
@@ -0,0 +1,121 @@
+package org.evomaster.experiments.objects
+
+import com.google.inject.Injector
+import com.netflix.governator.guice.LifecycleInjector
+import org.evomaster.core.BaseModule
+import org.evomaster.core.search.gene.MapGene
+import org.evomaster.core.search.gene.StringGene
+
+class Main {
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+            Main.testMapPrint()
+            Main.base()
+//            infeasible()
+        }
+
+        fun base () {
+            printHeader()
+
+            val inj = setStuffUp()
+
+            println("Injector: ${inj}")
+
+            val sampler = inj.getInstance(ObjRestSampler::class.java)
+
+            println("And the sampler ref is:... TA DAAAAA: ${sampler}")
+
+            val action = sampler.sampleAtRandom()
+
+            println("I took an action, me: ${action.debugginPrint()} ")
+            //BMR: debugginPrint uses .getName() which returns {variable} etc...
+            println("In practice it would look like: ${action.debugginPrintProcessed()} ")
+            //BMR: debugginPrintProcessed uses .toString(), and seems to return the {variable} values already processed.
+
+            val models = sampler.getModelCluster()
+
+            val available = action.seeActions()
+            //models["Organization"].fields.forEach{ t -> t.name.contains("Id"); println("${t.name} => ${t.getValueAsPrintableString()}") }
+
+            models.forEach{(k, v) -> v.fields.forEach {
+                if(it.name.equals("id", ignoreCase = true) || it.name.equals("name", ignoreCase = true)){
+                    println("Exact Match Model: $k => ${it.name} =>>> ${it.getValueAsPrintableString()}")
+                }
+                else{
+                    if (it.name.contains("id", ignoreCase = true) || it.name.contains("name", ignoreCase = true)){
+                        println("Partial Match Model: $k => ${it.name} =>>> ${it.getValueAsPrintableString()}")
+                    }
+                }
+            }}
+
+            for (ac in available){
+                var genes = ac.seeGenes()
+                for (g in genes){
+                    if (g.getVariableName().contains("Id")){
+                        println("(Id): Action ${ac.getName()}; Gene ${g.getVariableName()}")
+                    }
+                    if (g.getVariableName().contains("Name")){
+                        println("(Name): Action ${ac.getName()}; Gene ${g.getVariableName()}")
+                    }
+                }
+            }
+
+            println("Available actions: $available")
+
+            println("===============================================>")
+
+            for (i in 0..5) {
+                var randomobject = sampler.getRandomObject()
+                println("I have a pseudo-random object: ${randomobject.getValueAsPrintableString()}")
+            }
+
+
+            println("===============================================>")
+
+            for (i in 0..25) {
+                var swaggerishRandomobject = sampler.getRandomObjectSwaggerish()
+                println("I have a swaggerish-random object: ${swaggerishRandomobject.name} =>> ${swaggerishRandomobject.getValueAsPrintableString()}")
+            }
+
+
+            //var smarts = sampler.getRandomObjIndividual()
+            //println("Let's try to be smart again: ${smarts.debugginPrintProcessed()}")
+            // TODO: Smart need more work
+        }
+
+        fun printHeader() {
+            println("Let's start mucking about")
+        }
+
+        fun setStuffUp () : Injector{
+            val injector = LifecycleInjector.builder()
+                    .withModules(ObjModule(), BaseModule(arrayOf("--showProgress=false")))
+                    .build().createInjector()
+
+            return injector
+
+        }
+
+        fun testMapPrint() {
+
+            println("Quick test of the map print ")
+            val s1 = StringGene("string_1")
+            val s2 = StringGene("string_2")
+
+            println("String 1 is: ${s1.getValueAsPrintableString()}")
+            println("String 2 is: ${s2.getValueAsPrintableString()}")
+
+            var map = MapGene<StringGene>("PrintableMap", StringGene("map"), 7, mutableListOf(s1, s2))
+            var mapstring = map.getValueAsPrintableString()
+            println("PrintableMap is =>  $mapstring")
+            assert(mapstring.contains(s1.getValueAsPrintableString(), ignoreCase = true) &&
+                    mapstring.contains(s2.getValueAsPrintableString(), ignoreCase = true))
+        }
+
+
+    }
+
+}
---
+++ experiments/src/main/kotlin/org/evomaster/experiments/objects/RestPath.kt
@@ -0,0 +1,230 @@
+package org.evomaster.experiments.objects
+
+import org.evomaster.experiments.objects.param.Param
+import org.evomaster.experiments.objects.param.PathParam
+import org.evomaster.experiments.objects.param.QueryParam
+import org.evomaster.core.search.gene.OptionalGene
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.net.URI
+import java.net.URLEncoder
+
+
+class RestPath(path: String) {
+
+    private data class Token(val name: String, val isParameter: Boolean) {
+
+        override fun toString(): String {
+            if (isParameter) {
+                return "{$name}"
+            } else {
+                return name
+            }
+        }
+    }
+
+    private val tokens: List<Token>
+
+    init {
+        if (path.contains("?") || path.contains("#")) {
+            throw IllegalArgumentException("The path contains invalid characters. " +
+                    "Are you sure you didn't pass a full URI?\n$path")
+        }
+
+        tokens = path.split("/").filter { s -> !s.isBlank() }
+                .map { s ->
+                    /*
+                        TODO: technically, leading spaces are valid in a URI.
+                        Furthermore, need to check if things like /x-{id} are possible,
+                        and how Swagger handle endpoint encoding
+                     */
+                    val trimmed = s.trim()
+                    if (trimmed.startsWith("{")) {
+                        if (!trimmed.endsWith("}")) {
+                            throw IllegalArgumentException("Opening { was not matched by closing } in: $path")
+                        }
+                        Token(trimmed.substring(1, trimmed.lastIndex), true)
+                    } else {
+                        Token(trimmed, false)
+                    }
+                }
+    }
+
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(RestPath::class.java)
+    }
+
+    override fun toString(): String {
+        return "/" + tokens.map { t -> t.toString() }.joinToString("/")
+    }
+
+    /**
+     * @return the number of distinct elements in the path, ie the
+     * hierarchy level
+     */
+    fun levels() = tokens.size
+
+    /**
+     * Return ordered list of names of all the Path Parameter variables
+     * in this path
+     */
+    fun getVariableNames(): List<String> {
+        return tokens.filter { t -> t.isParameter }.map { t -> t.name }
+    }
+
+    fun hasVariablePathParameters(): Boolean {
+        return tokens.any { t -> t.isParameter }
+    }
+
+    fun isEquivalent(other: RestPath): Boolean {
+        if (this.tokens.size != other.tokens.size) {
+            return false
+        }
+        return (0 until tokens.size).none { this.tokens[it] != other.tokens[it] }
+    }
+
+    fun isAResolvedOf(other: RestPath) : Boolean {
+        if (this.tokens.size != other.tokens.size) {
+            return false
+        }
+
+        return (0 until tokens.size).none {
+            val tt =  this.tokens[it]
+            val ot = other.tokens[it]
+            tt.isParameter || (tt != ot && !ot.isParameter)
+        }
+    }
+
+    fun lastElement(): String {
+        if (tokens.isEmpty()) {
+            return "root"
+        }
+        return tokens.last().name
+    }
+
+    fun isLastElementAParameter(): Boolean {
+        if (tokens.isEmpty()) {
+            return false
+        }
+        return tokens.last().isParameter
+    }
+
+    fun isDirectChildOf(other: RestPath): Boolean {
+        if (this.tokens.size != 1 + other.tokens.size) {
+            return false
+        }
+
+        return other.isAncestorOf(this)
+    }
+
+
+    /**
+     * Prefix or same as "other"
+     */
+    fun isAncestorOf(other: RestPath): Boolean {
+        if (this.tokens.size > other.tokens.size) {
+            return false
+        }
+
+        return (0 until this.tokens.size).none { other.tokens[it] != this.tokens[it] }
+    }
+
+    /**
+     * Return a resolved path (starting with "/") based on input parameters.
+     * For example:
+     *
+     * foo/bar/{id}
+     *
+     * will be resolved into
+     *
+     * /foo/bar/5
+     *
+     * if the input params have a variable called "id" with value 5
+     */
+    fun resolve(params: List<out Param>): String {
+
+        var path = resolveOnlyPath(params)
+
+        val queries = resolveOnlyQuery(params)
+        if (queries.size > 0) {
+            path += "?" + queries.joinToString("&")
+        }
+
+        return path
+    }
+
+    private fun usableQueryParamsFunction(): (Param) -> Boolean {
+        return { p -> p is QueryParam && (p.gene !is OptionalGene || p.gene.isActive) }
+    }
+
+    fun numberOfUsableQueryParams(params: List<out Param>): Int {
+        return params.filter(usableQueryParamsFunction()).size
+    }
+
+    fun resolveOnlyQuery(params: List<out Param>): List<String> {
+
+        return params
+                .filter(usableQueryParamsFunction())
+                .map { q ->
+                    val name = encode(q.name)
+                    val gene = q.gene
+                    val value = encode(gene.getValueAsRawString())
+                    "$name=$value"
+                }
+    }
+
+    fun resolveOnlyPath(params: List<out Param>): String {
+
+        var path = StringBuffer()
+        tokens.forEach { t ->
+            var value: String
+
+            if (!t.isParameter) {
+                value = t.name
+            } else {
+                var p = params.find { p -> p is PathParam && p.name == t.name } ?:
+                        throw IllegalArgumentException("Cannot resolve path parameter '${t.name}'")
+
+                value = p.gene.getValueAsRawString()
+
+                if (value.isBlank()) {
+                    /*
+                        We should avoid having path params that are blank,
+                        as they would easily lead to useless 404/405 errors
+
+                        TODO handle this case better, eg avoid having blank in
+                        the first place
+                     */
+                    value = "1"
+                }
+            }
+
+            path.append("/$value")
+        }
+
+        /*
+           reserved characters need to be encoded
+           https://tools.ietf.org/html/rfc3986#section-2.2
+
+           why not using URI also for Query part???
+           it seems unclear how to properly build it as a single string...
+         */
+        path = StringBuffer(URI(null, null, path.toString(), null, null).rawPath)
+
+        return path.toString()
+    }
+
+
+    /**
+     * URIs query elements need to be encoded, eg space " " turns into a +,
+     * and other symbols get into the %XX hexadecimal format
+     *
+     * Encoding of query parameters can be quite tricky... good
+     * discussion is at:
+     * http://stackoverflow.com/questions/1547899/which-characters-make-a-url-invalid
+     * http://stackoverflow.com/questions/1634271/url-encoding-the-space-character-or-20
+     */
+    private fun encode(s: String): String {
+        return URLEncoder.encode(s, "UTF-8")
+    }
+}
