c30ded1ebee5a4a274221c66c07c6f8c3fc726da
Bogdan Marculescu <bogdan.marculescu@bth.se>
2018-11-07 16:41:55 +0100

    Initial stages (update):
    1. Generate random objects based on swagger definitions collected from the SUT
    
    2. Generate a random object based on a particular swagger definition, appropriate to a given endpoint. The degree to which the parameter name matches the object name gives the weight that the choice of swagger object is based on.
    
    TODO:
    - ensure it doesn't crash if the swagger definitions are null
    - recover from some faults (e.g. proxyprint's file/null problem)
    - some refactoring is sorely needed.
    - complete the building of Rest calls based on the created objects.

--- experiments/src/main/kotlin/org/evomaster/experiments/objects/Main.kt
+++ experiments/src/main/kotlin/org/evomaster/experiments/objects/Main.kt
@@ -4,7 +4,10 @@ import com.google.inject.Injector
 import com.netflix.governator.guice.LifecycleInjector
 import org.evomaster.core.BaseModule
 import org.evomaster.core.search.gene.MapGene
+import org.evomaster.core.search.gene.ObjectGene
 import org.evomaster.core.search.gene.StringGene
+import java.lang.Integer.max
+import java.util.*
 
 class Main {
     companion object {
@@ -20,36 +23,25 @@ class Main {
         private fun base () {
             printHeader()
 
-            //println("Injector: ${inj}")
-
             val sampler = setStuffUp().getInstance(ObjRestSampler::class.java)
 
-            //println("And the sampler ref is:... TA DAAAAA: ${sampler}")
-
-            //val action = sampler.sampleAtRandom()
-
-            //println("I took an action, me: ${action.debugginPrint()} ")
             //BMR: debugginPrint uses .getName() which returns {variable} etc...
-            //println("In practice it would look like: ${action.debugginPrintProcessed()} ")
             //BMR: debugginPrintProcessed uses .toString(), and seems to return the {variable} values already processed.
-            //TODO figure out why the model overview is printed 3 times
 
             val models = sampler.getModelCluster()
 
             val available = sampler.seeAvailableActions()
-            //models["Organization"].fields.forEach{ t -> t.name.contains("Id"); println("${t.name} => ${t.getValueAsPrintableString()}") }
 
-            models.forEach{(k, v) -> v.fields.forEach {
-                if(it.name.equals("id", ignoreCase = true) || it.name.equals("name", ignoreCase = true)){
-                    println("Exact Match Model: $k => ${it.name} =>>> ${it.getValueAsPrintableString()}")
-                }
-                else{
-                    if (it.name.contains("id", ignoreCase = true) || it.name.contains("name", ignoreCase = true)){
-                        println("Partial Match Model: $k => ${it.name} =>>> ${it.getValueAsPrintableString()}")
-                    }
-                }
-            }}
+            println("Overview of Models:")
+            models.forEach{
+                println("${it.key} => ${it.value.getValueAsPrintableString()}")
+            }
 
+
+            //nameMatchExperiments(models)
+
+
+            /*
             for (ac in available){
                 var genes = ac.seeGenes()
                 for (g in genes){
@@ -60,24 +52,36 @@ class Main {
                         println("(Name): Action ${ac.getName()}; Gene ${g.getVariableName()}")
                     }
                 }
-            }
-
-            println("Available actions: $available")
+            }*/
 
             println("===============================================>")
 
-            for (i in 0..5) {
-                var randomobject = sampler.getRandomObject()
-                println("I have a pseudo-random object: ${randomobject.getValueAsPrintableString()}")
+            println("Available actions:")
+            available.forEach{
+                println("${it.getName()}")
+                it.seeGenes().forEach {
+                    println(" -> ${it.getVariableName()}")
+                    val likely = likelyhoods(it.getVariableName(), models)
+                    println("Likelyhoods: ${likely}")
+                    (0..10).forEach{k ->
+                        val sel = pickWithProbability(likely)
+                        val selObj = sampler.getObjectFromTemplate(sel)
+                        println("Selected -> ${selObj.name} => ${selObj.getValueAsPrintableString()}")
+                        if(!selObj.fields.isEmpty()){
+                            println("Picked field -> ${selObj.fields.random().getValueAsPrintableString()}")
+                        }
+                    }
+                }
+                println(" --- ")
             }
-
 
             println("===============================================>")
 
-            for (i in 0..25) {
-                var swaggerishRandomobject = sampler.getRandomObjectSwaggerish()
-                println("I have a swaggerish-random object: ${swaggerishRandomobject.name} =>> ${swaggerishRandomobject.getValueAsPrintableString()}")
-            }
+            //randomObjectExperiments(sampler, 10)
+
+            println("===============================================>")
+
+            //swaggerishObjectExperiments(sampler, 50)
 
 
             //var smarts = sampler.getRandomObjIndividual()
@@ -125,6 +129,79 @@ class Main {
                     mapstring.contains(s2.getValueAsPrintableString(), ignoreCase = true))
         }
 
+        fun likelyhoods(parameter: String, candidates: MutableMap<String, ObjectGene>): MutableMap<String, Float>{
+
+            var result = mutableMapOf<String, Float>()
+            var sum : Float = 0.toFloat()
+            candidates.forEach { k, v ->
+                val temp = lcs(parameter, k).length.toFloat()/max(parameter.length, k.length).toFloat()
+                result[k] = temp
+                sum += temp
+            }
+            result.forEach { k, u ->
+                result[k] = u/sum
+            }
+
+            return result
+        }
+
+        fun lcs(a: String, b: String): String {
+            if (a.length > b.length) return lcs(b, a)
+            var res = ""
+            for (ai in 0 until a.length) {
+                for (len in a.length - ai downTo 1) {
+                    for (bi in 0 until b.length - len) {
+                        if (a.regionMatches(ai, b, bi,len) && len > res.length) {
+                            res = a.substring(ai, ai + len)
+                        }
+                    }
+                }
+            }
+            return res
+        }
+
+        fun pickWithProbability(map: MutableMap<String, Float>): String{
+            val randFl = Random().nextFloat()
+            var temp = 0.toFloat()
+            var found = map.keys.first()
+
+            for((k, v) in map){
+                if(randFl <= (v + temp)){
+                    found = k
+                    break
+                }
+                temp += v
+            }
+            return found
+        }
+
+        fun nameMatchExperiments(models: MutableMap<String, ObjectGene>){
+            println("Name match experiments:")
+            models.forEach{(k, v) -> v.fields.forEach {
+                if(it.name.equals("id", ignoreCase = true) || it.name.equals("name", ignoreCase = true)){
+                    println("Exact Match Model: $k => ${it.name} =>>> ${it.getValueAsPrintableString()}")
+                }
+                else{
+                    if (it.name.contains("id", ignoreCase = true) || it.name.contains("name", ignoreCase = true)){
+                        println("Partial Match Model: $k => ${it.name} =>>> ${it.getValueAsPrintableString()}")
+                    }
+                }
+            }}
+        }
+
+        fun randomObjectExperiments(sampler: ObjRestSampler, numberOfObjects: Int = 10){
+            for (i in 0..numberOfObjects) {
+                var randomobject = sampler.getRandomObject()
+                println("Pseudo-random object: ${randomobject.getValueAsPrintableString()}")
+            }
+        }
+
+        fun swaggerishObjectExperiments(sampler: ObjRestSampler, numberOfObjects: Int = 25){
+            for (i in 0..numberOfObjects) {
+                var swaggerishRandomobject = sampler.getRandomSwaggerObject()
+                println("Swagger object: ${swaggerishRandomobject.name} =>> ${swaggerishRandomobject.getValueAsPrintableString()}")
+            }
+        }
 
     }
 
