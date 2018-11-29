8dc60c645d7470db83004bd2279f3493a742407b
Juan Pablo Galeotti <jgaleotti@dc.uba.ar>
2018-11-29 10:31:32 -0300

    Description of field primaryKeySequence in TableDto.

--- client-java/controller-api/src/main/java/org/evomaster/client/java/controller/api/dto/database/schema/TableDto.java
+++ client-java/controller-api/src/main/java/org/evomaster/client/java/controller/api/dto/database/schema/TableDto.java
@@ -21,5 +21,15 @@ public class TableDto {
     public List<ForeignKeyDto> foreignKeys = new ArrayList<>();
 
 
+    /**
+     * Order in which the columns in the primary keys are listed
+     * in the schema.
+     *
+     * For example, the primary key (activity_1_id, activity_2_id) results
+     * in the list "activity_1_id", "activity_2_id".
+     *
+     * If the primary key has only one column, this sequence has only one
+     * element.
+     */
     public List<String> primaryKeySequence = new ArrayList<>();
 }
