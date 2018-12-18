19ad47094aeb1b9e698eef6eed897c9779fbf1a1
Bogdan Marculescu <bogdan.marculescu@bth.se>
2018-12-18 17:13:56 +0100

    WIP: major refactoring incoming.
    Helper class (instead of current usedObj map) will be used to:
    - store and return objects
    - store and return links between action/gene -> object/gene
    - ensure cohesion
    - propagate changes from actions to objects and back.

--- experiments/src/main/kotlin/org/evomaster/experiments/objects/ObjIndividual.kt
+++ experiments/src/main/kotlin/org/evomaster/experiments/objects/ObjIndividual.kt
@@ -1,8 +1,6 @@
 package org.evomaster.experiments.objects
 
-import org.evomaster.core.Lazy
 import org.evomaster.core.database.DbAction
-import org.evomaster.core.database.DbActionUtils
 import org.evomaster.core.search.Action
 import org.evomaster.core.search.Individual
 import org.evomaster.core.search.gene.Gene
@@ -13,21 +11,26 @@ import org.evomaster.core.problem.rest.RestAction
 import org.evomaster.core.problem.rest.SampleType
 
 
-class ObjIndividual(val actions: MutableList<RestAction>,
+class ObjIndividual(val callActions: MutableList<RestAction>,
                     val sampleType: SampleType,
-                    var usedObject: UsedObj,
+                    var usedObj : MutableMap<Pair<String, String> , ObjectGene> = mutableMapOf(),
                     val dbInitialization: MutableList<DbAction> = mutableListOf()
+                    //var usedObj : MutableList<ObjectGene> = mutableListOf()
+                    //var usedObj : MutableMap<Pair<ObjRestCallAction, String> , ObjectGene> = mutableMapOf()
 
 ) : Individual() {
 
     override fun copy(): Individual {
         return ObjIndividual(
-                actions.map { a -> a.copy() as RestAction } as MutableList<RestAction>,
+                callActions.map { a -> a.copy() as RestAction } as MutableList<RestAction>,
                 sampleType,
-                usedObject.copy(),
+                usedObj.filterKeys { it -> callActions.any{a -> a.toString().contains(it.first)} }.toMutableMap(),
                 dbInitialization.map { d -> d.copy() as DbAction } as MutableList<DbAction>
-                //TODO: BMR rename uo
-                // BMR : this folds into RestIndividual
+                //usedObj.map { (k, u) ->
+                //    usedObj[Pair((k.first.copy() as ObjRestCallAction), (k.second.copy() as Gene))] = u.copy() as ObjectGene} as MutableMap<Pair<ObjRestCallAction, Gene> , ObjectGene>
+                //usedObj.map { u -> u.copy() as ObjectGene} as MutableMap<Pair<ObjRestCallAction, Gene> , ObjectGene>
+                //usedObj = mutableMapOf()
+
         )
     }
 
@@ -40,10 +43,14 @@ class ObjIndividual(val actions: MutableList<RestAction>,
     override fun seeGenes(filter: GeneFilter): List<out Gene> {
 
         return when(filter){
+            /*GeneFilter.ALL ->  dbInitialization.flatMap(DbAction::seeGenes)
+                    .plus(callActions.flatMap(RestAction::seeGenes))*/
             GeneFilter.ALL ->  dbInitialization.flatMap(DbAction::seeGenes)
-                    .plus(usedObject.usedObjects())
+                    .plus(usedObj.values.flatMap(ObjectGene::flatView))
 
-            GeneFilter.NO_SQL -> usedObject.usedObjects()
+
+            //GeneFilter.NO_SQL -> callActions.flatMap(RestAction::seeGenes)
+            GeneFilter.NO_SQL -> usedObj.values.flatMap(ObjectGene::flatView)
             GeneFilter.ONLY_SQL -> dbInitialization.flatMap(DbAction::seeGenes)
         }
     }
@@ -54,14 +61,14 @@ class ObjIndividual(val actions: MutableList<RestAction>,
         need to think about it
      */
 
-    override fun size() = actions.size
+    override fun size() = callActions.size
 
     override fun seeActions(): List<out Action> {
-        return actions
+        return callActions
     }
 
     override fun verifyInitializationActions(): Boolean {
-        return DbActionUtils.verifyActions(dbInitialization.filterIsInstance<DbAction>())
+        return DbAction.verifyActions(dbInitialization.filterIsInstance<DbAction>())
     }
 
     override fun repairInitializationActions(randomness: Randomness) {
@@ -72,21 +79,48 @@ class ObjIndividual(val actions: MutableList<RestAction>,
         GeneUtils.repairGenes(this.seeGenes(Individual.GeneFilter.ONLY_SQL).flatMap { it.flatView() })
 
         /**
-         * Now repair database constraints (primary keys, foreign keys, unique fields, etc.)
+         * Now repair databse constraints (primary keys, foreign keys, unique fields, etc.)
          */
         if (!verifyInitializationActions()) {
-            DbActionUtils.repairBrokenDbActionsList(dbInitialization, randomness)
-            Lazy.assert{verifyInitializationActions()}
+            DbAction.repairBrokenDbActionsList(dbInitialization, randomness)
+            assert(verifyInitializationActions())
         }
+
+        //TODO: is this a place for the coherence check between usedObj and actions?
+
     }
 
     override fun seeInitializingActions() : List<Action>{
         return dbInitialization
     }
 
+    fun coherenceCheck() : Boolean{
+
+
+        callActions.forEach{ action ->
+            action.seeGenes().forEach{ gene ->
+                usedObj.get(Pair(action.toString(), gene.getVariableName()))
+                //the object has been mutated (?) so the next step is to update the action/gene combination to match
+                // next step: pick which of the object's optional genes to use? (substring?)
+                // same as selectedGene from the ObjRestSampler. Might make sense to just store that as well?
+            }
+        }
+
+        /*
+        TODO: Refactor (BMR)
+        the usedObj becomes a separate class/object, attached to each individual
+        it stores and returns   - the objects used (ensuring coherence and non-repetition)
+                                - links between objects and actions/genes and objects/genes used
+                                - ensures coherence (propagate object mutations forward and action results backward)
+         */
+
+
+        return true
+    }
+
     fun debugginPrint() : String{
         var rez = ""
-        for(r in this.actions){
+        for(r in this.callActions){
             rez += r.getName() + "\n"
             //rez += r.seeGenes() + "\n"
         }
@@ -94,7 +128,8 @@ class ObjIndividual(val actions: MutableList<RestAction>,
     }
     fun debugginPrintProcessed() : String{
         var rez = ""
-        for(r in this.actions){
+        for(r in this.callActions){
+            //rez += r.getName() + "\n"
             rez += r.toString() + "\n"
         }
         return rez
