17226a087e4fa3d8b49e7afbde94d1c087ed94e0
Bogdan Marculescu <bogdan.marculescu@bth.se>
2019-01-15 12:25:43 +0100

    Refactoring done. UsedObj is a new class, meant to store the objects used by a particular individual, as well as the connection between individual genes and the fields of those objects they use.
    Notes:
    - adHocIndividuals (due to their separate generation process) share a single pool of used objects. (I intend to fix this at some point)
    - The gene mutation has been slighting changes as follows:
    --- the individual now returns the objects (as flat map) for mutation
    --- the objects are mutated
    --- the changes are propagated back to the action of the individual

---
+++ experiments/src/main/kotlin/org/evomaster/experiments/objects/UsedObj.kt
@@ -0,0 +1,62 @@
+package org.evomaster.experiments.objects
+
+import org.evomaster.core.search.gene.DisruptiveGene
+import org.evomaster.core.search.gene.Gene
+import org.evomaster.core.search.gene.ObjectGene
+import org.evomaster.core.search.gene.OptionalGene
+
+class UsedObj(var mapping:MutableMap<Pair<ObjRestCallAction, Gene> , ObjectGene> = mutableMapOf(),
+              var selection:MutableMap<Pair<ObjRestCallAction, Gene>, Pair<String, String>> = mutableMapOf()){
+
+    fun copy(): UsedObj{
+        val mapcopy: MutableMap<Pair<ObjRestCallAction, Gene> , ObjectGene> = mutableMapOf()
+        val selcopy: MutableMap<Pair<ObjRestCallAction, Gene>, Pair<String, String>> = mutableMapOf()
+        mapping.forEach { k, v ->
+            mapcopy[Pair((k.first.copy() as ObjRestCallAction), (k.second.copy() as Gene))] = (v.copy() as ObjectGene)
+        }
+        selection.forEach{ k, v ->
+            val copykey = Pair((k.first.copy() as ObjRestCallAction), (k.second.copy() as Gene))
+            val copyval = Pair(v.first, v.second)
+            selcopy[copykey] = copyval
+        }
+        return UsedObj(mapcopy, selcopy)
+    }
+
+    fun usedObjects(): List<Gene>{
+        //return all objects for mutation and randomization purposes
+        return mapping.values.toMutableSet().flatMap(ObjectGene::flatView)
+    }
+
+    fun coherenceCheck(){
+        mapping.forEach { key, value ->
+            val sel = selection[key] as Pair<String, String>
+            val v = value.fields.filter { g -> (g as OptionalGene).name === sel.second }?.single() as OptionalGene
+            when (key.second::class) {
+                DisruptiveGene::class -> (key.second as DisruptiveGene<*>).gene.copyValueFrom(v.gene)
+                OptionalGene::class ->  (key.second as OptionalGene).gene.copyValueFrom(v.gene)
+            }
+        }
+    }
+
+    fun assign(key:Pair<ObjRestCallAction, Gene>, value:ObjectGene, selectedField:Pair<String, String>){
+        mapping[key] = value
+        selection[key] = selectedField
+    }
+
+
+    fun displayInline() : String{
+        //display inline (mostly for debugging)
+        var res = "{{"
+        mapping.forEach { k, v ->
+            res += "[${k.first.toString()}, ${k.second.getVariableName()}]=${v.getVariableName()}"
+        }
+        res+="}}"
+        return res
+    }
+
+    fun clearLists(){
+        mapping.clear()
+        selection.clear()
+    }
+
+}
