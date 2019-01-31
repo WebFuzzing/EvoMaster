d22a482a063282739fc6042f854f0cd9d8f753bd
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2019-01-31 16:16:00 +0100

    Runs with all systems (except OCVN, which is a special case). Added some exception handling to avoid problems when trying to copy mismatched objects. If appropriate objects cannot be found, or there are problems copying values (e.g. due to mismatched objects), the system defaults to regular (i.e. not object-creating) behaviour.
    Still having problems with object coherence.

--- experiments/src/main/kotlin/org/evomaster/experiments/objects/UsedObj.kt
+++ experiments/src/main/kotlin/org/evomaster/experiments/objects/UsedObj.kt
@@ -1,25 +1,34 @@
 package org.evomaster.experiments.objects
 
-import org.evomaster.core.search.gene.DisruptiveGene
-import org.evomaster.core.search.gene.Gene
-import org.evomaster.core.search.gene.ObjectGene
-import org.evomaster.core.search.gene.OptionalGene
+import org.evomaster.core.search.gene.*
+import org.evomaster.core.problem.rest.RestAction
 
-class UsedObj(var mapping:MutableMap<Pair<ObjRestCallAction, Gene> , ObjectGene> = mutableMapOf(),
-              var selection:MutableMap<Pair<ObjRestCallAction, Gene>, Pair<String, String>> = mutableMapOf(),
-              var select_body:MutableMap<ObjRestCallAction, ObjectGene> = mutableMapOf()){
+class UsedObj(val mapping:MutableMap<Pair<ObjRestCallAction, Gene> , Gene> = mutableMapOf(),
+              val selection:MutableMap<Pair<ObjRestCallAction, Gene>, Pair<String, String>> = mutableMapOf(),
+              val select_body:MutableMap<ObjRestCallAction, Gene> = mutableMapOf()){
 
     fun copy(): UsedObj{
-        val mapcopy: MutableMap<Pair<ObjRestCallAction, Gene> , ObjectGene> = mutableMapOf()
+        val mapcopy: MutableMap<Pair<ObjRestCallAction, Gene> , Gene> = mutableMapOf()
         val selcopy: MutableMap<Pair<ObjRestCallAction, Gene>, Pair<String, String>> = mutableMapOf()
-        val bodycopy: MutableMap<ObjRestCallAction, ObjectGene> = mutableMapOf()
+        val bodycopy: MutableMap<ObjRestCallAction, Gene> = mutableMapOf()
+        mapping.forEach { k, v -> mapcopy[k] = v }
+        selection.forEach{ k, v ->  selcopy[k] = v }
+        select_body.forEach { k, v -> bodycopy[k] = v }
+
+        return UsedObj(mapcopy, selcopy, bodycopy)
+    }
+    /*
+    fun copy(): UsedObj{
+        val mapcopy: MutableMap<Pair<ObjRestCallAction, Gene> , Gene> = mutableMapOf()
+        val selcopy: MutableMap<Pair<ObjRestCallAction, Gene>, Pair<String, String>> = mutableMapOf()
+        val bodycopy: MutableMap<ObjRestCallAction, Gene> = mutableMapOf()
         mapping.forEach { k, v ->
-            mapcopy[Pair(k.first, k.second)] = v
+            mapcopy[k] = v
         }
         selection.forEach{ k, v ->
-            val copykey = Pair(k.first, k.second)
-            val copyval = Pair(v.first, v.second)
-            selcopy[copykey] = copyval
+            //val copykey = Pair(k.first, k.second)
+            //val copyval = Pair(v.first, v.second)
+            selcopy[k] = v
         }
         select_body.forEach { k, v ->
             bodycopy[k] = v
@@ -27,29 +36,47 @@ class UsedObj(var mapping:MutableMap<Pair<ObjRestCallAction, Gene> , ObjectGene>
 
         return UsedObj(mapcopy, selcopy, bodycopy)
     }
-
+    */
     fun usedObjects(): List<Gene>{
         //return all objects for mutation and randomization purposes
-        return mapping.values.flatMap{ it -> it.flatView()}
+        return mapping.values.flatMap{ it.flatView() }
     }
 
     fun coherenceCheck(){
         mapping.forEach { key, value ->
-            val sel = selection[key] as Pair<String, String>
-            val v = value.fields.filter { g -> (g as OptionalGene).name === sel.second }?.single() as OptionalGene
-            when (key.second::class) {
-                DisruptiveGene::class -> (key.second as DisruptiveGene<*>).gene.copyValueFrom(v.gene)
-                OptionalGene::class ->  (key.second as OptionalGene).gene.copyValueFrom(v.gene)
+            when (value::class){
+                ObjectGene::class -> {
+                    val sel = selection[key] as Pair<String, String>
+                    var v: OptionalGene
+                    if (sel.second === ""){
+                        v = OptionalGene("temp", (value as ObjectGene) )
+                    }
+                    else {
+                        v = (value as ObjectGene).fields.filter { g -> (g as OptionalGene).name === sel.second }?.single() as OptionalGene
+                    }
+                    when (key.second::class) {
+                        DisruptiveGene::class -> (key.second as DisruptiveGene<*>).gene.copyValueFrom(v.gene)
+                        OptionalGene::class ->  (key.second as OptionalGene).gene.copyValueFrom(v.gene)
+                        ObjectGene::class -> (key.second as ObjectGene).copyValueFrom(v.gene)
+                    }
+                }
             }
+
         }
     }
 
-    fun assign(key:Pair<ObjRestCallAction, Gene>, value:ObjectGene, selectedField:Pair<String, String>){
+    fun pruneObjects(actions: MutableList<RestAction>){
+        mapping.forEach { if(!actions.contains(it.key.first)) {mapping.remove(it.key)} }
+        selection.forEach { if(!actions.contains(it.key.first)) {selection.remove(it.key)} }
+        select_body.forEach { if(!actions.contains(it.key)) {select_body.remove(it.key)} }
+    }
+
+    fun assign(key:Pair<ObjRestCallAction, Gene>, value:Gene, selectedField:Pair<String, String>){
         mapping[key] = value
         selection[key] = selectedField
     }
 
-    fun selectbody(action:ObjRestCallAction, obj:ObjectGene){
+    fun selectbody(action:ObjRestCallAction, obj:Gene){
         select_body[action] = obj
     }
 
@@ -68,6 +95,14 @@ class UsedObj(var mapping:MutableMap<Pair<ObjRestCallAction, Gene> , ObjectGene>
         mapping.clear()
         selection.clear()
         select_body.clear()
+    }
+
+    fun getRelevantGene(action: ObjRestCallAction, gene: Gene): Gene{
+        val selectedField = selection[Pair(action, gene)]
+        val returningGene = (mapping[Pair(action, gene)] as ObjectGene).fields
+                .filter { f -> f.name === selectedField?.second }
+                .first()
+        return returningGene
     }
 
 }
