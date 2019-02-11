f354734d0ca02471f899a63b90541343c3499700
Bogdan Marculescu <bogdan.marculescu@bth.se>
2019-02-11 16:27:13 +0100

    Object coherence problem appears fixed.

--- experiments/src/main/kotlin/org/evomaster/experiments/objects/UsedObj.kt
+++ experiments/src/main/kotlin/org/evomaster/experiments/objects/UsedObj.kt
@@ -3,16 +3,17 @@ package org.evomaster.experiments.objects
 import org.evomaster.core.search.gene.*
 import org.evomaster.core.problem.rest.RestAction
 
-class UsedObj(val mapping:MutableMap<Pair<ObjRestCallAction, Gene> , Gene> = mutableMapOf(),
-              val selection:MutableMap<Pair<ObjRestCallAction, Gene>, Pair<String, String>> = mutableMapOf(),
-              val select_body:MutableMap<ObjRestCallAction, Gene> = mutableMapOf()){
+class UsedObj(//val mapping:MutableMap<Pair<ObjRestCallAction, Gene> , Gene> = mutableMapOf(),
+              val mapping:MutableMap<Pair<String, String> , Gene> = mutableMapOf(),
+              val selection:MutableMap<Pair<String, String>, Pair<String, String>> = mutableMapOf(),
+              val select_body:MutableMap<String, Gene> = mutableMapOf()){
 
     fun copy(): UsedObj{
-        val mapcopy: MutableMap<Pair<ObjRestCallAction, Gene> , Gene> = mutableMapOf()
-        val selcopy: MutableMap<Pair<ObjRestCallAction, Gene>, Pair<String, String>> = mutableMapOf()
-        val bodycopy: MutableMap<ObjRestCallAction, Gene> = mutableMapOf()
-        mapping.forEach { k, v -> mapcopy[k] = v }
-        selection.forEach{ k, v ->  selcopy[k] = v }
+        val mapcopy: MutableMap<Pair<String, String> , Gene> = mutableMapOf()
+        val selcopy: MutableMap<Pair<String, String>, Pair<String, String>> = mutableMapOf()
+        val bodycopy: MutableMap<String, Gene> = mutableMapOf()
+        mapping.forEach { k, v -> mapcopy[k] = v.copy() }
+        selection.forEach{ k, v ->  selcopy[k] = v.copy() }
         select_body.forEach { k, v -> bodycopy[k] = v }
 
         return UsedObj(mapcopy, selcopy, bodycopy)
@@ -66,18 +67,40 @@ class UsedObj(val mapping:MutableMap<Pair<ObjRestCallAction, Gene> , Gene> = mut
     }
 
     fun pruneObjects(actions: MutableList<RestAction>){
-        mapping.forEach { if(!actions.contains(it.key.first)) {mapping.remove(it.key)} }
-        selection.forEach { if(!actions.contains(it.key.first)) {selection.remove(it.key)} }
-        select_body.forEach { if(!actions.contains(it.key)) {select_body.remove(it.key)} }
+        //Fix this if it is ever used again. Otherwise, scrap it.
+        mapping.forEach { if(!actions.any{action -> (action as ObjRestCallAction).resolvedPath().contains(it.key.first)}) {mapping.remove(it.key)}   }
+        selection.forEach {  if(!actions.any{action -> (action as ObjRestCallAction).resolvedPath().contains(it.key.first)})  {selection.remove(it.key)} }
+        select_body.forEach {  if(!actions.any{action -> (action as ObjRestCallAction).resolvedPath().contains(it.key)})  {select_body.remove(it.key)} }
     }
+
+    /*
+
+    Leftover from mapping:MutableMap<Pair<ObjRestCallAction, Gene> , Gene>
 
     fun assign(key:Pair<ObjRestCallAction, Gene>, value:Gene, selectedField:Pair<String, String>){
         mapping[key] = value
         selection[key] = selectedField
     }
 
+    fun pruneObjects(actions: MutableList<RestAction>){
+        mapping.forEach { if(!actions.contains(it.key.first)) {mapping.remove(it.key)} }
+        selection.forEach { if(!actions.contains(it.key.first)) {selection.remove(it.key)} }
+        select_body.forEach { if(!actions.contains(it.key)) {select_body.remove(it.key)} }
+    }
+    fun assign(key:Pair<String, String>, value:Gene, selectedField:Pair<String, String>){
+        mapping[key] = value
+        selection[key] = selectedField
+    }
+
+    */
+
+    fun assign(key:Pair<ObjRestCallAction, Gene>, value:Gene, selectedField:Pair<String, String>){
+        mapping[Pair(key.first.getName(), key.second.getVariableName())] = value
+        selection[Pair(key.first.getName(), key.second.getVariableName())] = selectedField
+    }
+
     fun selectbody(action:ObjRestCallAction, obj:Gene){
-        select_body[action] = obj
+        select_body[action.getName()] = obj
     }
 
 
@@ -85,7 +108,7 @@ class UsedObj(val mapping:MutableMap<Pair<ObjRestCallAction, Gene> , Gene> = mut
         //display inline (mostly for debugging)
         var res = "{{"
         mapping.forEach { k, v ->
-            res += "[${k.first.toString()}, ${k.second.getVariableName()}]=${v.getVariableName()}"
+            res += "[${k.first}, ${k.second}]=${v.getVariableName()}"
         }
         res+="}}"
         return res
@@ -98,8 +121,12 @@ class UsedObj(val mapping:MutableMap<Pair<ObjRestCallAction, Gene> , Gene> = mut
     }
 
     fun getRelevantGene(action: ObjRestCallAction, gene: Gene): Gene{
-        val selectedField = selection[Pair(action, gene)]
-        val returningGene = (mapping[Pair(action, gene)] as ObjectGene).fields
+        val selectedField = selection[Pair(action.getName(), gene.getVariableName())]
+        //TODO: this does not work this way. Perhaps compare by resolvedPath
+        // action (when mutated) is done by copy. So action and the keys stored here will DEFINITELY not be the same object
+        // can I key this by action.resolvePath()?
+
+        val returningGene = (mapping[Pair(action.getName(), gene.getVariableName())] as ObjectGene).fields
                 .filter { f -> f.name === selectedField?.second }
                 .first()
         return returningGene
