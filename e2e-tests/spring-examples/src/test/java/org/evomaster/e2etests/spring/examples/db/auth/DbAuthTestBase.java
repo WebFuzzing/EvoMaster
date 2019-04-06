497c3ced1d1a8273f9533321c01f979e04a13d53
arcuri82 <arcuri82@gmail.com>
2019-04-06 14:34:06 +0200

    adding JaCoCo coverage

--- e2e-tests/spring-examples/src/test/java/org/evomaster/e2etests/spring/examples/db/auth/DbAuthTestBase.java
+++ e2e-tests/spring-examples/src/test/java/org/evomaster/e2etests/spring/examples/db/auth/DbAuthTestBase.java
@@ -4,13 +4,19 @@ import com.foo.rest.examples.spring.db.auth.DbAuthController;
 import org.evomaster.e2etests.spring.examples.SpringTestBase;
 import org.junit.jupiter.api.BeforeAll;
 
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+
 public class DbAuthTestBase extends SpringTestBase {
 
 
     @BeforeAll
     public static void initClass() throws Exception {
 
-        SpringTestBase.initClass(new DbAuthController());
+        assertTimeoutPreemptively(Duration.ofMinutes(2), () -> {
+            SpringTestBase.initClass(new DbAuthController());
+        });
     }
 
 }
