cc566df55d506baf40b3b83262516a75cbaf87fc
arcuri82 <arcuri82@gmail.com>
2019-04-06 17:36:10 +0200

    more on debugging CircleCI issue

--- e2e-tests/spring-examples/src/test/java/org/evomaster/e2etests/spring/examples/db/auth/DbAuthManualTest.java
+++ e2e-tests/spring-examples/src/test/java/org/evomaster/e2etests/spring/examples/db/auth/DbAuthManualTest.java
@@ -4,73 +4,94 @@ import io.restassured.http.ContentType;
 import org.evomaster.client.java.controller.api.dto.database.operations.InsertionDto;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.evomaster.client.java.controller.db.dsl.SqlDsl.sql;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
 public class DbAuthManualTest extends DbAuthTestBase{
+
+    /**
+     * TODO remove
+     *  assertTimeoutPreemptively(Duration.ofMinutes(2), () -> {
+     * once JUnit will support global timeouts
+     */
 
 
     @Test
     public void testGetUsers() {
 
 
-        String url = baseUrlOfSut + "/api/db/auth/users";
+        assertTimeoutPreemptively(Duration.ofMinutes(2), () -> {
+
+            String url = baseUrlOfSut + "/api/db/auth/users";
 
 
-        given().accept(ContentType.JSON)
-                .get(url)
-                .then()
-                .statusCode(200)
-                .body("size()", equalTo(1))
-                .body("[0].userId", equalTo("foo"))
-                .body("[0].password", equalTo("123"));
+            given().accept(ContentType.JSON)
+                    .get(url)
+                    .then()
+                    .statusCode(200)
+                    .body("size()", equalTo(1))
+                    .body("[0].userId", equalTo("foo"))
+                    .body("[0].password", equalTo("123"));
+        });
     }
+
 
     @Test
     public void testUnauthorized(){
 
-        String url = baseUrlOfSut + "/api/db/auth/projects";
+        assertTimeoutPreemptively(Duration.ofMinutes(2), () -> {
 
-        given().accept(ContentType.JSON)
-                .get(url)
-                .then()
-                .statusCode(401);
+            String url = baseUrlOfSut + "/api/db/auth/projects";
+
+            given().accept(ContentType.JSON)
+                    .get(url)
+                    .then()
+                    .statusCode(401);
+        });
     }
 
 
     @Test
     public void testAuthorizedButNoData(){
 
-        String url = baseUrlOfSut + "/api/db/auth/projects";
+        assertTimeoutPreemptively(Duration.ofMinutes(2), () -> {
 
-        given().accept(ContentType.JSON)
-                .auth().basic("foo","123")
-                .get(url)
-                .then()
-                .statusCode(400);
+            String url = baseUrlOfSut + "/api/db/auth/projects";
+
+            given().accept(ContentType.JSON)
+                    .auth().basic("foo", "123")
+                    .get(url)
+                    .then()
+                    .statusCode(400);
+        });
     }
 
 
     @Test
     public void testAuthorizedWithData(){
 
-        List<InsertionDto> dtos = sql()
-                .insertInto("Auth_Project_Entity")
-                .d("name", "\"whatever\"")
-                .d("owner_user_id ", "\"foo\"")
-                .dtos();
+        assertTimeoutPreemptively(Duration.ofMinutes(2), () -> {
 
-        controller.execInsertionsIntoDatabase(dtos);
+            List<InsertionDto> dtos = sql()
+                    .insertInto("Auth_Project_Entity")
+                    .d("name", "\"whatever\"")
+                    .d("owner_user_id ", "\"foo\"")
+                    .dtos();
 
-        String url = baseUrlOfSut + "/api/db/auth/projects";
+            controller.execInsertionsIntoDatabase(dtos);
 
-        given().accept(ContentType.JSON)
-                .auth().basic("foo","123")
-                .get(url)
-                .then()
-                .statusCode(200);
+            String url = baseUrlOfSut + "/api/db/auth/projects";
+
+            given().accept(ContentType.JSON)
+                    .auth().basic("foo", "123")
+                    .get(url)
+                    .then()
+                    .statusCode(200);
+        });
     }
 }
