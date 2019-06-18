5e97c2558584ba0ae97ae026fb8af4f34d867d24
Juan Pablo Galeotti <jgaleotti@dc.uba.ar>
2019-06-18 08:48:14 -0300

    Implementation of RegexGene.getValueAsRawString() and Regex.getValueAsPrintableString()

--- core/src/main/kotlin/org/evomaster/core/search/gene/regex/RegexGene.kt
+++ core/src/main/kotlin/org/evomaster/core/search/gene/regex/RegexGene.kt
@@ -3,7 +3,6 @@ package org.evomaster.core.search.gene.regex
 import org.evomaster.core.output.OutputFormat
 import org.evomaster.core.search.gene.Gene
 import org.evomaster.core.search.service.Randomness
-import java.lang.IllegalArgumentException
 
 /**
  * A gene representing a regular expression (regex).
@@ -21,9 +20,22 @@ class RegexGene(
         disjunctions.randomize(randomness, forceNewValue, allGenes)
     }
 
-    override fun getValueAsPrintableString(previousGenes: List<Gene>, mode: String?, targetFormat: OutputFormat?): String {
-        return disjunctions.getValueAsPrintableString(previousGenes, mode, targetFormat)
+    override fun getValueAsRawString(): String {
+        return disjunctions.getValueAsPrintableString(targetFormat = null)
     }
+
+    override fun getValueAsPrintableString(previousGenes: List<Gene>, mode: String?, targetFormat: OutputFormat?): String {
+        val rawValue = getValueAsRawString()
+        when {
+            (targetFormat == null) -> return "\"$rawValue\""
+            targetFormat.isKotlin() -> return "\"$rawValue\""
+                    .replace("\\", "\\\\")
+                    .replace("$", "\\$")
+            else -> return "\"$rawValue\""
+                    .replace("\\", "\\\\")
+        }
+    }
+
 
     override fun copyValueFrom(other: Gene) {
         if(other !is RegexGene){
