b5dcb1f615c06b807e06c483a6bf1e0aade6ea12
jgaleotti <htlfdi41>
2019-07-04 18:51:43 -0300

    The date formatter must include that the language is Locale.ENGLISH

--- client-java/controller/src/main/java/org/evomaster/client/java/controller/internal/db/HeuristicsCalculator.java
+++ client-java/controller/src/main/java/org/evomaster/client/java/controller/internal/db/HeuristicsCalculator.java
@@ -14,7 +14,9 @@ import org.evomaster.client.java.utils.SimpleLogger;
 import java.sql.Timestamp;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.time.format.DateTimeParseException;
+import java.util.Locale;
 import java.util.Objects;
 
 import static org.evomaster.client.java.controller.internal.db.ParserUtils.getWhere;
@@ -258,7 +260,15 @@ public class HeuristicsCalculator {
                     this shouldn't really happen, but looks like Hibernate generate SQL from
                     JPQL with Date handled like this :(
                  */
-                return LocalDate.parse(obj.toString(), DateTimeFormatter.ofPattern("dd-MMM-yy"))
+                DateTimeFormatter df = new DateTimeFormatterBuilder()
+                        // case insensitive to parse JAN and FEB
+                        .parseCaseInsensitive()
+                        // add pattern
+                        .appendPattern("dd-MMM-yy")
+                        // create formatter (use English Locale to parse month names)
+                        .toFormatter(Locale.ENGLISH);
+
+                return LocalDate.parse(obj.toString(), df)
                         .atStartOfDay().toInstant(ZoneOffset.UTC);
             }
         }
