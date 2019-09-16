96488588386d7b68f082927ed377971b0705d143
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2019-09-16 12:35:52 +0200

    Minor tweak for the GeneUtilsEscape test.

--- core/src/test/kotlin/org/evomaster/core/search/gene/GeneUtilsEscapeTest.kt
+++ core/src/test/kotlin/org/evomaster/core/search/gene/GeneUtilsEscapeTest.kt
@@ -4,21 +4,31 @@ import org.evomaster.core.output.OutputFormat
 import org.evomaster.core.output.formatter.OutputFormatter
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertFalse
 
 internal class GeneUtilsEscapeTest {
 
     @Test
     fun testEscapes(){
         val testJson = "{\n" +
-            "\"name\" : \"contains\\\"now\"\n" +
+                "\"name\" : \"contains\\\"now\"\n" +
+                "\"values\" : \"various\u001dother\u0016unicode\"\n" +
+                "\"kotlinTest\" : \"some\$kotlin\"\n" +
             "}"
 
         val formatKotlin = OutputFormat.KOTLIN_JUNIT_5
         val formatJava5 = OutputFormat.JAVA_JUNIT_5
-        println("TestJson: ${GeneUtils.applyEscapes(testJson, "json")}")
+        println("TestJson Raw: $testJson")
+        println("TestJson Kotlin: ${GeneUtils.applyEscapes(testJson, "json", formatKotlin)}")
+        println("TestJson Java: ${GeneUtils.applyEscapes(testJson, "json", formatJava5)}")
 
         assertTrue(GeneUtils.applyEscapes(testJson, "json", formatKotlin).contains("\\"))
         assertTrue(GeneUtils.applyEscapes(testJson, "json", formatJava5).contains("\\"))
+
+        assertTrue(GeneUtils.applyEscapes(testJson, "json", formatKotlin).contains("\""))
+        assertTrue(GeneUtils.applyEscapes(testJson, "json", formatKotlin).contains("\$"))
+
+        assertFalse(GeneUtils.applyEscapes(testJson, "json", formatKotlin).contains("\\u"))
 
         //escaped JSON does not trigger formatter problems?
         //OutputFormatter.JSON_FORMATTER.getFormatted(GeneUtils.applyEscapes(testJson, "json", formatKotlin))
