c0b7a50865f678eb199959e14c12c4763d446ed8
arcuri82 <arcuri82@gmail.com>
2019-09-19 13:32:27 +0200

    fixed bug in MapClassReplacement preventing the start of scout-api

--- client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/coverage/methodreplacement/classes/MapClassReplacement.java
+++ client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/coverage/methodreplacement/classes/MapClassReplacement.java
@@ -4,10 +4,7 @@ import org.evomaster.client.java.instrumentation.coverage.methodreplacement.Meth
 import org.evomaster.client.java.instrumentation.coverage.methodreplacement.Replacement;
 import org.evomaster.client.java.instrumentation.shared.ReplacementType;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class MapClassReplacement implements MethodReplacementClass {
     @Override
@@ -19,14 +16,18 @@ public class MapClassReplacement implements MethodReplacementClass {
     public static boolean containsKey(Map c, Object o, String idTemplate) {
         Objects.requireNonNull(c);
 
+        if (idTemplate == null || c instanceof IdentityHashMap) {
+            /*
+                IdentityHashMap does not use .equals() for the comparisons
+             */
+            return c.containsKey(o);
+        }
+
         // keyset() returns an set instance that indirectly calls
         // to containsKey(). In order to avoid an stack overflow
         // we compute a fresh collection
         Collection keyCollection = new HashSet<>(c.keySet());
         final boolean rv = keyCollection.contains(o);
-        if (idTemplate == null) {
-            return rv;
-        }
 
         CollectionClassReplacement.containsHelper(keyCollection, o, idTemplate);
         return rv;
