a5e5634fbff1d14cf7ccb99b454aea026c6afff3
jgaleotti <htlfdi41>
2019-09-25 07:29:45 -0300

    refactor: distance methods for date and time are moved to DateTimeParsingUtils class

--- client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/coverage/methodreplacement/DateTimeParsingUtils.java
+++ client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/coverage/methodreplacement/DateTimeParsingUtils.java
@@ -1,5 +1,7 @@
 package org.evomaster.client.java.instrumentation.coverage.methodreplacement;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 
 import static org.evomaster.client.java.instrumentation.coverage.methodreplacement.DistanceHelper.*;
@@ -212,4 +214,86 @@ public class DateTimeParsingUtils {
         return base + ((1d - base) / (distance + 1));
     }
 
+    /**
+     * returns a value that represents how close is the value to the format YYYY-MM-DD
+     *
+     * @param input
+     * @return
+     */
+    public static double getDistanceToDateTime(CharSequence input) {
+
+        if (input == null) {
+            return H_REACHED_BUT_NULL;
+        }
+
+        try {
+            new SimpleDateFormat("YYYY-MM-DD HH:MM").parse(input.toString());
+            /*
+                due to the simplification later on, still must make
+                sure to get a 1 if no exception is thrown
+             */
+            return 1d;
+        } catch (ParseException e) {
+            //nothing to do
+        }
+
+        final double base = H_NOT_NULL;
+
+        long distance = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+
+            char c = input.charAt(i);
+
+            //format YYYY-MM-DD HH:MM
+
+            if (i >= 0 && i <= 3) {
+                //any Y value is ok
+                distance += distanceToDigit(c);
+            } else if (i == 4 || i == 7) {
+                distance += distanceToChar(c, '-');
+            } else if (i == 5) {
+                //let's simplify and only allow 01 to 09 for MM
+                distance += distanceToChar(c, '0');
+            } else if (i == 6) {
+                distance += distanceToRange(c, '1', '9');
+            } else if (i == 8) {
+                //let's simplify and only allow 01 to 28
+                distance += distanceToRange(c, '0', '2');
+            } else if (i == 9) {
+                distance += distanceToRange(c, '1', '8');
+            } else if (i == 10) {
+                distance += distanceToChar(c, ' ');
+            } else if (i == 11) {
+                // let's simplify and only allow 00 to 19 for HH
+                distance += distanceToRange(c, '0', '1');
+            } else if (i == 12) {
+                distance += distanceToRange(c, '0', '9');
+            } else if (i == 13) {
+                distance += distanceToChar(c, ':');
+            } else if (i == 14) {
+                // allow 00 to 59 for MM
+                distance += distanceToRange(c, '0', '5');
+            } else if (i == 15) {
+                distance += distanceToRange(c, '0', '9');
+            } else {
+                distance += MAX_CHAR_DISTANCE;
+            }
+        }
+
+        int requiredLength = "YYYY-MM-DD HH:MM".length();
+        if (input.length() < requiredLength) {
+            //too short
+            distance += (MAX_CHAR_DISTANCE * (requiredLength - input.length()));
+        }
+
+        //recall h in [0,1] where the highest the distance the closer to 0
+        return base + ((1d - base) / (distance + 1));
+    }
+
+    public static double getDistanceToDateTimePattern(String input, String dateFormatPattern) {
+        // TODO translate dateFormatPattern to Java regular expression
+        // TODO use distance to Java Regular Expression as an approximate gradient to satisfy the pattern
+        return 0;
+    }
 }
