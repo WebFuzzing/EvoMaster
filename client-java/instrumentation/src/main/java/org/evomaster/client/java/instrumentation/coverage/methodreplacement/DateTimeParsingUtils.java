6690c9a08b1f7891d7e4014610399ddfda6fa02a
jgaleotti <htlfdi41>
2019-09-26 13:54:53 -0300

    bugfix: fixing incorrect distance when input is parseable as a date or time

--- client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/coverage/methodreplacement/DateTimeParsingUtils.java
+++ client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/coverage/methodreplacement/DateTimeParsingUtils.java
@@ -3,9 +3,12 @@ package org.evomaster.client.java.instrumentation.coverage.methodreplacement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+import java.util.Objects;
 
 import static org.evomaster.client.java.instrumentation.coverage.methodreplacement.DistanceHelper.*;
-import static org.evomaster.client.java.instrumentation.coverage.methodreplacement.DistanceHelper.MAX_CHAR_DISTANCE;
 
 public class DateTimeParsingUtils {
 
@@ -22,31 +25,60 @@ public class DateTimeParsingUtils {
 
     private static final int ISO_LOCAL_DATE_TIME_LENGTH = ISO_LOCAL_DATE_TIME_PATTERN.length();
 
+    private static final String DATE_TIME_NO_SECONDS_FORMAT = "YYYY-MM-DD HH:MM";
 
     /**
-     * returns how close the input was to HH:MM
+     * returns how close the input was to HH:MM. If the input fails
      *
      * @param input
      * @return
      */
     private static double getDistanceToLocalTimeWithoutSeconds(CharSequence input) {
-        return getDistanceToLocalTimeWithSeconds(input == null ? null : input + ":00");
+        Objects.requireNonNull(input);
+
+        return getDistanceToLocalTimeWithSeconds(input + ":00");
     }
 
     /**
-     * returns how close the input was to HH:MM:SS
+     * returns how close the input was to HH:MM:SS. If the input
+     * fails to parse, then the distance only considers hours in the range
+     * 00 to 19.
      *
      * @param input
      * @return
      */
     private static double getDistanceToLocalTimeWithSeconds(CharSequence input) {
+        Objects.requireNonNull(input);
 
-        if (input == null) {
-            return H_REACHED_BUT_NULL;
+        try {
+            /*
+                due to the simplification later on (i.e. not all valid local dates
+                are considered, only a subrange), still must make sure to get a 1
+                if no exception is thrown
+             */
+            LocalTime.parse(input);
+            return H_PARSED_OK;
+        } catch (DateTimeParseException ex) {
+            return getApproximateDistanceToLocalTimeWithSeconds(input);
+
         }
 
-        final double base = H_NOT_NULL;
+    }
 
+    /**
+     * Returns the approximate (i.e. simplified) distance of a string
+     * to the format HH:MM:SS.
+     * For simplification, only the range of hours between 00 and 19
+     * are considered.
+     *
+     * @param input a non-null string that fails to parse in
+     *              the HH:MM:SS format
+     * @return
+     */
+    private static double getApproximateDistanceToLocalTimeWithSeconds(CharSequence input) {
+        Objects.requireNonNull(input);
+
+        final double base = H_NOT_NULL;
         long distance = 0;
 
         for (int i = 0; i < input.length(); i++) {
@@ -85,10 +117,30 @@ public class DateTimeParsingUtils {
      * @return
      */
     public static double getDistanceToISOLocalDateTime(CharSequence input) {
-
         if (input == null) {
             return H_REACHED_BUT_NULL;
         }
+        try {
+            LocalDateTime.parse(input);
+            return H_PARSED_OK;
+        } catch (DateTimeParseException ex) {
+            return getApproximateDistanceToISOLocalDateTime(input);
+        }
+    }
+
+    /**
+     * Returns the approximate (i.e. simplified) distance of a string
+     * to the format YYYY-MM-DDTHH:MM (T is case insensitive)
+     *
+     * For simplification, only the range of days between 01 and 28,
+     * and months between 01 and 09, and hours between 00 and 19.
+     *
+     * @param input a non-null string that fails to parse in
+     *              the YYYY-MM-DDTHH:MM format
+     * @return
+     */
+    private static double getApproximateDistanceToISOLocalDateTime(CharSequence input) {
+        Objects.requireNonNull(input);
 
         final double base = H_NOT_NULL;
 
@@ -141,40 +193,66 @@ public class DateTimeParsingUtils {
     }
 
     /**
-     * returns how close the input was to HH:MM or HH:MM:SS
+     * returns how close the input was to HH:MM or HH:MM:SS.
+     * If the input fails to parse in any of those formats, the
+     * distance only considers hours in the 00 to 19 range.
      *
      * @param input
      * @return
      */
     public static double getDistanceToISOLocalTime(CharSequence input) {
+        if (input == null) {
+            return H_REACHED_BUT_NULL;
+        }
+
         return Math.min(
                 getDistanceToLocalTimeWithoutSeconds(input),
                 getDistanceToLocalTimeWithSeconds(input));
     }
 
     /**
-     * returns a value that represents how close is the value to the format YYYY-MM-DD
+     * returns a distance that represents how close is the value to the format YYYY-MM-DD
      *
      * @param input
      * @return
      */
     public static double getDistanceToISOLocalDate(CharSequence input) {
-
         if (input == null) {
             return H_REACHED_BUT_NULL;
         }
-
         try {
-            LocalDate.parse(input);
             /*
-                due to the simplification later on, still must make
-                sure to get a 1 if no exception is thrown
+                due to the simplification later on (i.e. not all valid local dates
+                are considered, only a subrange), still must make sure to get a 1
+                if no exception is thrown
              */
-            return 1d;
-        } catch (RuntimeException e) {
-            //nothing to do
+            LocalDate.parse(input);
+            return H_PARSED_OK;
+        } catch (DateTimeParseException e) {
+            return getApproximateDistanceToISOLocalDate(input);
         }
+    }
 
+    /**
+     * Distance for successfully parsed input
+     */
+    private static final double H_PARSED_OK = 0d;
+
+
+    /**
+     * Returns the approximate (i.e. simplified) distance of a string
+     * to the format YYYY-MM-DD.
+     * For simplification, only the range of days between 01 and 28,
+     * and months between 01 and 09.
+     *
+     * @param input a non-null string that fails to parse in
+     *              the YYYY-MM-DD format
+     * @return
+     */
+    private static double getApproximateDistanceToISOLocalDate(CharSequence input) {
+        Objects.requireNonNull(input);
+
+        //nothing to do
         final double base = H_NOT_NULL;
 
         long distance = 0;
@@ -221,21 +299,34 @@ public class DateTimeParsingUtils {
      * @return
      */
     public static double getDistanceToDateTime(CharSequence input) {
-
         if (input == null) {
             return H_REACHED_BUT_NULL;
         }
-
         try {
-            new SimpleDateFormat("YYYY-MM-DD HH:MM").parse(input.toString());
             /*
-                due to the simplification later on, still must make
-                sure to get a 1 if no exception is thrown
+                due to the simplification later on (i.e. only some valid dates are considered,
+                but not all), still must make sure to get a 1 if no exception is thrown
              */
-            return 1d;
+            new SimpleDateFormat(DATE_TIME_NO_SECONDS_FORMAT).parse(input.toString());
+            return H_PARSED_OK;
         } catch (ParseException e) {
-            //nothing to do
+            return getApproximateDistanceToDateTime(input);
         }
+
+    }
+
+    /**
+     * Returns the approximate (i.e. simplified) distance of a string
+     * to the format YYYY-MM-DD HH:MM.
+     * For simplification, only the range of days between 01 and 28,
+     * and months between 01 and 09, and hours between 00 and 19.
+     *
+     * @param input a non-null string that fails to parse in
+     *              the YYYY-MM-DD HH:MM format
+     * @return
+     */
+    private static double getApproximateDistanceToDateTime(CharSequence input) {
+        Objects.requireNonNull(input);
 
         final double base = H_NOT_NULL;
 
@@ -258,7 +349,7 @@ public class DateTimeParsingUtils {
             } else if (i == 6) {
                 distance += distanceToRange(c, '1', '9');
             } else if (i == 8) {
-                //let's simplify and only allow 01 to 28
+                //let's simplify and only allow 01 to 28 for DD
                 distance += distanceToRange(c, '0', '2');
             } else if (i == 9) {
                 distance += distanceToRange(c, '1', '8');
@@ -292,8 +383,21 @@ public class DateTimeParsingUtils {
     }
 
     public static double getDistanceToDateTimePattern(String input, String dateFormatPattern) {
-        // TODO translate dateFormatPattern to Java regular expression
-        // TODO use distance to Java Regular Expression as an approximate gradient to satisfy the pattern
-        return 0;
+        if (input == null) {
+            return H_REACHED_BUT_NULL;
+        }
+
+        try {
+            /*
+                due to the simplification later on (i.e. only some valid dates are considered,
+                but not all), still must make sure to get a 1 if no exception is thrown
+             */
+            new SimpleDateFormat(dateFormatPattern).parse(input.toString());
+            return H_PARSED_OK;
+        } catch (ParseException e) {
+            // TODO translate dateFormatPattern to Java regular expression
+            // TODO use distance to Java Regular Expression as an approximate gradient to satisfy the pattern
+            return 1d;
+        }
     }
 }
