53bbb49f5d8898df3efd5741af879db74d25534d
arcuri82 <arcuri82@gmail.com>
2019-12-15 12:53:02 +0100

    trying to get multi-module working

---
+++ client-js/integration-tests/src/books-api/book.js
@@ -0,0 +1,15 @@
+
+export default class Book {
+
+    id;
+    title;
+    author;
+    year;
+
+    constructor(id, title, author, year) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+}
---
+++ client-js/integration-tests/src/books-api/repository.js
@@ -0,0 +1,54 @@
+import Book from "./book";
+
+class BookRepository {
+
+    books = new Map();
+
+    counter = 0;
+
+    createNewBook(title, author, year) {
+
+        const id = "" + this.counter;
+        this.counter++;
+
+        const book = new Book(id, title, author, year);
+
+        this.books.set(id, book);
+
+        return id;
+    }
+
+    deleteBook(id) {
+        return this.books.delete(id);
+    }
+
+    getBook(id) {
+        return this.books.get(id);
+    }
+
+    getAllBooks(){
+        return Array.from(this.books.values());
+    }
+
+    updateBook(book) {
+
+        if (! this.books.has(book.id)) {
+            return false;
+        }
+
+        this.books.set(book.id, book);
+        return true;
+    }
+
+    getAllBooksSince(year) {
+        return Array.from(this.books.values()).filter((b) => b.year >= year);
+    }
+
+    reset(){
+        this.books.clear();
+        this.counter = 0;
+    }
+
+}
+
+export default new BookRepository();
