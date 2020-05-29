d9d3748b32f6fb6ed72efdd87863c789a06cdfbc
arcuri82 <arcuri82@gmail.com>
2020-05-29 16:33:58 +0200

    resetting static state in E2E

--- core/src/main/kotlin/org/evomaster/core/StaticCounter.kt
+++ core/src/main/kotlin/org/evomaster/core/StaticCounter.kt
@@ -4,9 +4,9 @@ package org.evomaster.core
 /**
  * WARNING: here we have mutable static state.
  * In general, we should avoid doing something like this.
- * But we need to have way to create unique ids withount a dependency injection.
+ * But we need to have way to create unique ids without a dependency injection.
  * As this should have extremely low impact on the execution, forgetting to reset
- * it at each test case should no impact, apart from very, very special cases (eg,
+ * it at each test case should have no impact, apart from very, very special cases (eg,
  * when we test for determinism)
  */
 class StaticCounter {
