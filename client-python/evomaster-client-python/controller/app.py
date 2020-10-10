19b5fa6f32ebd00aefe1acc833e94fed43c3fd78
axelmaddonni <axel.maddonni@gmail.com>
2020-10-10 17:33:50 -0300

    rename module

--- client-python/evomaster-client-python/controller/app.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, Blueprint
-from controller import controller
-
-
-app = Flask(__name__)
-app.register_blueprint(controller)
--- client-python/evomaster-client-python/instrumentation/injected_functions.py
+++
@@ -1,9 +0,0 @@
-def entering_statement(module: str, line: int, statement: int):
-    print(f"entering statement: {module}-{line}-{statement}")
-
-def completed_statement(module: str, line: int, statement: int):
-    print(f"completed statement: {module}-{line}-{statement}")
-
-def completing_statement(value: any, module: str, line: int, statement: int):
-    completed_statement(module, line, statement)
-    return value
