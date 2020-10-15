531e1fecfa1b0e00d2fcd54e69892b5e3ef8d63f
axelmaddonni <axel.maddonni@gmail.com>
2020-10-14 21:09:45 -0300

    refactor EM controller

--- client-python/evomaster_client/controller/em_app.py
+++ client-python/evomaster_client/controller/em_app.py
@@ -2,10 +2,8 @@ from flask import Flask
 
 from evomaster_client.controller.em_controller import controller
 
-app = Flask(__name__)
-app.register_blueprint(controller)
-
 
 def run_em(config, sut_handler) -> None:
-    globals()['sut_handler'] = sut_handler
+    app = Flask(__name__)
+    app.register_blueprint(controller(sut_handler))
     app.run()
--- client-python/evomaster_client/controller/em_controller.py
+++ client-python/evomaster_client/controller/em_controller.py
@@ -2,57 +2,52 @@
 
 from flask import Blueprint
 
-controller = Blueprint('controller', __name__, url_prefix='/controller/api')
+from evomaster_client.controller.sut_handler import SutHandler
 
 
-# TODO: remove this (used for testing)
-@controller.route('/testStartSut', methods=['GET'])
-def testStartSut():
-    from evomaster_client.controller.em_app import sut_handler
-    print('Starting SUT...')
-    sut_handler.start_sut()
-    return ''
+def controller(sut_handler: SutHandler) -> Blueprint:
+    controller = Blueprint('controller', __name__, url_prefix='/controller/api')
 
+    # TODO: remove this (used for testing)
+    @controller.route('/testStartSut', methods=['GET'])
+    def testStartSut():
+        print('Starting SUT PLUS...')
+        sut_handler.start_sut()
+        return ''
 
-# TODO: remove this (used for testing)
-@controller.route('/testStopSut', methods=['GET'])
-def testStopSut():
-    from evomaster_client.controller.em_app import sut_handler
-    print('Stopping SUT...')
-    sut_handler.stop_sut()
-    return ''
+    # TODO: remove this (used for testing)
+    @controller.route('/testStopSut', methods=['GET'])
+    def testStopSut():
+        print('Stopping SUT...')
+        sut_handler.stop_sut()
+        return ''
 
+    @controller.route('/infoSUT', methods=['GET'])
+    def infoSUT():
+        raise NotImplementedError()
 
-@controller.route('/infoSUT', methods=['GET'])
-def infoSUT():
-    raise NotImplementedError()
+    @controller.route('/runSUT', methods=['PUT'])
+    def runSUT():
+        raise NotImplementedError()
 
+    @controller.route('/testResults', methods=['GET'])
+    def testResults():
+        raise NotImplementedError()
 
-@controller.route('/runSUT', methods=['PUT'])
-def runSUT():
-    raise NotImplementedError()
+    @controller.route('/controllerInfo', methods=['GET'])
+    def controllerInfo():
+        raise NotImplementedError()
 
+    @controller.route('/newSearch', methods=['POST'])
+    def newSearch():
+        raise NotImplementedError()
 
-@controller.route('/testResults', methods=['GET'])
-def testResults():
-    raise NotImplementedError()
+    @controller.route('/newAction', methods=['PUT'])
+    def newAction():
+        raise NotImplementedError()
 
+    @controller.route('/databaseCommand', methods=['POST'])
+    def databaseCommand():
+        raise NotImplementedError()
 
-@controller.route('/controllerInfo', methods=['GET'])
-def controllerInfo():
-    raise NotImplementedError()
-
-
-@controller.route('/newSearch', methods=['POST'])
-def newSearch():
-    raise NotImplementedError()
-
-
-@controller.route('/newAction', methods=['PUT'])
-def newAction():
-    raise NotImplementedError()
-
-
-@controller.route('/databaseCommand', methods=['POST'])
-def databaseCommand():
-    raise NotImplementedError()
+    return controller
