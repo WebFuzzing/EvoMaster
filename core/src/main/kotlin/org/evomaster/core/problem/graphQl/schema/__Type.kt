7676b0fe316b6fed3b376d9092c1a8aff96c976d
asmab <asma.belhadi.17@gmail.com>
2020-11-06 15:34:15 +0100

    add sub-package add data classes add documentation remove null values inside a list use meaningful name add query actions add mutation actions

---
+++ core/src/main/kotlin/org/evomaster/core/problem/graphQl/schema/__Type.kt
@@ -0,0 +1,11 @@
+package org.evomaster.core.problem.graphQl.schema
+
+/**
+ *  Field: __type part of the introspection system.
+ */
+
+data class __Type (var name: String,
+                   var kind: __TypeKind){
+
+
+}
