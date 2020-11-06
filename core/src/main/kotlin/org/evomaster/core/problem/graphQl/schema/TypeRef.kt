065b3351f9cf7605ff831cc5ffed3ef432fca995
asmab <asma.belhadi.17@gmail.com>
2020-11-06 15:09:34 +0100

    add query actions add mutation actions

---
+++ core/src/main/kotlin/org/evomaster/core/problem/graphQl/schema/TypeRef.kt
@@ -0,0 +1,14 @@
+package org.evomaster.core.problem.graphQl.schema
+
+/**
+ * TypeRef: fragment on __Type
+ */
+
+data class TypeRef (var kind: __TypeKind,
+               var name: String,
+               var ofType: ofTypeOn__Type){
+
+
+
+}
+
