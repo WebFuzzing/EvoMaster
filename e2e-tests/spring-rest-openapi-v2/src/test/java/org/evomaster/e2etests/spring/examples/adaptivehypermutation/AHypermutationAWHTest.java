f682d1efde3deee4af4b95fe42500196eec3137f
ZhangMan <m.zhang1126@gmail.com>
2020-12-02 11:52:11 +0100

    update the awh example and add comments

--- e2e-tests/spring-rest-openapi-v2/src/test/java/org/evomaster/e2etests/spring/examples/adaptivehypermutation/AHypermutationAWHTest.java
+++ e2e-tests/spring-rest-openapi-v2/src/test/java/org/evomaster/e2etests/spring/examples/adaptivehypermutation/AHypermutationAWHTest.java
@@ -12,57 +12,84 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
+/**
+ * with same budget, i.e., 20_000 HTTP calls (see {@link #budget})
+ * for the example (only include /foos/{x} endpoints see {@link #initClass()}),
+ * we tested the performances of default MIO and MIO-WH* based on
+ * a capability of covering expected targets(see {@link #countExpectedCoveredTargets(Solution, List)} ).
+ * Based on the tests, with 3 attempts,
+ * 1) default MIO always covers less than 3 out of the 7 expected targets (even 0 sometimes);
+ * 2) MIO-WH* is able to cover at least 6 out of the 7 expected targets.
+ */
 public class AHypermutationAWHTest extends AHypermuationTestBase {
-
+    private static int budget = 20_000;
+    private static String statisticsFile = TESTS_OUTPUT_ROOT_FOLDER + "/AWH/statistics.csv";
+    private static String snapshotFile = TESTS_OUTPUT_ROOT_FOLDER + "/AWH/snapshot.csv";
     @Test
-    public void testRunMIO() throws Throwable {
+    public void testRunMIO() {
+        
+        List<String> msg = new ArrayList<>();
+        assertThrows(Throwable.class, () -> {
+            runTestHandlingFlakyAndCompilation(
+                    "AWH/TestBase",
+                    "org.adaptivehypermuation.BaseTest",
+                    budget,
+                    true,
+                    (args) -> {
 
-        runTestHandlingFlakyAndCompilation(
-                "TestBase",
-                "org.adaptivehypermuation.BaseTest",
-                20_000,
-                true,
-                (args) -> {
+                        args.add("--probOfArchiveMutation");
+                        args.add("0.0");
 
-                    args.add("--probOfArchiveMutation");
-                    args.add("0.0");
+                        args.add("--weightBasedMutationRate");
+                        args.add("false");
 
-                    args.add("--weightBasedMutationRate");
-                    args.add("false");
+                        args.add("--adaptiveGeneSelectionMethod");
+                        args.add("NONE");
 
-                    args.add("--adaptiveGeneSelectionMethod");
-                    args.add("NONE");
+                        args.add("--archiveGeneMutation");
+                        args.add("NONE");
 
-                    args.add("--archiveGeneMutation");
-                    args.add("NONE");
+                        args.add("--enableTrackEvaluatedIndividual");
+                        args.add("false");
 
-                    args.add("--enableTrackEvaluatedIndividual");
-                    args.add("false");
+                        args.add("--writeStatistics");
+                        args.add("true");
 
-                    Solution<RestIndividual> solution = initAndRun(args);
-                    int count = 0;
-                    List<String> msg = new ArrayList<>();
-                    count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B0", count, msg);
-                    count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B1", count, msg);
-                    count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B2", count, msg);
-                    count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B3", count, msg);
-                    count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B4", count, msg);
-                    count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B5", count, msg);
-                    count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B6", count, msg);
+                        args.add("--statisticsFile");
+                        args.add(statisticsFile);
 
-                    assertTrue(count > 3, String.join("\n", msg));
-                }, 10);
+                        args.add("--appendToStatisticsFile");
+                        args.add("true");
+
+                        args.add("--snapshotInterval");
+                        args.add("5");
+
+                        args.add("--snapshotStatisticsFile");
+                        args.add(snapshotFile);
+
+                        args.add("--statisticsColumnId");
+                        args.add("awh-example-base");
+
+                        Solution<RestIndividual> solution = initAndRun(args);
+
+
+                        int count = countExpectedCoveredTargets(solution, msg);
+
+                        assertTrue(count >= 3);
+                    }, 10);
+        }, String.join("\n", msg));
+
     }
 
     @Test
     public void testRunMIOAWH() throws Throwable {
 
         runTestHandlingFlakyAndCompilation(
-                "TestAHW",
+                "AWH/TestAHW",
                 "org.adaptivehypermuation.AWHTest",
-                20_000,
+                budget,
                 true,
                 (args) -> {
 
@@ -81,20 +108,45 @@ public class AHypermutationAWHTest extends AHypermuationTestBase {
                     args.add("--enableTrackEvaluatedIndividual");
                     args.add("true");
 
-                    Solution<RestIndividual> solution = initAndRun(args);
-                    int count = 0;
-                    List<String> msg = new ArrayList<>();
-                    count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B0", count, msg);
-                    count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B1", count, msg);
-                    count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B2", count, msg);
-                    count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B3", count, msg);
-                    count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B4", count, msg);
-                    count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B5", count, msg);
-                    count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B6", count, msg);
+                    args.add("--writeStatistics");
+                    args.add("true");
 
-                    assertTrue(count >= 5, String.join("\n", msg));
+                    args.add("--statisticsFile");
+                    args.add(statisticsFile);
+
+                    args.add("--appendToStatisticsFile");
+                    args.add("true");
+
+                    args.add("--snapshotInterval");
+                    args.add("5");
+
+                    args.add("--snapshotStatisticsFile");
+                    args.add(snapshotFile);
+
+                    args.add("--statisticsColumnId");
+                    args.add("awh-example-awh");
+
+                    Solution<RestIndividual> solution = initAndRun(args);
+
+                    List<String> msg = new ArrayList<>();
+                    int count = countExpectedCoveredTargets(solution, msg);
+
+                    assertTrue(count >= 6, String.join("\n", msg));
                 }, 10);
     }
+
+    private int countExpectedCoveredTargets(Solution<RestIndividual> solution , List<String> msg){
+        int count = 0;
+        count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B0", count, msg);
+        count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B1", count, msg);
+        count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B2", count, msg);
+        count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B3", count, msg);
+        count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B4", count, msg);
+        count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B5", count, msg);
+        count = countExpected(solution, HttpVerb.POST, 200, "/api/foos/{x}", "B6", count, msg);
+        return count;
+    }
+
     @BeforeAll
     public static void initClass() throws Exception {
         SpringTestBase.initClass(new AHypermutationRestController(Arrays.asList("/api/bars/{a}")));
