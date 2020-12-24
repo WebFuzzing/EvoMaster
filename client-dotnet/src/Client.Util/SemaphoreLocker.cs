86bd31b016a2fc2da72116e2544c61b3f5ea4cb7
amid <golmohammadi.edu@gmail.com>
2020-12-24 13:49:03 +0330

    Complete SemaphoreLocker

--- client-dotnet/src/Client.Util/SemaphoreLocker.cs
+++ client-dotnet/src/Client.Util/SemaphoreLocker.cs
@@ -6,14 +6,14 @@ namespace Client.Util {
     public class SemaphoreLocker {
         private readonly SemaphoreSlim _semaphore = new SemaphoreSlim (1, 1);
 
-        // public async Task LockAsync (Func<Task> worker) {
-        //     await _semaphore.WaitAsync ();
-        //     try {
-        //         await worker ();
-        //     } finally {
-        //         _semaphore.Release ();
-        //     }
-        // }
+        public async Task LockAsync (Func<Task> worker) {
+            await _semaphore.WaitAsync ();
+            try {
+                await worker ();
+            } finally {
+                _semaphore.Release ();
+            }
+        }
 
         public async Task<T> LockAsync<T> (Func<Task<T>> worker) {
             await _semaphore.WaitAsync ();
