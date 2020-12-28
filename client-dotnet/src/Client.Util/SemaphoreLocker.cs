fe5dc07b4a6fc50a2eb50753b645e034f85675dd
amid <golmohammadi.edu@gmail.com>
2020-12-28 12:05:11 +0330

    Add some comments for SemaphoreLocker

--- client-dotnet/src/Client.Util/SemaphoreLocker.cs
+++ client-dotnet/src/Client.Util/SemaphoreLocker.cs
@@ -3,6 +3,11 @@ using System.Threading;
 using System.Threading.Tasks;
 
 namespace Client.Util {
+    /*This class is used for limiting access to a segment of code by multiple threads
+      It works like a lock which limits the accessing threads to one
+      However, it is not allowed to put an asynchronous operation inside locks
+      This class tackles this limitation
+    */
     public class SemaphoreLocker {
         private readonly SemaphoreSlim _semaphore = new SemaphoreSlim (1, 1);
 
