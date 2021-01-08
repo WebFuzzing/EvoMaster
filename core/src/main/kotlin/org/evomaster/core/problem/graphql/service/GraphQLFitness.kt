64fdfb6deffd7426f8915ec8b721168709cea3ea
asmab <asma.belhadi.17@gmail.com>
2021-01-08 14:55:30 +0100

    Add a GQ call result+ Implement handleGraphQLCall + Implement createInvocation (1 version).

--- core/src/main/kotlin/org/evomaster/core/problem/graphql/service/GraphQLFitness.kt
+++ core/src/main/kotlin/org/evomaster/core/problem/graphql/service/GraphQLFitness.kt
@@ -1,16 +1,34 @@
 package org.evomaster.core.problem.graphql.service
 
+import GraphQlCallResult
+import org.evomaster.client.java.controller.api.EMTestUtils
 import org.evomaster.core.problem.graphql.GraphQLAction
 import org.evomaster.core.problem.graphql.GraphQLIndividual
+import org.evomaster.core.problem.graphql.param.GQInputParam
+import org.evomaster.core.problem.graphql.param.GQReturnParam
 import org.evomaster.core.problem.httpws.service.HttpWsFitness
+import org.evomaster.core.remote.TcpUtils
 import org.evomaster.core.search.ActionResult
 import org.evomaster.core.search.EvaluatedIndividual
 import org.evomaster.core.search.FitnessValue
+import org.evomaster.core.search.gene.GeneUtils
 import org.evomaster.core.taint.TaintAnalysis
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import javax.ws.rs.ProcessingException
+import javax.ws.rs.client.ClientBuilder
+import javax.ws.rs.client.Entity
+import javax.ws.rs.client.Invocation
+import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.NewCookie
+
+
 
 class GraphQLFitness : HttpWsFitness<GraphQLIndividual>() {
 
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(GraphQLFitness::class.java)
+    }
 
     override fun doCalculateCoverage(individual: GraphQLIndividual, targets: Set<Int>): EvaluatedIndividual<GraphQLIndividual>? {
         rc.resetSUT()
@@ -87,7 +105,132 @@ class GraphQLFitness : HttpWsFitness<GraphQLIndividual>() {
             on their length
          */
 
+        val gqlcr = GraphQlCallResult()
+        actionResults.add(gqlcr)
+
+        val response = try {
+            createInvocation(action, cookies).invoke()
+        } catch (e: ProcessingException) {
+
+            /*
+                this could have happened for example if call ends up in an infinite redirection loop.
+                However, as we no longer follow 3xx automatically, it should not happen anymore
+             */
+            when {
+                TcpUtils.isTooManyRedirections(e) -> {
+                    gqlcr.setInfiniteLoop(true)
+                    gqlcr.setErrorMessage(e.cause!!.message!!)
+                    return false
+                }
+                TcpUtils.isTimeout(e) -> {
+                    /*
+                        This is very tricky. In theory it shouldn't happen that a REST call
+                        does timeout (eg 10 seconds). But it might happen due to glitch,
+                        or if very slow hardware. If it is a glitch, we do not want to
+                        kill whole EM process, as might not happen again. If it is a
+                        constant, we want to avoid using such test if possible, as it
+                        would kill performance.
+                        In any case, a generated test should never check assertions on time,
+                        eg expect that a is SocketTimeoutException thrown. Not only because
+                        maybe it was just a glitch, but also because the test might be run
+                        on different machines (remote CI vs local development PC) with
+                        different performance (and so the test would become flaky)
+                     */
+                    gqlcr.setTimedout(true)
+                    statistics.reportTimeout()
+                    return false
+                }
+                TcpUtils.isOutOfEphemeralPorts(e) -> {
+                    /*
+                        This could happen if for any reason we run out of ephemeral ports.
+                        In such a case, we wait X seconds, and try again, as OS might have released ports
+                        meanwhile.
+                        And while we are at it, let's release any hanging network resource
+                     */
+                    client.close() //make sure to release any resource
+                    client = ClientBuilder.newClient()
+
+                    TcpUtils.handleEphemeralPortIssue()
+
+                    createInvocation(action, cookies).invoke()
+                }
+                TcpUtils.isStreamClosed(e) || TcpUtils.isEndOfFile(e) -> {
+                    /*
+                        This should not really happen... but it does :( at least on Windows...
+                     */
+                    log.warn("TCP connection to SUT problem: ${e.cause!!.message}")
+                    gqlcr.setTcpProblem(true)
+                    return false
+                }
+                else -> throw e
+            }
+        }
+
+
         //TODO
         return false
     }
+
+
+    private fun createInvocation(a: GraphQLAction, cookies: Map<String, List<NewCookie>>): Invocation {
+        val baseUrl = getBaseUrl()
+
+        val path = "/graphql"
+
+        val locationHeader = null
+
+        val fullUri = EMTestUtils.resolveLocation(locationHeader, baseUrl + path)!!
+                .let {
+                    /*
+                        TODO this will be need to be done properly, and check if
+                        it is or not a valid char.
+                        Furthermore, likely needed to be done in resolveLocation,
+                        or at least check how RestAssured would behave
+                     */
+                    //it.replace("\"", "")
+                    GeneUtils.applyEscapes(it, GeneUtils.EscapeMode.URI, configuration.outputFormat)
+                }
+
+
+        val builder = client.target(fullUri).request("*/*")
+
+        a.auth.headers.forEach {
+            builder.header(it.name, it.value)
+        }
+
+        val prechosenAuthHeaders = a.auth.headers.map { it.name }
+
+
+        if (a.auth.cookieLogin != null) {
+            val list = cookies[a.auth.cookieLogin!!.username]
+            if (list == null || list.isEmpty()) {
+                log.warn("No cookies for ${a.auth.cookieLogin!!.username}")
+            } else {
+                list.forEach {
+                    builder.cookie(it.toCookie())
+                }
+            }
+        }
+
+
+        val body = a.parameters.find { p -> p is GQReturnParam }
+
+
+        val bodyEntity = if (body != null && body is GQReturnParam) {
+            val mode = GeneUtils.EscapeMode.JSON
+               if (mode != GeneUtils.EscapeMode.JSON){
+                throw IllegalStateException("Cannot handle body type: ")
+            }
+            Entity.entity(body.gene.getValueAsPrintableString(mode = mode, targetFormat = configuration.outputFormat), "")
+        } else if (body != null && body is GQInputParam) {
+            val mode = GeneUtils.EscapeMode.JSON
+            Entity.entity(body.gene.getValueAsPrintableString(mode = mode, targetFormat = configuration.outputFormat), "")
+        } else {
+            null
+        }
+
+        val invocation = builder.buildPost(bodyEntity)
+        return invocation
+    }
+
 }
