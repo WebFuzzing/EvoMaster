aa9451abf108f87bb145723527e0b84424e4cd01
arcuri82 <arcuri82@gmail.com>
2021-03-03 11:03:37 +0100

    fixed another source of non-determinism

--- client-java/controller/src/main/java/org/evomaster/client/java/controller/internal/db/StandardOutputTracker.java
+++ client-java/controller/src/main/java/org/evomaster/client/java/controller/internal/db/StandardOutputTracker.java
@@ -70,6 +70,7 @@ public class StandardOutputTracker extends ByteArrayOutputStream{
             reset();
 
             getOut().print(data);
+            getOut().flush();
         }
 
         if (data != null) {
