1c8838c62feb2ecf1073f38c8a1ba0400b0bf443
Man Zhang <m.zhang1126@gmail.com>
2021-03-18 11:59:08 +0100

    forget to remove runWithDeterministicLogger from LoggingUtil.kt

--- core/src/main/kotlin/org/evomaster/core/logging/LoggingUtil.kt
+++ core/src/main/kotlin/org/evomaster/core/logging/LoggingUtil.kt
@@ -81,50 +81,6 @@ class LoggingUtil {
             }
             return true
         }
-
-
-        /**
-         * Run lambda within a deterministic version of logback.xml (e.g., no time-stamps),
-         * and return the generated logs
-         */
-        fun runWithDeterministicLogger(lambda: () -> Unit) : String{
-
-            val latestOut = System.out
-
-            //create an in-memory buffer to write to
-            val byteStream = ByteArrayOutputStream()
-            val outStream = PrintStream(byteStream)
-
-            if (latestOut is WrappedPrintStream){
-                /*
-                    we can also manipulate the current [latestOut], i.e., set [outStream] as printStream of
-                    the current StandardOutputTracker, but it might cause some problems when setting its status back.
-                    therefore, here, we create a new PrintStream which keeps the same setting with the current one.
-                 */
-                System.setOut(latestOut.copyWithRestPrintStream(outStream))
-            }else{
-                System.setOut(outStream)
-            }
-
-            changeLogbackFile("logback_for_determinism_check.xml")
-
-            try {
-                lambda()
-            } finally {
-                //before returning the logs, restore the default settings
-                changeLogbackFile("logback.xml")
-                System.setOut(latestOut)
-            }
-
-            val logs = byteStream.toString()
-
-            //as done by a separated thread, their ordering in the logs is not guaranteed, so we skip them
-            val filtered = logs.split("\n")
-                    .filter { ! it.startsWith(P6SpyFormatter.PREFIX) }
-                    .joinToString("\n")
-
-            return filtered
-        }
     }
 }
 
