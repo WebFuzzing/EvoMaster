5cbef8b5c9a82c5e6c45cc77feaca855d522d848
asmab <asma.belhadi.17@gmail.com>
2021-03-26 09:33:49 +0100

    Add primitive Type API.

---
+++ e2e-tests/spring-graphql/src/test/kotlin/org/evomaster/e2etests/spring/graphql/primitiveType/GQLPrimitiveTypeEMTest.kt
@@ -0,0 +1,41 @@
+package org.evomaster.e2etests.spring.graphql.primitiveType
+
+import com.foo.graphql.primitiveType.PrimitiveTypeController
+import org.evomaster.core.EMConfig
+import org.evomaster.e2etests.spring.graphql.SpringTestBase
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+
+
+
+class GQLPrimitiveTypeEMTest: SpringTestBase() {
+
+
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun init() {
+            initClass(PrimitiveTypeController())
+        }
+    }
+
+
+    @Test
+    fun testRunEM() {
+        runTestHandlingFlakyAndCompilation(
+                "GQL_PrimitiveTypeEM",
+                "org.foo.graphql.MutationEM",
+                20
+        ) { args: MutableList<String> ->
+
+            args.add("--problemType")
+            args.add(EMConfig.ProblemType.GRAPHQL.toString())
+
+            val solution = initAndRun(args)
+
+            Assertions.assertTrue(solution.individuals.size >= 1)
+            assertHasAtLeastOneResponseWithData(solution)
+        }
+    }
+}
