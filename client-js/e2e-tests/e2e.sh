120d48ce80ab224bd2e9f4888e627b02545a944b
arcuri82 <arcuri82@gmail.com>
2021-03-27 09:59:00 +0100

    fix for CI issue

--- client-js/e2e-tests/e2e.sh
+++ client-js/e2e-tests/e2e.sh
@@ -7,6 +7,6 @@ trap 'kill $(jobs -p)' EXIT
 SCRIPT_FOLDER_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $SCRIPT_FOLDER_LOCATION || exit 1
 
-RUN=./e2e_runner.sh
+RUN="bash ./e2e_runner.sh"
 
 $RUN /client-js/integration-tests/build/src/books-api   em-main.js   app-driver.js  50
