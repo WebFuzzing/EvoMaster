9672c77406bf4ba802fc29c38602a33585d2115c
asmab <asma.belhadi.17@gmail.com>
2021-03-29 11:21:46 +0200

    Add basic Enum Input API.

---
+++ e2e-tests/spring-graphql/src/main/kotlin/com/foo/graphql/enumInput/resolver/QueryResolver.kt
@@ -0,0 +1,24 @@
+package com.foo.graphql.enumInput.resolver
+
+
+
+import com.foo.graphql.enumInput.DataRepository
+import com.foo.graphql.enumInput.type.Flower
+import com.foo.graphql.enumInput.type.FlowerType
+import graphql.kickstart.tools.GraphQLQueryResolver
+import org.springframework.stereotype.Component
+@Component
+open class QueryResolver(
+        private val dataRepo: DataRepository
+) : GraphQLQueryResolver {
+    fun flowersByType(type: FlowerType?): Flower?{
+        return dataRepo.findByFlTy(type)
+    }
+
+}
+
+
+
+
+
+
