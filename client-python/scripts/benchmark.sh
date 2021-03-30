b3198d6d79a57192240dc7013c9b7f9436dc3661
axelmaddonni <axel.maddonni@gmail.com>
2021-03-30 01:56:25 -0300

    Add SEED to benchmark script

--- client-python/scripts/benchmark.sh
+++ client-python/scripts/benchmark.sh
@@ -1,40 +1,43 @@
 
 #!/bin/sh
-set -euf -o pipefail
+# set -euf -o pipefail
 
-EVOMASTER_TIME="10s"
+EVOMASTER_TIME="30m"
 
-for APP in 'ncs' 'scs' 'news'
+for SEED in {1..5}
 do
-    for LEVEL in 0 1 2 3
+    for APP in 'ncs' 'scs'
     do
-        # Prepare workdirs
-        GENERATED_DIR="generated/$APP/$LEVEL"
-        mkdir -p $GENERATED_DIR
+        for LEVEL in 0 1 2 3
+        do
+            # Prepare workdirs
+            GENERATED_DIR="generated/$APP/$LEVEL"
+            mkdir -p $GENERATED_DIR
 
-        nohup python -m evomaster_client.cli run-em-handler -m "evomaster_benchmark.em_handlers.$APP" -c 'EMHandler' -i $LEVEL > "$GENERATED_DIR/tmp.log" 2>&1 &
-        echo $! > save_pid.txt
-        PID="$(cat save_pid.txt)"
+            nohup python -m evomaster_client.cli run-em-handler -m "evomaster_benchmark.em_handlers.$APP" -c 'EMHandler' -i $LEVEL > "$GENERATED_DIR/tmp.log" 2>&1 &
+            echo $! > save_pid.txt
+            PID="$(cat save_pid.txt)"
 
-        TESTS_DIR="$GENERATED_DIR/$PID/tests"
-        LOGS_DIR="$GENERATED_DIR/$PID/logs"
-        COVERAGE_DIR="$GENERATED_DIR/$PID/coverage"
-        mkdir -p $TESTS_DIR
-        mkdir -p $LOGS_DIR
-        mkdir -p $COVERAGE_DIR
+            TESTS_DIR="$GENERATED_DIR/$PID/tests"
+            LOGS_DIR="$GENERATED_DIR/$PID/logs"
+            COVERAGE_DIR="$GENERATED_DIR/$PID/coverage"
+            mkdir -p $TESTS_DIR
+            mkdir -p $LOGS_DIR
+            mkdir -p $COVERAGE_DIR
 
-        echo "Running EvoMaster handler for $APP (level $LEVEL) in $PID..."
-        java -jar ../core/target/evomaster.jar --maxTime $EVOMASTER_TIME --writeStatistics false --outputFolder $TESTS_DIR --outputFormat PYTHON_UNITTEST
+            echo "Running EvoMaster handler for $APP (level $LEVEL) in $PID..."
+            java -jar ../core/target/evomaster.jar --maxTime $EVOMASTER_TIME --writeStatistics false --outputFolder $TESTS_DIR --outputFormat PYTHON_UNITTEST
 
-        echo "Shutting down EvoMaster handler for $APP (level $LEVEL) in $PID..."
-        kill $PID
-        rm save_pid.txt
+            echo "Shutting down EvoMaster handler for $APP (level $LEVEL) in $PID..."
+            kill $PID
+            rm save_pid.txt
 
-        # Move logs from tmp file
-        mv "$GENERATED_DIR/tmp.log"  "$LOGS_DIR/emhandler.log"
+            # Move logs from tmp file
+            mv "$GENERATED_DIR/tmp.log"  "$LOGS_DIR/emhandler.log"
 
-        # Generate coverage reports
-        python -m pytest --cov-report html:$COVERAGE_DIR/cov_html --cov-report xml:$COVERAGE_DIR/cov.xml --cov-report annotate:$COVERAGE_DIR/cov_annotate \
-                        --cov=evomaster_benchmark/$APP "$TESTS_DIR/EvoMasterTest.py"
+            # Generate coverage reports
+            python -m pytest --cov-report html:$COVERAGE_DIR/cov_html --cov-report xml:$COVERAGE_DIR/cov.xml --cov-report annotate:$COVERAGE_DIR/cov_annotate \
+                            --cov=evomaster_benchmark/$APP "$TESTS_DIR/EvoMasterTest.py"
+        done
     done
 done
