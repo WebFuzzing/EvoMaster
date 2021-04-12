9c055750b8e538db5859d1f5a533596a4b778718
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2021-04-12 21:02:30 +0200

    Updates for the e2e test, and master merge.

--- e2e-tests/spring-rest-openapi-v3/src/test/kotlin/com/foo/rest/examples/spring/openapi/v3/statistics/StatisticsController.kt
+++ e2e-tests/spring-rest-openapi-v3/src/test/kotlin/com/foo/rest/examples/spring/openapi/v3/statistics/StatisticsController.kt
@@ -2,5 +2,4 @@ package com.foo.rest.examples.spring.openapi.v3.statistics
 
 import com.foo.rest.examples.spring.openapi.v3.SpringController
 
-class StatisticsController : SpringController(StatisticsApplication::class.java) {
-}
+class StatisticsController : SpringController(StatisticsApplication::class.java)
