07cfef4e2a7c3dff6c46598264829505fac095e5
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2021-04-14 20:24:03 +0200

    Minor update, trying to get all the e2e tests passing. Reverted changes to visibility to some Main functions.

--- e2e-tests/spring-rest-openapi-v3/src/test/kotlin/org/evomaster/e2etests/spring/openapi/v3/statistics/StatisticsEMTest.kt
+++ e2e-tests/spring-rest-openapi-v3/src/test/kotlin/org/evomaster/e2etests/spring/openapi/v3/statistics/StatisticsEMTest.kt
@@ -1,27 +1,20 @@
 package org.evomaster.e2etests.spring.openapi.v3.statistics
 
 import com.foo.rest.examples.spring.openapi.v3.statistics.StatisticsController
-import io.restassured.RestAssured
-import org.evomaster.core.EMConfig
-import org.evomaster.core.Main
 import org.evomaster.core.problem.rest.HttpVerb
-import org.evomaster.core.problem.rest.RestIndividual
-import org.evomaster.core.search.Solution
-import org.evomaster.core.search.service.IdMapper
-import org.evomaster.core.search.service.SearchTimeController
 import org.evomaster.core.search.service.Statistics
 import org.evomaster.e2etests.spring.openapi.v3.SpringTestBase
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 
-class StatisticsEMTest :SpringTestBase() {
+class StatisticsEMTest : SpringTestBase() {
 
     companion object {
         @BeforeAll
         @JvmStatic
         fun init() {
-            initClass(StatisticsController())
+            SpringTestBase.initClass(StatisticsController())
         }
     }
 
@@ -31,11 +24,7 @@ class StatisticsEMTest :SpringTestBase() {
                 "StatisticsEM",
                 "org.foo.StatisticsEM",
                 1000
-        ){args: MutableList<String> ->
-            args.add("--expectationsActive")
-            args.add("" + true)
-            args.add("--testSuiteSplitType")
-            args.add("NONE")
+        ){args: List<String> ->
 
             val solution = initAndRun(args)
 
@@ -47,8 +36,8 @@ class StatisticsEMTest :SpringTestBase() {
 
             assert(data.find { p -> p.header.contains("errors5xx") }?.element == "1")
             assert(data.find { p -> p.header.contains("distinct500Faults")}?.element == "1")
-            assert(data.find { p -> p.header.contains("failedOracleExpectations")}?.element == "0")
-            assert(data.find { p -> p.header.contains("potentialFaults")}?.element == "2")
+            assert(data.find { p -> p.header.contains("failedOracleExpectations")}?.element == "1")
+            assert(data.find { p -> p.header.contains("potentialFaults")}?.element == "3")
         }
     }
 }
