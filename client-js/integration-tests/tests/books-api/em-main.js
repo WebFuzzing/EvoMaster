d7767c28b5fdb37c7cd07e326259286921a50a9f
arcuri82 <arcuri82@gmail.com>
2021-04-29 07:28:11 +0200

    fixed issue with JS E2E

--- client-js/integration-tests/tests/books-api/em-main.js
+++ client-js/integration-tests/tests/books-api/em-main.js
@@ -4,6 +4,7 @@ const em = require("evomaster-client-js");
 
 const controller = new em.EMController(new AppController());
 
+controller.setPort(process.env.PORT || 40100)
 
 controller.startTheControllerServer();
 
