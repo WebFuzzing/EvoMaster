b35b0fdf86f459b06f3521fb70cc49675e5a5456
arcuri82 <arcuri82@gmail.com>
2021-05-03 11:53:35 +0200

    tests for other TT functions in String

--- client-js/integration-tests/tests/snippets/methodreplacement/babel-eval-string-test.js
+++ client-js/integration-tests/tests/snippets/methodreplacement/babel-eval-string-test.js
@@ -118,3 +118,129 @@ test("Test startsWith offset: heuristic", () => {
 
 });
 
+
+test("Test endsWith", () => {
+
+    const target = "123";
+
+    let f;
+    const code = dedent`
+        f = function(s,i,p){ return s.endsWith(i,p);}
+    `;
+    const instrumented = runPlugin(code).code;
+    eval(instrumented);
+
+    const lambda = (k) => {
+        f(k.s, k.i, k.p);
+    };
+
+    ExampleUtils.checkIncreasingTillCoveredForSingleMethodReplacement(
+        [
+            {s: "", i: target},
+            {s: "a", i: target},
+            {s: "abced", i: target},
+            {s: "abce1", i: target},
+            {s: "abce2", i: target},
+            {s: "abc12", i: target},
+            {s: "abc121", i: target}
+        ],
+        {s: "foobar" + target, i: target},
+        lambda
+    );
+
+});
+
+
+
+test("Test includes", () => {
+
+    const target = "abc";
+
+    let f;
+    const code = dedent`
+        f = function(s,i,p){ return s.includes(i,p);}
+    `;
+    const instrumented = runPlugin(code).code;
+    eval(instrumented);
+
+    const lambda = (k) => {
+        f(k.s, k.i, k.p);
+    };
+
+    ExampleUtils.checkIncreasingTillCoveredForSingleMethodReplacement(
+        [
+            {s: "", i: target},
+            {s: "a", i: target},
+            {s: "z  ", i: target},
+            {s: "z  zbcd", i: target},
+            {s: "z  zbcd bbd", i: target},
+            {s: "z  zbcd abbd", i: target}
+        ],
+        {s:target, i: target},
+        lambda
+    );
+
+});
+
+
+test("Test indexOf", () => {
+
+    let f;
+    const code = dedent`
+        f = function(s,i,p){ return s.indexOf(i,p);}
+    `;
+    const instrumented = runPlugin(code).code;
+    eval(instrumented);
+
+    const lambda = (k) => {
+        f(k.s, k.i, k.p);
+    };
+
+    const target = "42";
+
+    ExampleUtils.checkIncreasingTillCoveredForSingleMethodReplacement(
+        [
+            {s: "", i: target},
+            {s: "a", i: target},
+            {s: "z  ", i: target},
+            {s: "z  4", i: target},
+            {s: "z  40", i: target},
+            {s: "z  41  ", i: target}
+        ],
+        {s:"abc"+target+"foo", i: target},
+        lambda
+    );
+
+});
+
+test("Test lastIndexOf", () => {
+
+    let f;
+    const code = dedent`
+        f = function(s,i,p){ return s.indexOf(i,p);}
+    `;
+    const instrumented = runPlugin(code).code;
+    eval(instrumented);
+
+    const lambda = (k) => {
+        f(k.s, k.i, k.p);
+    };
+
+    const target = "42";
+
+    ExampleUtils.checkIncreasingTillCoveredForSingleMethodReplacement(
+        [
+            {s: "", i: target, p:-100},
+            {s: "a", i: target, p:0},
+            {s: "z  ", i: target, p:2},
+            {s: "z  1", i: target, p:2},
+            {s: "z  12 ", i: target, p:4},
+            {s: "z  41    3  ", i: target, p:9},
+            {s: "z  41    3  ", i: target, p:0},
+        ],
+        {s:"abc"+target+"foo", i: target},
+        lambda
+    );
+
+});
+
