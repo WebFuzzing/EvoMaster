487188206753f215bdcd78fb19f11af0bff14123
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2021-06-22 17:50:43 +0200

    Updates the Assertions EM tests and consolidated all the tests related to assertions in one place:
    openapi.v3.assertions -> AssertionEMTEst
    
    this includes turning basic assertions on/off and checking for java/kotlin.

--- e2e-tests/spring-rest-openapi-v3/src/test/kotlin/org/evomaster/e2etests/spring/openapi/v3/assertions/AssertionEMTest.kt
+++ e2e-tests/spring-rest-openapi-v3/src/test/kotlin/org/evomaster/e2etests/spring/openapi/v3/assertions/AssertionEMTest.kt
@@ -7,8 +7,10 @@ import org.evomaster.e2etests.spring.openapi.v3.SpringTestBase
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertTimeoutPreemptively
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.time.Duration
 
 /**
  *
@@ -31,33 +33,7 @@ class AssertionEMTest : SpringTestBase() {
         val outputFolderName = "AssertionEM"
         val className = ClassName("org.foo.AssertionEMOff")
 
-        runTestHandlingFlakyAndCompilation(
-                outputFolderName,
-                className.fullNameWithDots,
-                1_000
-        ) { args: MutableList<String> ->
-
-            args.add("--enableBasicAssertions")
-            args.add("false")
-
-            val solution = initAndRun(args)
-
-            assertTrue(solution.individuals.size >= 1)
-            assertHasAtLeastOne(solution, HttpVerb.POST, 201, "/api/assertions/data", "OK")
-            assertHasAtLeastOne(solution, HttpVerb.GET, 200, "/api/assertions/data", "{\"a\":42,\"c\":[1000,2000,3000],\"d\":{\"e\":66,\"f\":\"bar\",\"g\":{\"h\":[\"xvalue\",\"yvalue\"]}},\"i\":true,\"l\":false}")
-
-            assertHasAtLeastOne(solution, HttpVerb.GET, 200, "/api/assertions/simpleNumber", "42")
-            assertHasAtLeastOne(solution, HttpVerb.GET, 200, "/api/assertions/simpleString", "simple-string")
-            assertHasAtLeastOne(solution, HttpVerb.GET, 200, "/api/assertions/simpleText", "simple-text")
-
-            assertHasAtLeastOne(solution, HttpVerb.GET, 200, "/api/assertions/simpleArray", "123")
-            assertHasAtLeastOne(solution, HttpVerb.GET, 200, "/api/assertions/arrayObject", "777")
-            assertHasAtLeastOne(solution, HttpVerb.GET, 200, "/api/assertions/arrayEmpty", "[]")
-
-            assertHasAtLeastOne(solution, HttpVerb.GET, 200, "/api/assertions/objectEmpty", "{}")
-        }
-
-
+        testRunEMGeneric(false, className)
 
         val assertion = Files.lines(Paths.get(outputFolderPath(outputFolderName)
                 + "/"
@@ -68,15 +44,63 @@ class AssertionEMTest : SpringTestBase() {
     }
 
     @Test
-    fun testRunEM() {
-        runTestHandlingFlakyAndCompilation(
-                "AssertionEM",
-                "org.foo.AssertionEM",
-                1_000
-        ) { args: MutableList<String> ->
+    fun testRunEMAssertionsOn() {
+        val outputFolderName = "AssertionEM"
+        val className = ClassName("org.foo.AssertionEMOn")
+
+        testRunEMGeneric(true, className)
+
+        val assertion = Files.lines(Paths.get(outputFolderPath(outputFolderName)
+                + "/"
+                + className.getBytecodeName()
+                + ".kt")).anyMatch { l: String -> l.contains(".assertThat()") }
+
+        assertTrue(assertion)
+    }
+
+    @Test
+    fun testRunJavaAssertionsOn(){
+        val outputFolderName = "AssertionEM"
+        val className = ClassName("org.foo.AssertionJavaEMOn")
+
+        testRunEMGeneric(true, className, "JAVA_JUNIT_5")
+
+        val assertion = Files.lines(Paths.get(outputFolderPath(outputFolderName)
+                + "/"
+                + className.getBytecodeName()
+                + ".java")).anyMatch { l: String -> l.contains(".assertThat()") }
+
+        assertTrue(assertion)
+    }
+
+    @Test
+    fun testRunJavaAssertionsOff(){
+        val outputFolderName = "AssertionEM"
+        val className = ClassName("org.foo.AssertionJavaEMOff")
+
+        testRunEMGeneric(false, className, "JAVA_JUNIT_5")
+
+        val assertion = Files.lines(Paths.get(outputFolderPath(outputFolderName)
+                + "/"
+                + className.getBytecodeName()
+                + ".java")).noneMatch { l: String -> l.contains(".assertThat()") }
+
+        assertTrue(assertion)
+    }
+
+    fun testRunEMGeneric(basicAssertions: Boolean, className: ClassName, outputFormat: String? = null){
+        val outputFolderName = "AssertionEM"
+        val iterations = 10_000
+
+        val lambda = { args : MutableList<String> ->
+            args.add("--enableBasicAssertions")
+            args.add(basicAssertions.toString())
+
+            if (outputFormat != null) {
+                args.replaceAll { s -> s.replace("KOTLIN_JUNIT_5", outputFormat) }
+            }
 
             val solution = initAndRun(args)
-
             assertTrue(solution.individuals.size >= 1)
             assertHasAtLeastOne(solution, HttpVerb.POST, 201, "/api/assertions/data", "OK")
             assertHasAtLeastOne(solution, HttpVerb.GET, 200, "/api/assertions/data", "{\"a\":42,\"c\":[1000,2000,3000],\"d\":{\"e\":66,\"f\":\"bar\",\"g\":{\"h\":[\"xvalue\",\"yvalue\"]}},\"i\":true,\"l\":false}")
@@ -92,15 +116,21 @@ class AssertionEMTest : SpringTestBase() {
             assertHasAtLeastOne(solution, HttpVerb.GET, 200, "/api/assertions/objectEmpty", "{}")
         }
 
-        val outputFolderName = "AssertionEM"
-        val className = ClassName("org.foo.AssertionEM")
+        assertTimeoutPreemptively(Duration.ofMinutes(3)) {
+            clearGeneratedFiles(outputFolderName, className)
 
-        val assertion = Files.lines(Paths.get(outputFolderPath(outputFolderName)
-                + "/"
-                + className.getBytecodeName()
-                + ".kt")).anyMatch { l: String -> l.contains(".assertThat()") }
+            handleFlaky {
+                val args: MutableList<String> = getArgsWithCompilation(iterations,
+                        outputFolderName,
+                        className,
+                        true)
 
-        assertTrue(assertion)
+                lambda.invoke(args)
+            }
+
+        }
+
     }
+
 
 }
