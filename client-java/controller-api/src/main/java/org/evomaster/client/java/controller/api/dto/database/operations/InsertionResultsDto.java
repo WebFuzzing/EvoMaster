6104e4c1ca3a2131d4f649083692653680923ce4
Man Zhang <m.zhang1126@gmail.com>
2021-07-01 10:25:20 +0200

    update comments

--- client-java/controller-api/src/main/java/org/evomaster/client/java/controller/api/dto/database/operations/InsertionResultsDto.java
+++ client-java/controller-api/src/main/java/org/evomaster/client/java/controller/api/dto/database/operations/InsertionResultsDto.java
@@ -5,15 +5,20 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * the execution result of {@link DatabaseCommandDto} that performs insertions
+ */
 public class InsertionResultsDto {
 
     /**
-     * primary key generated with the insertions
+     * a map from InsertionDto id to id of auto-generated primary keys
+     * in the database (if any was generated).
      */
     public Map<Long, Long> idMapping = new HashMap<>();
 
     /**
-     * whether the insertion at the index executed successfully
+     * whether the insertion at the index of a sequence of SQL insertions (i.e., {@link DatabaseCommandDto#insertions})
+     * executed successfully
      */
     public List<Boolean> executionResults = new ArrayList<>();
 }
