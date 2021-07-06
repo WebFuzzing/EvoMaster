00f3d5cd07cd866c40f5b254c64634ab1a59cba4
Man Zhang <m.zhang1126@gmail.com>
2021-07-06 17:14:33 +0200

    update like test

--- core/src/test/kotlin/org/evomaster/core/database/extract/mysql/LikeCheckTest.kt
+++ core/src/test/kotlin/org/evomaster/core/database/extract/mysql/LikeCheckTest.kt
@@ -1,9 +1,13 @@
 package org.evomaster.core.database.extract.mysql
 
+import org.evomaster.client.java.controller.db.SqlScriptRunner
 import org.evomaster.client.java.controller.internal.db.SchemaExtractor
+import org.evomaster.core.database.DbActionTransformer
 import org.evomaster.core.database.SqlInsertBuilder
 import org.evomaster.core.search.gene.regex.RegexGene
-import org.junit.jupiter.api.Assertions
+import org.evomaster.core.search.service.Randomness
+import org.jetbrains.kotlin.utils.addToStdlib.firstIsInstance
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class LikeCheckTest : ExtractTestBaseMySQL() {
@@ -18,8 +22,29 @@ class LikeCheckTest : ExtractTestBaseMySQL() {
         val actions = builder.createSqlInsertionAction("x", setOf("f_id"))
         val genes = actions[0].seeGenes()
 
-        Assertions.assertEquals(1, genes.size)
-        Assertions.assertTrue(genes[0] is RegexGene)
+        assertEquals(1, genes.size)
+        assertTrue(genes[0] is RegexGene)
+    }
+
+    @Test
+    fun testInsertRegexGene() {
+        val randomness = Randomness()
+        val schema = SchemaExtractor.extract(connection)
+
+        val builder = SqlInsertBuilder(schema)
+        val actions = builder.createSqlInsertionAction("x", setOf("f_id"))
+
+        val genes = actions[0].seeGenes()
+        val regexGene = genes.firstIsInstance<RegexGene>()
+
+        for (i in 1..100) {
+            regexGene.randomize(randomness, false, listOf())
+
+
+            val dbCommandDto = DbActionTransformer.transform(actions)
+            SqlScriptRunner.execInsert(connection, dbCommandDto.insertions)
+
+        }
     }
 
 }
