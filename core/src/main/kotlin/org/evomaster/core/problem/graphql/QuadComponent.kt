9148b84a7912e13381eeba977d9ee7e40dc4fba2
asmab <asma.belhadi.17@gmail.com>
2021-08-18 11:16:36 +0200

    Add new class to define a Quadruple.

---
+++ core/src/main/kotlin/org/evomaster/core/problem/graphql/QuadComponent.kt
@@ -0,0 +1,8 @@
+package org.evomaster.core.problem.graphql
+
+import org.evomaster.core.problem.graphql.schema.__TypeKind
+
+/**
+ *  A data class used to define a Quadruple
+ */
+data class QuadComponent(val kind0: __TypeKind?, val kind1: __TypeKind?, val kind2: __TypeKind?, val kind3: __TypeKind?)
