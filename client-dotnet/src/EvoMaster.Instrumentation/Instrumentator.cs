863dd32d0f849772d73a739dc8e95c76e42e3755
Amid Gol <golmohammadi.edu@gmail.com>
2021-08-20 14:02:27 +0430

    Add instrumentation capability and move the instrumented file to a separate directory

---
+++ client-dotnet/src/EvoMaster.Instrumentation/Instrumentator.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using Mono.Cecil.Rocks;
+
+namespace EvoMaster.Instrumentation
+{
+    public class Instrumentator
+    {
+        private MethodReference _probe;
+
+        public void Instrument(string assembly, string destination)
+        {
+            if (string.IsNullOrEmpty(assembly)) throw new ArgumentNullException(assembly);
+            if (string.IsNullOrEmpty(destination)) throw new ArgumentNullException(destination);
+
+            var module =
+                ModuleDefinition.ReadModule(assembly, new ReaderParameters { ReadSymbols = true });
+
+            _probe =
+                module.ImportReference(
+                    typeof(Instrumentator).GetMethod(name: "CompletedLine",
+                        types: new[] { typeof(int) }));
+
+            foreach (var type in module.Types)
+            {
+                if (type.Name == "<Module>") continue;
+
+                Console.WriteLine($"* Type.Name: {type.Name}");
+
+                foreach (var method in type.Methods)
+                {
+                    Console.WriteLine($"\t Method.Name: {method.Name} HasBody: {method.HasBody}");
+
+                    if (!method.HasBody) continue;
+
+                    Console.WriteLine("Printing instructions before change...");
+                    foreach (var ins in method.Body.Instructions)
+                    {
+                        Console.WriteLine($"* {ins}");
+                    }
+
+                    Console.WriteLine("######################");
+
+                    var ilProcessor = method.Body.GetILProcessor();
+
+                    var mapping = method.DebugInformation.GetSequencePointMapping();
+
+                    var lastReachedLine = 0;
+                    var alreadyCompletedLines = new List<int>();
+                    method.Body.SimplifyMacros(); //This is to prevent overflow of short branch opcodes
+
+                    for (var i = 0; i < int.MaxValue; i++)
+                    {
+                        Instruction instruction;
+                        try
+                        {
+                            instruction = method.Body.Instructions[i];
+                        }
+                        catch (ArgumentOutOfRangeException)
+                        {
+                            break;
+                        }
+
+                        mapping.TryGetValue(instruction, out var sequencePoint);
+
+                        if ((sequencePoint == null || sequencePoint.IsHidden) && instruction.OpCode != OpCodes.Ret)
+                            continue;
+
+                        if (sequencePoint != null && sequencePoint.StartLine == lastReachedLine)
+                            continue;
+
+                        if (lastReachedLine != 0)
+                        {
+                            //This is to prevent insertion of completed probe after branch opcode
+                            //Checking alreadyCompletedLines is in order to calling Completed probe in loops two times...
+                            //However I'm not sure this will work in all cases, if it didn't work, we can try branchInstruction.Operand.Next
+                            if (IsBranchInstruction(instruction.Previous) &&
+                                !alreadyCompletedLines.Contains(lastReachedLine))
+                            {
+                                i = InsertVisitLineProbe(instruction.Previous, ilProcessor, i, type.Name, method.Name,
+                                    lastReachedLine);
+                            }
+                            else
+                            {
+                                i = InsertVisitLineProbe(instruction, ilProcessor, i, type.Name, method.Name,
+                                    lastReachedLine);
+
+                                //To cover cases when ret has line number
+                                if (instruction.OpCode == OpCodes.Ret)
+                                {
+                                    if (sequencePoint != null)
+                                    {
+                                        i = InsertVisitLineProbe(instruction, ilProcessor, i, type.Name,
+                                            method.Name,
+                                            sequencePoint.EndLine);
+                                    }
+                                }
+                            }
+
+                            alreadyCompletedLines.Add(lastReachedLine);
+                        }
+
+                        if (sequencePoint != null && !sequencePoint.IsHidden)
+                            lastReachedLine = sequencePoint.StartLine;
+                    }
+
+                    method.Body.OptimizeMacros(); //Change back Br opcodes to Br.s if possible
+
+                    Console.WriteLine("Printing instructions after change...");
+
+                    foreach (var instruction in method.Body.Instructions)
+                    {
+                        mapping.TryGetValue(instruction, out var seqPoint);
+
+                        if ((seqPoint != null && !seqPoint.IsHidden))
+                        {
+                            Console.WriteLine($"* {instruction} --- Line {seqPoint.StartLine}");
+                        }
+                        else if (instruction.OpCode == OpCodes.Ret)
+                        {
+                            Console.WriteLine(seqPoint == null
+                                ? $"* {instruction} --- seqPoint is null"
+                                : $"* {instruction} --- IsHidden {seqPoint.IsHidden}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"* {instruction}");
+                        }
+                    }
+                }
+            }
+
+            if (destination.Length > 1 && destination[^1] == '/')
+            {
+                destination = destination.Remove(destination.Length - 1, 1);
+            }
+
+            module.Write($"{destination}/InstrumentedSut.dll");
+        }
+
+        private int InsertVisitLineProbe(Instruction instruction, ILProcessor ilProcessor,
+            int byteCodeIndex, string className, string methodName, int line)
+        {
+            var lineNumberInstruction = ilProcessor.Create(OpCodes.Ldc_I4, line);
+            var visitedInstruction = ilProcessor.Create(OpCodes.Call, _probe);
+
+            ilProcessor.InsertBefore(instruction, lineNumberInstruction);
+            byteCodeIndex++;
+            ilProcessor.InsertBefore(instruction, visitedInstruction);
+            byteCodeIndex++;
+
+            return byteCodeIndex;
+        }
+
+        private static bool IsBranchInstruction(Instruction instruction) =>
+            instruction.OpCode.ToString().ToLower()[0].Equals('b') && instruction.OpCode != OpCodes.Break &&
+            instruction.OpCode != OpCodes.Box;
+
+        public static void CompletedLine(int lineNo)
+        {
+            var record = $"--- Completed Line \"{lineNo}\"";
+            Console.WriteLine(record);
+        }
+    }
+}
---
+++ client-dotnet/src/EvoMaster.Instrumentation/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace EvoMaster.Instrumentation
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var instrumentator = new Instrumentator();
+            instrumentator.Instrument(args[0], args[1]);
+        }
+    }
+}
