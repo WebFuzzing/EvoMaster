8be9c215ba10336ab0d358eae69db5893235e95d
arcuri82 <arcuri82@gmail.com>
2021-09-08 14:23:18 +0200

    hotfix for dependency update

--- e2e-tests/spring-rest-openapi-v3/src/main/kotlin/com/foo/rest/examples/spring/openapi/v3/cookielogin/CookieLoginRest.kt
+++ e2e-tests/spring-rest-openapi-v3/src/main/kotlin/com/foo/rest/examples/spring/openapi/v3/cookielogin/CookieLoginRest.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*
 import javax.servlet.http.Cookie
 import javax.servlet.http.HttpServletResponse
 
-@Controller
+@RestController
 @RequestMapping(path = ["/api/logintoken"])
 class CookieLoginRest {
 
