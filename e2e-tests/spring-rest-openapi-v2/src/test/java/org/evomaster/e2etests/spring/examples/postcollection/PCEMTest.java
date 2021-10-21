fb27c98ccc723cf7e19c5726196d5e7678f430dc
ZhangMan <m.zhang1126@gmail.com>
2021-10-21 14:06:26 +0200

    add comments for failing PCEMTest.java

--- e2e-tests/spring-rest-openapi-v2/src/test/java/org/evomaster/e2etests/spring/examples/postcollection/PCEMTest.java
+++ e2e-tests/spring-rest-openapi-v2/src/test/java/org/evomaster/e2etests/spring/examples/postcollection/PCEMTest.java
@@ -22,13 +22,23 @@ public class PCEMTest extends SpringTestBase {
     @Test
     public void testRunEM() throws Throwable {
 
-        defaultSeed = 45;
+        /*
+            NOTE THAT
+            default resource-based solution would have side effect on creating multiple resources in one test
+            before size of resource is handled, we employ the solution without resource handling.
+         */
 
         runTestHandlingFlakyAndCompilation(
                 "PcEM",
                 "org.bar.PcEM",
                 1_000,
                 (args) -> {
+
+                    args.add("--probOfSmartSampling");
+                    args.add("0.5");
+                    args.add("--resourceSampleStrategy");
+                    args.add("NONE");
+
 
                     Solution<RestIndividual> solution = initAndRun(args);
 
