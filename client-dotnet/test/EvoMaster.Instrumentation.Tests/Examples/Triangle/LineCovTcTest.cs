267c856e01824b57b4ee627866a511ef5fa8c117
Amid Gol <golmohammadi.edu@gmail.com>
2021-11-10 15:53:44 +0100

    Remove test for invalid results and add test for last line coverage in LiveCovTcTest

--- client-dotnet/test/EvoMaster.Instrumentation.Tests/Examples/Triangle/LineCovTcTest.cs
+++ client-dotnet/test/EvoMaster.Instrumentation.Tests/Examples/Triangle/LineCovTcTest.cs
@@ -9,13 +9,13 @@ namespace EvoMaster.Instrumentation.Tests.Examples.Triangle {
         [Fact]
         public void TestLineCoverage() {
             ITriangleClassification tc = new TriangleClassificationImpl();
-        
+
             ExecutionTracer.Reset();
-        
+
             Assert.Equal(0, ExecutionTracer.GetNumberOfObjectives());
-        
+
             tc.Classify(-1, 0, 0);
-        
+
             var a = ExecutionTracer.GetNumberOfObjectives();
             //at least one line should have been covered
             Assert.True(a > 0);
@@ -38,42 +38,28 @@ namespace EvoMaster.Instrumentation.Tests.Examples.Triangle {
 
             tc.Classify(a, b, c);
 
-            //assert that the first line of the method is reached
-            Assert.Contains("Line_at_TriangleClassificationImpl_00005", ObjectiveRecorder.AllTargets);
-
             Assert.Contains(returnLine, ObjectiveRecorder.AllTargets);
+        }
+
+        [Theory]
+        [InlineData(-1, 0, 0)]
+        [InlineData(6, 6, 6)]
+        [InlineData(10, 6, 3)]
+        [InlineData(7, 6, 7)]
+        [InlineData(7, 6, 5)]
+        public void TestLastLineCoverage(int a, int b, int c) {
+            ITriangleClassification tc = new TriangleClassificationImpl();
+
+            ExecutionTracer.Reset();
+            ObjectiveRecorder.Reset(true);
+
+            Assert.Equal(0, ExecutionTracer.GetNumberOfObjectives());
+            Assert.Empty(ObjectiveRecorder.AllTargets);
+
+            tc.Classify(a, b, c);
 
             //assert that the last line of the method is reached
             Assert.Contains("Line_at_TriangleClassificationImpl_00027", ObjectiveRecorder.AllTargets);
-        }
-
-        /// <summary>
-        /// This method checks not only if at least one line has been covered, but also if the valid input does not lead to invalid output.
-        /// In other words, we are checking if the SUT is not malfunctioning after being instrumented.
-        /// </summary>
-        [Theory]
-        //for example in the case below, line 33 should not be covered
-        [InlineData(-1, 0, 0, "Line_at_TriangleClassificationImpl_00026")]
-        [InlineData(6, 6, 6, "Line_at_TriangleClassificationImpl_00007")]
-        [InlineData(10, 6, 3, "Line_at_TriangleClassificationImpl_00011")]
-        [InlineData(7, 6, 7, "Line_at_TriangleClassificationImpl_00019")]
-        [InlineData(7, 6, 5, "Line_at_TriangleClassificationImpl_00023")]
-        public void TestInvalidLineCoverage(int a, int b, int c, string returnLine) {
-            ITriangleClassification tc = new TriangleClassificationImpl();
-        
-            ExecutionTracer.Reset();
-            ObjectiveRecorder.Reset(true);
-        
-            Assert.Equal(0, ExecutionTracer.GetNumberOfObjectives());
-            Assert.Empty(ObjectiveRecorder.AllTargets);
-        
-            tc.Classify(a, b, c);
-        
-            //To check if at least one line is covered or not
-            Assert.True(ExecutionTracer.GetNumberOfObjectives() > 0);
-        
-            //To make sure if the valid input is not giving any unexpected result
-            Assert.DoesNotContain(returnLine, ObjectiveRecorder.AllTargets);
         }
     }
 }
