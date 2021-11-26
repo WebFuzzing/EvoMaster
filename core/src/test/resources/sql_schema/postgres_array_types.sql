1133144183578e243bbd59c036a2c71b8a42c957
Juan Pablo Galeotti <jgaleotti@gmail.com>
2021-11-26 18:05:03 -0300

    removing unsupported tests

--- core/src/test/resources/sql_schema/postgres_array_types.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE ArrayTypes (
-           arrayColumn integer[] NOT NULL,
-           matrixColumn integer[][] NOT NULL,
-           spaceColumn integer[][][] NOT NULL,
-           exactSizeArrayColumn integer[3] NOT NULL,
-           exactSizeMatrixColumn integer[3][3] NOT NULL
-);
-
