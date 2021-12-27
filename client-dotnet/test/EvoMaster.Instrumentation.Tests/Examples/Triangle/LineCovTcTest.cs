1660da8e3b466c083b766d20ed1c64353360a5ab
Amid Gol <golmohammadi.edu@gmail.com>
2021-12-27 20:35:40 +0100

    Add unit tests to check if registering targets in the json works fine

--- client-dotnet/test/EvoMaster.Instrumentation.Tests/Examples/Triangle/LineCovTcTest.cs
+++ client-dotnet/test/EvoMaster.Instrumentation.Tests/Examples/Triangle/LineCovTcTest.cs
@@ -1,11 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using EvoMaster.Instrumentation.Examples.Triangle;
 using EvoMaster.Instrumentation.StaticState;
+using EvoMaster.Instrumentation_Shared;
 using Xunit;
 
-namespace EvoMaster.Instrumentation.Tests.Examples.Triangle {
-    public class LineCovTcTest {
+namespace EvoMaster.Instrumentation.Tests.Examples.Triangle
+{
+    public class LineCovTcTest
+    {
+        //TODO: a test method to check whether the extra file contains every line
+
         [Fact]
-        public void TestLineCoverage() {
+        public void TestLineCoverage()
+        {
             ITriangleClassification tc = new TriangleClassificationImpl();
 
             ExecutionTracer.Reset();
@@ -25,7 +35,8 @@ namespace EvoMaster.Instrumentation.Tests.Examples.Triangle {
         [InlineData(10, 6, 3, "Line_at_TriangleClassificationImpl_00019")]
         [InlineData(7, 6, 7, "Line_at_TriangleClassificationImpl_00023")]
         [InlineData(7, 6, 5, "Line_at_TriangleClassificationImpl_00026")]
-        public void TestSpecificLineCoverage(int a, int b, int c, string returnLine) {
+        public void TestSpecificLineCoverage(int a, int b, int c, string returnLine)
+        {
             ITriangleClassification tc = new TriangleClassificationImpl();
 
             ExecutionTracer.Reset();
@@ -45,7 +56,8 @@ namespace EvoMaster.Instrumentation.Tests.Examples.Triangle {
         [InlineData(10, 6, 3)]
         [InlineData(7, 6, 7)]
         [InlineData(7, 6, 5)]
-        public void TestLastLineCoverage(int a, int b, int c) {
+        public void TestLastLineCoverage(int a, int b, int c)
+        {
             ITriangleClassification tc = new TriangleClassificationImpl();
 
             ExecutionTracer.Reset();
@@ -58,6 +70,51 @@ namespace EvoMaster.Instrumentation.Tests.Examples.Triangle {
 
             //assert that the last line of the method is reached
             Assert.Contains("Line_at_TriangleClassificationImpl_00027", ObjectiveRecorder.AllTargets);
+        }
+
+        [Fact]
+        public void TestAllLinesGettingRegistered()
+        {
+            var expectedLineNumbers = new List<int>
+            {
+                5, 6, 7, 10, 11, 14, 16, 18, 19, 22, 23, 26, 27
+            };
+
+            var expectedLines = new List<string>();
+            expectedLineNumbers.ForEach(x =>
+                expectedLines.Add(ObjectiveNaming.LineObjectiveName("TriangleClassificationImpl", x)));
+            
+            var targets = GetRegisteredTargets();
+
+            Assert.Equal(expectedLines, targets.Lines);
+        }
+
+        [Fact]
+        public void TestAllClassesGettingRegistered()
+        {
+            var expectedclassNames = new List<string>
+            {
+                "TriangleClassificationImpl"
+            };
+
+            var expectedClasses = new List<string>();
+
+            expectedclassNames.ForEach(x=>expectedClasses.Add(ObjectiveNaming.ClassObjectiveName(x)));
+            
+            var targets = GetRegisteredTargets();
+
+            Assert.Equal(expectedClasses, targets.Classes);
+        }
+
+        private RegisteredTargets GetRegisteredTargets()
+        {
+            var bin = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            if (bin == null) throw new Exception("Executing directory not found");
+
+            var json = File.ReadAllText(Path.Combine(bin, "Targets.json"));
+
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<RegisteredTargets>(json);
         }
     }
 }
