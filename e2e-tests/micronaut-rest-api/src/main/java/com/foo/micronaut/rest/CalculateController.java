54555278e0e0981d34bde0f13bac7f2310d611b4
seran <7030273+seran@users.noreply.github.com>
2022-01-06 15:46:02 +0100

    bug fixes

--- e2e-tests/micronaut-rest-api/src/main/java/com/foo/micronaut/rest/CalculateController.java
+++ e2e-tests/micronaut-rest-api/src/main/java/com/foo/micronaut/rest/CalculateController.java
@@ -4,15 +4,14 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 
-import javax.validation.constraints.NotNull;
+import javax.annotation.Nullable;
 import javax.validation.constraints.PositiveOrZero;
 
 @Controller("/calculate")
 public class CalculateController {
 
     @Get(value = "{?x,y}", produces = MediaType.TEXT_PLAIN)
-    public String index(@NotNull @PositiveOrZero Integer x, @NotNull @PositiveOrZero Integer y) {
-        Integer z = x + y;
-        return z.toString();
+    public int index(@Nullable @PositiveOrZero Integer x, @Nullable @PositiveOrZero Integer y) {
+        return ( x != null && y != null) ? x + y : 0;
     }
 }
