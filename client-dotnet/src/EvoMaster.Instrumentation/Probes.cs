27edececb5b33bcc323d36db0cc9219ea8b2cf2a
Amid Gol <golmohammadi.edu@gmail.com>
2022-01-08 00:47:14 +0100

    Remove printing logs inside the probes

--- client-dotnet/src/EvoMaster.Instrumentation/Probes.cs
+++ client-dotnet/src/EvoMaster.Instrumentation/Probes.cs
@@ -46,12 +46,10 @@ namespace EvoMaster.Instrumentation {
 
         //This method is called by the probe inserted after each covered statement in the instrumented SUT
         public static void CompletedStatement(string className, int lineNo, int columnNo) {
-            SimpleLogger.Info($"*** completed {className}: {lineNo}, {columnNo}");
             ExecutionTracer.CompletedStatement(className, lineNo, columnNo);
         }
 
         public static void EnteringStatement(string className, int lineNo, int columnNo) {
-            SimpleLogger.Info($"### entering {className}: {lineNo}, {columnNo}");
             ExecutionTracer.EnteringStatement(className, lineNo, columnNo);
         }
     }
