efc9aacbde775960229cf29d65fa2a471056b76b
seran <7030273+seran@users.noreply.github.com>
2022-01-10 13:29:23 +0100

    minor changes in test case

--- e2e-tests/micronaut-rest-api/src/test/java/org/evomaster/e2etests/micronaut/rest/MicronautTest.java
+++ e2e-tests/micronaut-rest-api/src/test/java/org/evomaster/e2etests/micronaut/rest/MicronautTest.java
@@ -1,17 +1,22 @@
 package org.evomaster.e2etests.micronaut.rest;
 
 import com.foo.micronaut.rest.MicronautTestController;
+import io.micronaut.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpHeaderValues;
-import org.evomaster.core.Main;
+//import org.evomaster.core.Main;
 import org.evomaster.core.problem.rest.HttpVerb;
+import org.evomaster.core.problem.rest.RestCallAction;
+import org.evomaster.core.problem.rest.RestCallResult;
 import org.evomaster.core.problem.rest.RestIndividual;
+import org.evomaster.core.search.ActionResult;
 import org.evomaster.core.search.Solution;
 import org.evomaster.e2etests.utils.RestTestBase;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MicronautTest extends RestTestBase {
 
@@ -22,22 +27,18 @@ public class MicronautTest extends RestTestBase {
 
     @Test
     public void testRunEM() throws Throwable {
-        handleFlaky(() -> {
-            String[] args = new String[] {
-                    "--createTests", "false",
-                    "--seed", "" + defaultSeed++,
-                    "--sutControllerPort", "" + controllerPort,
-                    "--maxActionEvaluations", "500",
-                    "--stoppingCriterion", "FITNESS_EVALUATIONS"
-            };
 
-            Solution<RestIndividual> solution = (Solution<RestIndividual>) Main.initAndRun(args);
+        runTestHandlingFlaky("MicronautTest", "com.foo.MicronautTest", 1100, false, (args) -> {
+            args.add("--killSwitch");
+            args.add("false");
+
+            Solution<RestIndividual> solution = initAndRun(args);
 
             assertTrue(solution.getIndividuals().size() >= 1);
-
             assertHasAtLeastOne(solution, HttpVerb.GET, 500);
             assertHasAtLeastOne(solution, HttpVerb.POST, 200);
-            assertEquals(HttpHeaderValues.KEEP_ALIVE, HttpHeaderValues.KEEP_ALIVE);
-        });
+            // this is not completed yet, below one is a experiment
+            assertHasAtLeastOne(solution, HttpVerb.GET, 500, "/", "Crashed");
+        } );
     }
 }
