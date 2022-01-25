1d799c4161a8187137083cb4ac82986b872949b1
Amid Gol <golmohammadi.edu@gmail.com>
2022-01-25 20:02:35 +0100

    Put the test for instrumentation under the same collection

--- client-dotnet/test/EvoMaster.Instrumentation.Tests/Examples/Triangle/LineCovTcTest.cs
+++ client-dotnet/test/EvoMaster.Instrumentation.Tests/Examples/Triangle/LineCovTcTest.cs
@@ -8,6 +8,7 @@ using Xunit;
 using Xunit.Abstractions;
 
 namespace EvoMaster.Instrumentation.Tests.Examples.Triangle {
+    [Collection("Sequential")]
     public class LineCovTcTest : CovTcTestBase {
         private readonly ITestOutputHelper _testOutputHelper;
 
@@ -142,85 +143,6 @@ namespace EvoMaster.Instrumentation.Tests.Examples.Triangle {
             var res = tc.Classify(a, b, c);
 
             Assert.Equal(expectedOutcome, res);
-        }
-        
-         private readonly NumericOperations _numericOperations = new NumericOperations();
-
-        [Theory]
-        [InlineData(8, 7, true)]
-        [InlineData(4, 4400, false)]
-        [InlineData(99, 99, false)]
-        public void TestGreaterThanForDouble(double a, double b, bool expectedResult) {
-            var actualResult = _numericOperations.GreaterThan(a, b);
-
-            Assert.Equal(expectedResult, actualResult);
-        }
-
-        [Theory]
-        [InlineData(8, 7, true)]
-        [InlineData(4, 4400, false)]
-        [InlineData(99, 99, false)]
-        public void TestGreaterThanForFloat(float a, float b, bool expectedResult) {
-            var actualResult = _numericOperations.GreaterThan(a, b);
-
-            Assert.Equal(expectedResult, actualResult);
-        }
-
-        [Theory]
-        [InlineData(8, 7, true)]
-        [InlineData(4, 4400, false)]
-        [InlineData(9, 9, false)]
-        public void TestGreaterThanForLong(long a, long b, bool expectedResult) {
-            var actualResult = _numericOperations.GreaterThan(a, b);
-
-            Assert.Equal(expectedResult, actualResult);
-        }
-
-        [Theory]
-        [InlineData(8, 7, true)]
-        [InlineData(4, 4400, false)]
-        [InlineData(9, 9, false)]
-        public void TestGreaterThanForShort(short a, short b, bool expectedResult) {
-            var actualResult = _numericOperations.GreaterThan(a, b);
-
-            Assert.Equal(expectedResult, actualResult);
-        }
-
-        [Theory]
-        [InlineData(10, 1)]
-        [InlineData(7, 0)]
-        [InlineData(2, -1)]
-        public void TestCompareWithLocalVariable(int a, int expectedResult) {
-            var actualResult = _numericOperations.CompareWithLocalVariable(a);
-
-            Assert.Equal(expectedResult, actualResult);
-        }
-
-        [Theory]
-        [InlineData(10, 1)]
-        [InlineData(7, 0)]
-        [InlineData(2, -1)]
-        public void TestCompareWithGlobalVariable(int a, int expectedResult) {
-            var actualResult = _numericOperations.CompareWithGlobalVariable(a);
-
-            Assert.Equal(expectedResult, actualResult);
-        }
-
-        [Theory]
-        [InlineData(10, 1)]
-        [InlineData(7, 0)]
-        [InlineData(2, -1)]
-        public void TestCompareWithStaticVariable(int a, int expectedResult) {
-            var actualResult = _numericOperations.CompareWithStaticVariable(a);
-
-            Assert.Equal(expectedResult, actualResult);
-        }
-
-        [Fact]
-        public void TestCompareTwoGlobalVariables() {
-            var actualResult = _numericOperations.CompareTwoGlobalVariables();
-
-            Assert.Equal(1, actualResult);
         }
     }
 }
