81d44fe1a6e4dbff0ad2fc73901d3dbe8b8e51b0
Man Zhang <man.mzhang@gmail.com>
2022-01-26 12:58:34 +0100

    add branches example for branch distance tests

---
+++ client-dotnet/test/EvoMaster.Instrumentation.Examples/Branches/BranchesImp.cs
@@ -0,0 +1,39 @@
+namespace EvoMaster.Instrumentation.Examples.Branches{
+    public class BranchesImp : IBranches{
+        public int Pos(int x, int y){
+            if(x>0){
+                return 0;
+            }
+
+            if(y >= 0){
+                return 1;
+            }
+
+            return 2;
+        }
+
+        public int Neg(int x, int y){
+            if(x<0){
+                return 3;
+            }
+
+            if(y <= 0){
+                return 4;
+            }
+
+            return 5;
+        }
+
+        public int Eq(int x, int y){
+            if(x==0) {
+                return 6;
+            }
+
+            if(y!=0) {
+                return 7;
+            }
+
+            return 8;
+        }
+    }
+}
