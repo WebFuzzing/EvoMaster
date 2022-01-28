ad52d2b58ae0fc2d9e115378804bc56a9a9e4166
Man Zhang <man.mzhang@gmail.com>
2022-01-28 12:34:26 +0100

    add comments

--- client-dotnet/src/EvoMaster.Instrumentation/Heuristic/HeuristicsForNonintegerComparisons.cs
+++ client-dotnet/src/EvoMaster.Instrumentation/Heuristic/HeuristicsForNonintegerComparisons.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using Mono.Cecil.Cil;
 
 namespace EvoMaster.Instrumentation.Heuristic{
     public class HeuristicsForNonintegerComparisons{
         
+        /*
+         * a set of (NonintegerComparisons) opcode which could be handed for branch distance 
+         */
         public static readonly ISet<Code> CODES = new HashSet<Code>(){
             //0xFE 0x01	ceq	Push 1 (of type int32) if value1 equals value2, else push 0.
             //https://docs.microsoft.com/en-us/dotnet/api/system.reflection.emit.opcodes.ceq?view=net-6.0
@@ -24,10 +26,16 @@ namespace EvoMaster.Instrumentation.Heuristic{
             Code.Cgt_Un
         };
 
+        /**
+         * a set of opCode which work with unsigned and unchecked values
+         */
         public static readonly ISet<Code> UNSIGNED = new HashSet<Code>(){
             Code.Clt_Un, Code.Cgt_Un
         };
 
+        /**
+         * compute distance for floating point value
+         */
         public static Truthness GetForFloatAndDoubleComparison(double firstValue, double secondValue, Code opcode){
             
             switch(opcode){
@@ -45,6 +53,9 @@ namespace EvoMaster.Instrumentation.Heuristic{
             }
         }
         
+        /**
+         * compute distance for int value, eg, long, int, short
+         */
         public static Truthness GetForLongComparison(long firstValue, long secondValue, Code opcode){
             
             switch(opcode){
@@ -62,6 +73,9 @@ namespace EvoMaster.Instrumentation.Heuristic{
             }
         }
         
+        /**
+         * compute distance for ulong which is not assignable to long
+         */
         public static Truthness GetForULongComparison(ulong firstValue, ulong secondValue, Code opcode){
             
             switch(opcode){
