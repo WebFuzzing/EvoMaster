8ff4fb0ca1d374e867e0f05b54137664791a31c5
Amid Gol <golmohammadi.edu@gmail.com>
2022-02-01 00:36:29 +0100

    Add StringEquals sut and its driver and tests for it

---
+++ e2e-tests/dotnet-rest/src/RestApis.StringEquals/Controllers/EqualsController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestApis.StringEquals.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class EqualsController : ControllerBase {
+        [HttpGet("getConstant/{value}")]
+        public IActionResult GetConstant(string value) => Ok(value);
+    }
+}
