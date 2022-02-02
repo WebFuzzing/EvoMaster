7af8b685e270161a55d29daaee41907e70f254fa
arcuri82 <arcuri82@gmail.com>
2022-02-02 10:59:33 +0100

    fixed script

--- scripts/schedule.py
+++ scripts/schedule.py
@@ -1,31 +1,39 @@
 #!/usr/bin/env python
 
+# When we generate an experiment folder (FOLDER) with exp.py script on local machine,
+# with K generated bash scripts, we can use this schedule.py to run all these scripts
+# with N of them in parallel. The others (assuming N << K) will be started as soon as
+# one current running job is completed
+
 import random
 import sys
 import os
 import subprocess
 import time
+import platform
 
 if len(sys.argv) != 3:
-    print("Usage:\nschedule.py <N> <FOLDER> ")
+    print("Usage:\nschedule.py <N> <FOLDER>")
     exit(1)
 
-
+# The number of jobs to run in parallel
 N = int(sys.argv[1])
 
-if(N < 1)
+if N < 1:
     print("Invalid value for N: " + str(N))
     exit(1)
 
+# Location of experiment folder
 FOLDER = sys.argv[2]
 
-# SCRIPT_LOCATION = os.path.dirname(os.path.realpath(__file__))
-# SHELL = platform.system() == 'Windows'
+SHELL = platform.system() == 'Windows'
 
+SCRIPTS_FOLDER = os.path.join(FOLDER, "scripts")
 
 buffer = []
 
-scripts = [f for f in os.listdir(FOLDER) if os.path.isfile(os.path.join(FOLDER, f)) and f.startswith("evomaster") and f.endswith(".sh")]
+#collect name of all bash files
+scripts = [f for f in os.listdir(SCRIPTS_FOLDER) if os.path.isfile(os.path.join(SCRIPTS_FOLDER, f)) and f.startswith("evomaster") and f.endswith(".sh")]
 
 print("There are " + str(len(scripts)) + " EvoMaster script files")
 
@@ -34,10 +42,13 @@ random.shuffle(scripts)
 k = 1
 
 def runScript(s):
+    global k
     print("Running script " + str(k)+ "/"+ str(len(scripts)) +": " + s)
-    nonlocal k
     k = k + 1
-    handler = subprocess.Popen(os.path.join(FOLDER, s))
+
+    command = ["bash", os.path.join("scripts", s)]
+
+    handler = subprocess.Popen(command, shell=SHELL, cwd=FOLDER)
     buffer.append(handler)
 
 for s in scripts:
@@ -47,6 +58,10 @@ for s in scripts:
         while len(buffer) == N:
             for h in buffer:
                 h.poll()
+                if h.returncode is not None and h.returncode != 0:
+                    print("Process terminated with code: " + str(h.returncode))
+
+            # keep the ones running... those have return code not set yet
             buffer = [h for h in buffer if h.returncode is None]
             if len(buffer) == N :
                 time.sleep(5)
@@ -58,6 +73,8 @@ print("Waiting for last scripts to end")
 
 for h in buffer:
     h.wait()
+    if h.returncode != 0:
+        print("Process terminated with code: " + str(h.returncode))
 
 print("All jobs are completed")
 
