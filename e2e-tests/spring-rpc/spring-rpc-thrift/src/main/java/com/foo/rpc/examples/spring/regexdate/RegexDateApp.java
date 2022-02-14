9c3aa549b1f5eff15ee04e68c16fa091de347bfd
Man Zhang <man.mzhang@gmail.com>
2022-02-14 23:14:51 +0100

    add regexDate e2e

---
+++ e2e-tests/spring-rpc/spring-rpc-thrift/src/main/java/com/foo/rpc/examples/spring/regexdate/RegexDateApp.java
@@ -0,0 +1,31 @@
+package com.foo.rpc.examples.spring.regexdate;
+
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocolFactory;
+import org.apache.thrift.server.TServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class RegexDateApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegexDateApp.class, args);
+    }
+
+    @Bean
+    public TProtocolFactory tProtocolFactory() {
+        return new TBinaryProtocol.Factory();
+    }
+
+    @Bean
+    public ServletRegistrationBean dbBaseServlet(TProtocolFactory protocolFactory, RegexDateServiceImp service) {
+        TServlet tServlet =  new TServlet(new RegexDateService.Processor<>(service), protocolFactory);
+        return new ServletRegistrationBean(tServlet, "/taint");
+    }
+}
---
+++ e2e-tests/spring-rpc/spring-rpc-thrift/src/test/java/com/foo/rpc/examples/spring/regexdate/RegexDateController.java
@@ -0,0 +1,43 @@
+package com.foo.rpc.examples.spring.regexdate;
+
+import com.foo.rpc.examples.spring.SpringController;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.THttpClient;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
+import org.evomaster.client.java.controller.problem.ProblemInfo;
+import org.evomaster.client.java.controller.problem.RPCProblem;
+
+import java.util.HashMap;
+
+public class RegexDateController extends SpringController {
+
+    private RegexDateService.Client client;
+
+    public RegexDateController(){
+        super(RegexDateApp.class);
+    }
+
+    @Override
+    public ProblemInfo getProblemInfo() {
+        return new RPCProblem(new HashMap<String, Object>() {{
+            put(RegexDateService.Iface.class.getName(), client);
+        }});
+    }
+
+    @Override
+    public String startClient() {
+        String url = "http://localhost:"+getSutPort()+"/taint";
+        try {
+            // init client
+            TTransport transport = new THttpClient(url);
+            TProtocol protocol = new TBinaryProtocol(transport);
+            client = new RegexDateService.Client(protocol);
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+}
