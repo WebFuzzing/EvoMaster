49c03ec36624a7a15b3ffd04cbb33f876e087e87
Man Zhang <man.mzhang@gmail.com>
2022-04-20 22:07:49 +0200

    add impacts for BigIntegerGene and BigDecimalGene

---
+++ core/src/main/kotlin/org/evomaster/core/search/impact/impactinfocollection/value/numeric/BigDecimalGeneImpact.kt
@@ -0,0 +1,27 @@
+package org.evomaster.core.search.impact.impactinfocollection.value.numeric
+
+import org.evomaster.core.search.gene.BigDecimalGene
+import org.evomaster.core.search.gene.Gene
+import org.evomaster.core.search.impact.impactinfocollection.GeneImpact
+import org.evomaster.core.search.impact.impactinfocollection.SharedImpactInfo
+import org.evomaster.core.search.impact.impactinfocollection.SpecificImpactInfo
+
+
+class BigDecimalGeneImpact (sharedImpactInfo: SharedImpactInfo, specificImpactInfo: SpecificImpactInfo) : GeneImpact(sharedImpactInfo, specificImpactInfo){
+
+    constructor(
+            id : String
+    ) : this(SharedImpactInfo(id), SpecificImpactInfo())
+
+    override fun copy(): BigDecimalGeneImpact {
+        return BigDecimalGeneImpact(
+                shared.copy(), specific.copy())
+    }
+
+    override fun clone(): BigDecimalGeneImpact {
+        return BigDecimalGeneImpact(
+                shared.clone(), specific.clone())
+    }
+
+    override fun validate(gene: Gene): Boolean = gene is BigDecimalGene
+}
