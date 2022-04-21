62844879191c2d72f380b9dfd4c645b0ac992da7
Juan Pablo Galeotti <jgaleotti@gmail.com>
2022-04-21 17:18:26 -0300

    Documentation for ColumnDto.isCompositeType

--- client-java/controller-api/src/main/java/org/evomaster/client/java/controller/api/dto/database/schema/ColumnDto.java
+++ client-java/controller-api/src/main/java/org/evomaster/client/java/controller/api/dto/database/schema/ColumnDto.java
@@ -24,6 +24,29 @@ public class ColumnDto {
 
     public boolean isEnumeratedType = false;
 
+    /**
+     * From https://www.postgresql.org/docs/14/rowtypes.html
+     * """
+     * A composite type represents the structure of a row or record;
+     * it is essentially just a list of field names and their data types.
+     * PostgreSQL allows composite types to be used in many of the same
+     * ways that simple types can be used.
+     * For example, a column of a table can be declared to be of a composite type.
+     * """
+     * Example composite types:
+     *
+     * CREATE TYPE complex AS (
+     *     r       double precision,
+     *     i       double precision
+     * );
+     *
+     * CREATE TYPE inventory_item AS (
+     *     name            text,
+     *     supplier_id     integer,
+     *     price           numeric
+     * );
+     */
+
     public boolean isCompositeType = false;
     /**
      * precision of number
