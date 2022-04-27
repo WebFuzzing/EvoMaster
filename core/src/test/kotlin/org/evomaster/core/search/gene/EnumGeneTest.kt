29f530c0e8eda27235b19b38f47b805ada1838a4
Man Zhang <man.mzhang@gmail.com>
2022-04-27 13:55:38 +0200

    enable enum genes with empty data add unit test

---
+++ core/src/test/kotlin/org/evomaster/core/search/gene/EnumGeneTest.kt
@@ -0,0 +1,14 @@
+package org.evomaster.core.search.gene
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class EnumGeneTest {
+
+    @Test
+    fun testEmptyDataList(){
+        val data = listOf<Int>()
+        val enumGene = EnumGene("value", data)
+        assertEquals(0, enumGene.values.size)
+    }
+}
