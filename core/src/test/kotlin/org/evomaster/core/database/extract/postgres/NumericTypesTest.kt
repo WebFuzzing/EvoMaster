24052c414a5c9fbb8ebbac896ff72c187815701e
Man Zhang <man.mzhang@gmail.com>
2022-04-28 10:39:24 +0200

    a fix due to merge

--- core/src/test/kotlin/org/evomaster/core/database/extract/postgres/NumericTypesTest.kt
+++ core/src/test/kotlin/org/evomaster/core/database/extract/postgres/NumericTypesTest.kt
@@ -5,7 +5,10 @@ import org.evomaster.client.java.controller.db.SqlScriptRunner
 import org.evomaster.client.java.controller.internal.db.SchemaExtractor
 import org.evomaster.core.database.DbActionTransformer
 import org.evomaster.core.database.SqlInsertBuilder
-import org.evomaster.core.search.gene.*
+import org.evomaster.core.search.gene.DoubleGene
+import org.evomaster.core.search.gene.FloatGene
+import org.evomaster.core.search.gene.IntegerGene
+import org.evomaster.core.search.gene.LongGene
 import org.evomaster.core.search.gene.regex.RegexGene
 import org.evomaster.core.search.gene.sql.SqlAutoIncrementGene
 import org.junit.jupiter.api.Assertions.*
@@ -48,7 +51,7 @@ class NumericTypesTest : ExtractTestBasePostgres() {
         assertTrue(genes[1] is IntegerGene) // integer
         assertTrue(genes[2] is LongGene) // bigint
         assertTrue(genes[3] is FloatGene) //decimal
-        assertTrue(genes[3] is FloatGene) // numeric
+        assertTrue(genes[4] is FloatGene) // numeric
         assertTrue(genes[5] is DoubleGene) //real
         assertTrue(genes[6] is DoubleGene) // double precision
         assertTrue(genes[7] is SqlAutoIncrementGene) // smallserial
