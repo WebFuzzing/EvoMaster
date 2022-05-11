42cb1f57297caebc53b4479100e7146d51a885a8
seran <7030273+seran@users.noreply.github.com>
2022-05-11 14:39:31 +0200

    basic logic for external service initialization implemented

--- core/src/main/kotlin/org/evomaster/core/problem/external/service/ExternalServiceInfo.kt
+++ core/src/main/kotlin/org/evomaster/core/problem/external/service/ExternalServiceInfo.kt
@@ -7,6 +7,8 @@ class ExternalServiceInfo(
         val remoteHostname: String,
         val remotePort: Int) {
 
+    private lateinit var wireMockServer : WireMockServer
+
     init {
         if (remoteHostname.isBlank()) {
             throw IllegalArgumentException("Remote hostname can not be blank")
@@ -14,6 +16,13 @@ class ExternalServiceInfo(
         if (protocol.isBlank()) {
             throw IllegalArgumentException("Protocol can not be blank")
         }
+    }
+
+    fun assignWireMockServer(wm: WireMockServer) {
+        wireMockServer = wm
+    }
+    fun getWireMockServer() : WireMockServer {
+        return wireMockServer
     }
 
     override fun equals(other: Any?): Boolean {
