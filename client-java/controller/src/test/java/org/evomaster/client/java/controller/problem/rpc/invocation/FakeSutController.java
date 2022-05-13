8afffe2561a8458513017c76cb025842f170b644
Man Zhang <man.mzhang@gmail.com>
2022-05-13 23:11:29 +0200

    add tests for seed

--- client-java/controller/src/test/java/org/evomaster/client/java/controller/problem/rpc/invocation/FakeSutController.java
+++ client-java/controller/src/test/java/org/evomaster/client/java/controller/problem/rpc/invocation/FakeSutController.java
@@ -1,18 +1,20 @@
 package org.evomaster.client.java.controller.problem.rpc.invocation;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thrift.example.artificial.RPCInterfaceExample;
 import com.thrift.example.artificial.RPCInterfaceExampleImpl;
 import org.evomaster.client.java.controller.EmbeddedSutController;
 import org.evomaster.client.java.controller.api.dto.AuthenticationDto;
 import org.evomaster.client.java.controller.api.dto.LocalAuthenticationDto;
 import org.evomaster.client.java.controller.api.dto.SutInfoDto;
+import org.evomaster.client.java.controller.api.dto.problem.rpc.SeededRPCActionDto;
+import org.evomaster.client.java.controller.api.dto.problem.rpc.SeededRPCTestDto;
 import org.evomaster.client.java.controller.internal.db.DbSpecification;
 import org.evomaster.client.java.controller.problem.ProblemInfo;
 import org.evomaster.client.java.controller.problem.RPCProblem;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * created by manzhang on 2021/11/27
@@ -21,6 +23,8 @@ public class FakeSutController extends EmbeddedSutController {
 
     public boolean running;
     private RPCInterfaceExampleImpl sut = new RPCInterfaceExampleImpl();
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public String startSut() {
@@ -84,5 +88,20 @@ public class FakeSutController extends EmbeddedSutController {
         boolean auth =  authenticationInfo.equals("local_foo");
         sut.setAuthorized(auth);
         return auth;
+    }
+
+    @Override
+    public List<SeededRPCTestDto> seedRPCTests() {
+        return Arrays.asList(new SeededRPCTestDto(){{
+            testName = "test_1";
+            rpcFunctions = Arrays.asList(
+                    new SeededRPCActionDto(){{
+                        interfaceName = RPCInterfaceExample.class.getName();
+                        functionName = "seedcheck";
+                        inputParams= Arrays.asList("[1,2,3]","[1,2,3]","[{\"bdPositiveFloat\":10.12,\"bdNegativeFloat\":-10.12,\"bdPositiveOrZeroFloat\":0.00,\"bdNegativeOrZeroFloat\":-2.16,\"biPositive\":10,\"biPositiveOrZero\":0,\"biNegative\":-10,\"biNegativeOrZero\":-2}]","{\"1\":\"1\",\"2\":\"2\"}");
+                        inputParamTypes= Arrays.asList(List.class.getName(),List.class.getName(),List.class.getName(), Map.class.getName());
+                    }}
+            );
+        }});
     }
 }
