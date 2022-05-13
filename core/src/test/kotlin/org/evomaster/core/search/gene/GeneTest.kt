e0c2fbdf7183e5da0529f18266b58e27d82418fa
arcuri82 <arcuri82@gmail.com>
2022-05-13 10:14:35 +0200

    adding (failing) tests for randomization

--- core/src/test/kotlin/org/evomaster/core/search/gene/GeneTest.kt
+++ core/src/test/kotlin/org/evomaster/core/search/gene/GeneTest.kt
@@ -4,6 +4,8 @@ import org.evomaster.core.search.service.Randomness
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import kotlin.reflect.full.isSuperclassOf
 
 class GeneTest {
@@ -11,7 +13,7 @@ class GeneTest {
     private val genes = GeneSamplerForTests.geneClasses
 
     @Test
-    fun testNumberOfGenes(){
+    fun testNumberOfGenes() {
         /*
             This number should not change, unless you explicitly add/remove any gene.
             if so, update this number accordingly
@@ -24,34 +26,34 @@ class GeneTest {
     fun testPackage() {
 
         val errors = genes.map { it.qualifiedName!! }
-                .filter { ! it.startsWith("org.evomaster.core.search.gene") }
+                .filter { !it.startsWith("org.evomaster.core.search.gene") }
 
-        if(errors.isNotEmpty()){
+        if (errors.isNotEmpty()) {
             println("Wrong packages: $errors")
         }
         assertEquals(0, errors.size)
     }
 
     @Test
-    fun testNameSuffix(){
+    fun testNameSuffix() {
 
         val errors = genes.map { it.qualifiedName!! }
-                .filter { ! it.endsWith("Gene") }
+                .filter { !it.endsWith("Gene") }
 
-        if(errors.isNotEmpty()){
+        if (errors.isNotEmpty()) {
             println("Wrong names: $errors")
         }
         assertEquals(0, errors.size)
     }
 
     @Test
-    fun testHierarchy(){
+    fun testHierarchy() {
 
         val errors = genes.filter {
             it != Gene::class && !SimpleGene::class.isSuperclassOf(it) && !CompositeGene::class.isSuperclassOf(it)
         }
 
-        if(errors.isNotEmpty()){
+        if (errors.isNotEmpty()) {
             println("Wrong names: $errors")
         }
         assertEquals(0, errors.size)
@@ -59,13 +61,13 @@ class GeneTest {
 
 
     @Test
-    fun testStringGene(){
+    fun testStringGene() {
         /*
             this kind of test could be moved directly into StringGeneTest, but here it is just to checkout
             if sampler is working fine
          */
         val rand = Randomness()
-        for(i in 0..100){
+        for (i in 0..100) {
             val s = GeneSamplerForTests.sample(StringGene::class, rand)
             s.randomize(rand, true, listOf())
             assertTrue(s.isValid())
@@ -75,21 +77,25 @@ class GeneTest {
     }
 
     @Test
-    fun testCanSample(){
+    fun testCanSample() {
 
         val errors = genes
                 .filter { !it.isAbstract }
                 .filter {
-            try{GeneSamplerForTests.sample(it, Randomness()); false} catch (e: Exception){true}
-        }
+                    try {
+                        GeneSamplerForTests.sample(it, Randomness()); false
+                    } catch (e: Exception) {
+                        true
+                    }
+                }
 
-        if(errors.isNotEmpty()){
+        if (errors.isNotEmpty()) {
             println("Cannot sample: $errors")
         }
         assertEquals(0, errors.size)
     }
 
-    private fun getSample(seed: Long) : List<Gene>{
+    private fun getSample(seed: Long): List<Gene> {
         val rand = Randomness()
         rand.updateSeed(seed)
 
@@ -99,7 +105,7 @@ class GeneTest {
     }
 
     @Test
-    fun testRoot(){
+    fun testRoot() {
 
         val sample = getSample(123)
 
@@ -109,7 +115,7 @@ class GeneTest {
             val wholeTree = root.flatView().filter { it != root }
 
             val errors = wholeTree.filter { it.isDefinedRoot() }
-            if(errors.isNotEmpty()){
+            if (errors.isNotEmpty()) {
                 println("Extra roots for $root: $errors")
             }
             assertEquals(0, errors.size)
@@ -117,7 +123,7 @@ class GeneTest {
     }
 
     @Test
-    fun testParent(){
+    fun testParent() {
         val sample = getSample(321)
 
         sample.forEach { root ->
@@ -125,15 +131,71 @@ class GeneTest {
             val wholeTree = root.flatView().filter { it != root }
 
             wholeTree.forEach { n ->
-                var p  = n
-                while(p.parent != null){p = p.parent as Gene}
+                var p = n
+                while (p.parent != null) {
+                    p = p.parent as Gene
+                }
                 assertEquals(root, p)
             }
         }
     }
 
     @Test
-    fun testParentWhenCopy(){
+    fun testParentRandomized() {
+        val seed = 45328L
+        val rand = Randomness()
+        rand.updateSeed(seed)
+        val sample = getSample(seed)
+
+        sample.filter { it.isMutable() }
+                .forEach { root ->
+                    root.identifyAsRoot()
+                    root.randomize(rand, true)
+                    assertTrue(root.isValid())
+
+                    val wholeTree = root.flatView().filter { it != root }
+
+                    wholeTree.forEach { n ->
+                        var p = n
+                        while (p.parent != null) {
+                            p = p.parent as Gene
+                        }
+                        assertEquals(root, p)
+                    }
+                }
+    }
+
+
+    @Test
+    fun testParentWhenCopyRandomized() {
+
+        val seed = 25778L
+        val rand = Randomness()
+        rand.updateSeed(seed)
+        val sample = getSample(seed)
+
+        sample.filter { it.isMutable() }
+                .forEach { root ->
+                    root.identifyAsRoot()
+                    root.randomize(rand, true)
+                    assertTrue(root.isValid())
+
+                    val copy = root.copy()
+                    assertTrue(copy != root) //TODO what is immutable root? might fail
+                    val wholeTree = copy.flatView().filter { it != root }
+
+                    wholeTree.forEach { n ->
+                        var p = n
+                        while (p.parent != null) {
+                            p = p.parent as Gene
+                        }
+                        assertEquals(copy, p)
+                    }
+                }
+    }
+
+    @Test
+    fun testParentWhenCopy() {
 
         val sample = getSample(321)
 
@@ -145,11 +207,33 @@ class GeneTest {
 
             wholeTree.forEach { n ->
                 var p = n
-                while(p.parent != null){p = p.parent as Gene}
+                while (p.parent != null) {
+                    p = p.parent as Gene
+                }
                 assertEquals(copy, p)
             }
         }
     }
+
+    @ParameterizedTest
+    @ValueSource(longs = [1,2,3,4,5,6,7,8,9,10])
+    fun testRandomized(seed: Long){
+
+        val rand = Randomness()
+        rand.updateSeed(seed)
+        val sample = getSample(seed)
+
+        sample.filter { it.isMutable() }
+                .forEach { root ->
+                    root.identifyAsRoot()
+                    root.randomize(rand, true)
+                    assertTrue(root.isValid())
+
+                    val copy = root.copy()
+                    assertTrue(copy.isValid())
+                }
+    }
+
 
     //TODO for each *Gene, sample random instances, and verify properties
 }
