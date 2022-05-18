b040611de2a5140c6c46f17edda01457bbdfdb3f
seran <7030273+seran@users.noreply.github.com>
2022-05-18 11:14:50 +0200

    minor fix

--- core/src/main/kotlin/org/evomaster/core/problem/external/service/ExternalServiceAction.kt
+++ core/src/main/kotlin/org/evomaster/core/problem/external/service/ExternalServiceAction.kt
@@ -2,6 +2,6 @@ package org.evomaster.core.problem.external.service
 
 import net.sf.jsqlparser.statement.ReferentialAction.Action
 
-class ExternalServiceAction : Action(listOf()) {
-    // TODO
-}
+//class ExternalServiceAction : Action(listOf()) {
+//    // TODO
+//}
