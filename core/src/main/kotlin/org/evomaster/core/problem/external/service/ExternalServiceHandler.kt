118e364aacb0917e84c9b48aee8347d46bc68c2c
seran <7030273+seran@users.noreply.github.com>
2022-06-01 18:22:47 +0200

    using remote port for WireMock

--- core/src/main/kotlin/org/evomaster/core/problem/external/service/ExternalServiceHandler.kt
+++ core/src/main/kotlin/org/evomaster/core/problem/external/service/ExternalServiceHandler.kt
@@ -148,7 +148,7 @@ class ExternalServiceHandler {
         val wm = WireMockServer(
             WireMockConfiguration()
                 .bindAddress(address)
-                .port(8080)
+                .port(port)
                 .extensions(ResponseTemplateTransformer(false)))
         wm.start()
 
