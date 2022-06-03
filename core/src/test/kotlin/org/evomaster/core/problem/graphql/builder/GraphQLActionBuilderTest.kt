de3694985e6f6e3ade6b33a3cdee310991acda7d
asmab <asma.belhadi.17@gmail.com>
2022-06-03 15:39:07 +0200

    Removing test+ Initialising GitHub schema.

--- core/src/test/kotlin/org/evomaster/core/problem/graphql/builder/GraphQLActionBuilderTest.kt
+++ core/src/test/kotlin/org/evomaster/core/problem/graphql/builder/GraphQLActionBuilderTest.kt
@@ -1201,18 +1201,7 @@ class GraphQLActionBuilderTest {
         val config = EMConfig()
         GraphQLActionBuilder.addActionsFromSchema(json, actionCluster, config.treeDepth)
 
-        assertEquals(19, actionCluster.size)
-    }
-
-    @Test
-    fun gitHub2Test() {
-        val actionCluster = mutableMapOf<String, Action>()
-        val json = GraphQLActionBuilderTest::class.java.getResource("/graphql/GitHub2.json").readText()
-
-        val config = EMConfig()
-        GraphQLActionBuilder.addActionsFromSchema(json, actionCluster, config.treeDepth)
-
-        assertEquals(19, actionCluster.size)
+        assertEquals(204, actionCluster.size)
     }
 
 }
