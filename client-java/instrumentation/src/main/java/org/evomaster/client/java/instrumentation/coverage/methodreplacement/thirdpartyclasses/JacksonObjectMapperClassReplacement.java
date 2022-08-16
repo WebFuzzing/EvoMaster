4be3ef8aa1209747b2c93816d04c7722521ea9be
seran <7030273+seran@users.noreply.github.com>
2022-08-16 11:56:23 +0200

    method replacement added

---
+++ client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/coverage/methodreplacement/thirdpartyclasses/JacksonObjectMapperClassReplacement.java
@@ -0,0 +1,54 @@
+package org.evomaster.client.java.instrumentation.coverage.methodreplacement.thirdpartyclasses;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import org.evomaster.client.java.instrumentation.coverage.methodreplacement.Replacement;
+import org.evomaster.client.java.instrumentation.coverage.methodreplacement.ThirdPartyMethodReplacementClass;
+import org.evomaster.client.java.instrumentation.coverage.methodreplacement.UsageFilter;
+import org.evomaster.client.java.instrumentation.object.ClassToSchema;
+import org.evomaster.client.java.instrumentation.shared.ReplacementCategory;
+import org.evomaster.client.java.instrumentation.shared.ReplacementType;
+import org.evomaster.client.java.instrumentation.staticstate.ExecutionTracer;
+import org.evomaster.client.java.instrumentation.staticstate.UnitsInfoRecorder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Objects;
+
+public class JacksonObjectMapperClassReplacement extends ThirdPartyMethodReplacementClass {
+
+    private static final JacksonObjectMapperClassReplacement singleton = new JacksonObjectMapperClassReplacement();
+
+    @Override
+    protected String getNameOfThirdPartyTargetClass() {
+        return "com.fasterxml.jackson.databind.ObjectMapper";
+    }
+
+    @Replacement(replacingStatic = false,
+            type = ReplacementType.TRACKER,
+            id = "Jackson_ObjectMapper_readValue_class",
+            usageFilter = UsageFilter.ONLY_SUT,
+            category = ReplacementCategory.NET)
+    public static <T> T readValue(Object caller, InputStream src, Class<T> valueType) throws IOException, JsonParseException, JsonMappingException {
+        Objects.requireNonNull(caller);
+
+        if(valueType != null) {
+            String name = valueType.getName();
+            String schema = ClassToSchema.getOrDeriveSchema(valueType);
+            UnitsInfoRecorder.registerNewParsedDto(name, schema);
+            ExecutionTracer.addParsedDtoName(name);
+        }
+
+        Method original = getOriginal(singleton, "Jackson_ObjectMapper_readValue_class", caller);
+
+        try {
+            return (T) original.invoke(caller, src, valueType);
+        } catch (IllegalAccessException e){
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e){
+            throw (RuntimeException) e.getCause();
+        }
+    }
+}
