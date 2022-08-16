f7e73aa1bbf29e7668641eae51585f8a4d3e7986
Man Zhang <m.zhang1126@gmail.com>
2022-08-16 23:57:54 +0200

    fix structural element tests

--- core/src/test/kotlin/org/evomaster/core/search/structuralelement/individual/RestIndividualStructureTest.kt
+++ core/src/test/kotlin/org/evomaster/core/search/structuralelement/individual/RestIndividualStructureTest.kt
@@ -83,8 +83,9 @@ class RestIndividualStructureTest : StructuralElementBaseTest(){
         val dbpath = listOf(1, 0)
         assertEquals(barId, root.targetWithIndex(dbpath))
 
+        // root.seeMainExecutableActions()[0] is obtained with 2-> 0-> 0, i.e., 2nd children (resourceCall) -> 0th group -> 0th action
         val floatValue = (((root.seeMainExecutableActions()[0]).parameters[0] as BodyParam).gene as ObjectGene).fields[3]
-        val path = listOf(2, 0, 0, 0, 3)
+        val path = listOf(2, 0, 0, 0, 0, 3)
         assertEquals(floatValue, root.targetWithIndex(path))
 
         val actualPath = mutableListOf<Int>()
