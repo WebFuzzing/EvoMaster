5e8f7f419a940f1f6301bdb348efd866f6e0c99c
arcuri82 <arcuri82@gmail.com>
2022-08-26 15:04:05 +0200

    increase default max length for strings

--- core/src/main/kotlin/org/evomaster/core/EMConfig.kt
+++ core/src/main/kotlin/org/evomaster/core/EMConfig.kt
@@ -1522,7 +1522,7 @@ class EMConfig {
     @Max(stringLengthHardLimit.toDouble())
     @Cfg("The maximum length allowed for evolved strings. Without this limit, strings could in theory be" +
             " billions of characters long")
-    var maxLengthForStrings = 16
+    var maxLengthForStrings = 200
 
 
     @Min(0.0)
