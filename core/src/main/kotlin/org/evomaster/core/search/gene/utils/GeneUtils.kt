25055fc132d30f711c668e5125b8dca979f27d37
asmab <asma.belhadi.17@gmail.com>
2022-09-09 09:59:14 +0200

    Fixing the issue with the cycle gene in the Boolean selection.

--- core/src/main/kotlin/org/evomaster/core/search/gene/utils/GeneUtils.kt
+++ core/src/main/kotlin/org/evomaster/core/search/gene/utils/GeneUtils.kt
@@ -554,7 +554,7 @@ object GeneUtils {
                                 gene.gene.elements.dropLast(1).plus(handleBooleanSelection(gene.gene.elements.last())),
                                 lastElementTreatedSpecially = true
                             ) else if (gene.gene is TupleGene)
-                            gene.gene else if (gene.gene is LimitObjectGene)  gene
+                            gene.gene else if (gene.gene is LimitObjectGene)  gene else if (gene.gene is CycleObjectGene) gene
                         else
                         // on by default, but can be deselected during the search
                             BooleanGene(gene.name, true)
