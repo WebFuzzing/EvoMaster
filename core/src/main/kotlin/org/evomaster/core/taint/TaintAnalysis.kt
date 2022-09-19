19eedbda32a4c4fc26ab49d5914a0a5322b82079
arcuri82 <arcuri82@gmail.com>
2022-09-19 13:34:15 +0200

    fix for regex issue

--- core/src/main/kotlin/org/evomaster/core/taint/TaintAnalysis.kt
+++ core/src/main/kotlin/org/evomaster/core/taint/TaintAnalysis.kt
@@ -98,7 +98,7 @@ object TaintAnalysis {
                 but for now we just keep a very basic, ad-hoc solution
              */
 
-            if(s.stringSpecialization != StringSpecialization.REGEX || genes.size != 2){
+            if(!s.stringSpecialization.isRegex || genes.size != 2){
                 continue
             }
 
@@ -126,11 +126,11 @@ object TaintAnalysis {
             try {
                 genes[0].addSpecializations(
                         genes[0].getValueAsRawString(),
-                        listOf(StringSpecializationInfo(StringSpecialization.REGEX, choices[0])),
+                        listOf(StringSpecializationInfo(StringSpecialization.REGEX_WHOLE, choices[0])),
                         randomness)
                 genes[1].addSpecializations(
                         genes[1].getValueAsRawString(),
-                        listOf(StringSpecializationInfo(StringSpecialization.REGEX, choices[1])),
+                        listOf(StringSpecializationInfo(StringSpecialization.REGEX_WHOLE, choices[1])),
                         randomness)
             }catch (e: Exception){
                 LoggingUtil.uniqueWarn(log, "Cannot handle partial match on regex: ${s.value}")
