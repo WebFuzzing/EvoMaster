ee2e97ba675829ad657b0124504495eacd2038e2
seran <7030273+seran@users.noreply.github.com>
2022-09-22 15:00:22 +0200

    exp

--- e2e-tests/spring-rest-openapi-v2/src/test/java/org/evomaster/e2etests/spring/examples/wiremock/service/ExternalServiceMockingFlakyEMTest.java
+++ e2e-tests/spring-rest-openapi-v2/src/test/java/org/evomaster/e2etests/spring/examples/wiremock/service/ExternalServiceMockingFlakyEMTest.java
@@ -44,8 +44,8 @@ public class ExternalServiceMockingFlakyEMTest extends SpringTestBase {
                     assertHasAtLeastOne(solution, HttpVerb.GET, 200, "/api/wiremock/external", "true");
                     assertHasAtLeastOne(solution, HttpVerb.GET, 500, "/api/wiremock/external", "false");
                     // TODO: disable till the basics works perfectly
-//                    assertHasAtLeastOne(solution, HttpVerb.GET, 200, "/api/wiremock/external/complex", "true");
-//                    assertHasAtLeastOne(solution, HttpVerb.GET, 500, "/api/wiremock/external/complex", "false");
+                    assertHasAtLeastOne(solution, HttpVerb.GET, 200, "/api/wiremock/external/complex", "true");
+                    assertHasAtLeastOne(solution, HttpVerb.GET, 500, "/api/wiremock/external/complex", "false");
                     // TODO: Disabled till the Jackson method replacement handled to unmarshall the JSON
 //                    assertHasAtLeastOne(solution, HttpVerb.GET, 200, "/api/wiremock/external/json", "false");
                 });
