1ca713d9aa583e43a33c77cb042256f91aa80274
arcuri82 <arcuri82@gmail.com>
2022-10-04 15:55:17 +0200

    added failing E2E test for WM response mocking

---
+++ e2e-tests/spring-rest-openapi-v3/src/main/kotlin/com/foo/rest/examples/spring/openapi/v3/wiremock/urlopen/WmUrlOpenRest.kt
@@ -0,0 +1,27 @@
+package com.foo.rest.examples.spring.openapi.v3.wiremock.urlopen
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.net.URL
+
+@RestController
+@RequestMapping(path = ["/api/wm/urlopen"])
+class WmUrlOpenRest {
+
+    @GetMapping(path = ["/string"])
+    open fun getString() : ResponseEntity<String> {
+
+        val url = URL("http://hello.there:8123/api/string")
+        val connection = url.openConnection()
+        connection.setRequestProperty("accept", "application/json")
+        val data = connection.content.toString()
+
+        return if (data == "HELLO THERE!!!"){
+            ResponseEntity.ok("OK")
+        } else{
+            ResponseEntity.status(500).build()
+        }
+    }
+}
---
+++ e2e-tests/spring-rest-openapi-v3/src/test/kotlin/com/foo/rest/examples/spring/openapi/v3/wiremock/urlopen/WmUrlOpenController.kt
@@ -0,0 +1,6 @@
+package com.foo.rest.examples.spring.openapi.v3.wiremock.urlopen
+
+import com.foo.rest.examples.spring.openapi.v3.SpringController
+
+class WmUrlOpenController : SpringController(WmUrlOpenApplication::class.java) {
+}
