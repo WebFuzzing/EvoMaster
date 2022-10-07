e0089890d32ce6141a976eff48a2539e53d3b4ca
arcuri82 <arcuri82@gmail.com>
2022-10-07 23:49:31 +0200

    TT for Thread.sleep

---
+++ client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/coverage/methodreplacement/classes/ThreadMethodReplacement.java
@@ -0,0 +1,40 @@
+package org.evomaster.client.java.instrumentation.coverage.methodreplacement.classes;
+
+import org.evomaster.client.java.instrumentation.coverage.methodreplacement.MethodReplacementClass;
+import org.evomaster.client.java.instrumentation.coverage.methodreplacement.Replacement;
+import org.evomaster.client.java.instrumentation.coverage.methodreplacement.UsageFilter;
+import org.evomaster.client.java.instrumentation.shared.ReplacementCategory;
+import org.evomaster.client.java.instrumentation.shared.ReplacementType;
+import org.evomaster.client.java.instrumentation.staticstate.ExecutionTracer;
+
+public class ThreadMethodReplacement implements MethodReplacementClass {
+
+    @Override
+    public Class<?> getTargetClass() {
+        return Thread.class;
+    }
+
+    @Replacement(replacingStatic = true,
+            type = ReplacementType.TRACKER,
+            usageFilter = UsageFilter.ANY,
+            category = ReplacementCategory.EXT_0)
+    public static void sleep(long millis) throws InterruptedException {
+        sleep(millis,0);
+    }
+
+    @Replacement(replacingStatic = true,
+            type = ReplacementType.TRACKER,
+            usageFilter = UsageFilter.ANY,
+            category = ReplacementCategory.EXT_0)
+    public static void sleep(long millis, int nanos) throws InterruptedException {
+
+        if(millis >= 2000){
+            millis = 2000;
+            nanos = 0;
+        }
+
+        ExecutionTracer.reportSleeping();
+
+        Thread.sleep(millis, nanos);
+    }
+}
