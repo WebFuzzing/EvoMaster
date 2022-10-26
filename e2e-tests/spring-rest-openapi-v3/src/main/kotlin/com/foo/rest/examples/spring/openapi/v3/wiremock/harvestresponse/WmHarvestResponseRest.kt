e12520267a3e653ab8406db2313f092816125043
Man Zhang <m.zhang1126@gmail.com>
2022-10-26 14:34:34 +0200

    update e2e

--- e2e-tests/spring-rest-openapi-v3/src/main/kotlin/com/foo/rest/examples/spring/openapi/v3/wiremock/harvestresponse/WmHarvestResponseRest.kt
+++ e2e-tests/spring-rest-openapi-v3/src/main/kotlin/com/foo/rest/examples/spring/openapi/v3/wiremock/harvestresponse/WmHarvestResponseRest.kt
@@ -1,5 +1,6 @@
 package com.foo.rest.examples.spring.openapi.v3.wiremock.harvestresponse
 
+import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.squareup.okhttp.OkHttpClient
 import com.squareup.okhttp.Request
@@ -13,14 +14,15 @@ import java.net.URL
 @RequestMapping(path = ["/api/wm/harvestresponse"])
 class WmHarvestResponseRest {
     private val urlToGetEUCCountries = "https://api.first.org/data/v1/countries?q=norway"
+    private val urlToGETGoRESTUsers = "https://gorest.co.in/public/v2/users"
 
     private val mapper = ObjectMapper()
 
     val client = OkHttpClient()
 
 
-    @GetMapping(path = ["/norway"])
-    fun getString() : ResponseEntity<String> {
+    @GetMapping(path = ["/country"])
+    fun getNumCountry() : ResponseEntity<String> {
 
         val url = URL(urlToGetEUCCountries)
 
@@ -35,6 +37,32 @@ class WmHarvestResponseRest {
                 return ResponseEntity.status(400).build()
             val msg = "$code:${if ((dto?.data?.size?:0) == 1) dto.data!!.keys.first() else if ((dto?.data?.size ?: 0) > 3) "MORE THAN 3" else "NONE OR TWO"}"
             return ResponseEntity.ok(msg)
+        }catch (e: Exception){
+            return ResponseEntity.status(500).build()
+        }
+    }
+
+    @GetMapping(path = ["/users"])
+    fun getNumUsers() : ResponseEntity<String> {
+
+        val url = URL(urlToGETGoRESTUsers)
+
+        val request = Request.Builder()
+            .url(url)
+            .header("Accept","application/json")
+            .header("Content-Type","application/json")
+            .header("Authorization","Bearer ACCESS-TOKEN")
+            .build()
+
+        try {
+            val data = client.newCall(request).execute()
+            val body = data.body()?.string()
+            val code = data.code()
+            val dto = mapper.readValue(body, object : TypeReference<List<UserDto>>() {})
+            return if (code == 200 && dto.size == 10)
+                ResponseEntity.ok("10 users as expected")
+            else
+                ResponseEntity.status(400).build()
         }catch (e: Exception){
             return ResponseEntity.status(500).build()
         }
