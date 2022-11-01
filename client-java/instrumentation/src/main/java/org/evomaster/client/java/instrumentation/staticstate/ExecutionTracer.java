d684519e4d6830c0160105f222066cc9efe5f908
Man Zhang <m.zhang1126@gmail.com>
2022-11-01 15:59:14 +0100

    address comments

--- client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/staticstate/ExecutionTracer.java
+++ client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/staticstate/ExecutionTracer.java
@@ -122,6 +122,7 @@ public class ExecutionTracer {
             executingAction = false;
             sleepingThreads.clear();
             lastCallerClass = null;
+            skippedHostName.clear();
         }
     }
 
