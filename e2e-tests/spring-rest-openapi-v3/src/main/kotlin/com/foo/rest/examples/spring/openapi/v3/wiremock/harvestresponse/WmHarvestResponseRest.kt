fd1aad285c2d7623fde09f70e84354c67fe190b0
Man Zhang <m.zhang1126@gmail.com>
2022-11-01 23:29:52 +0100

    add e2e

--- e2e-tests/spring-rest-openapi-v3/src/main/kotlin/com/foo/rest/examples/spring/openapi/v3/wiremock/harvestresponse/WmHarvestResponseRest.kt
+++ e2e-tests/spring-rest-openapi-v3/src/main/kotlin/com/foo/rest/examples/spring/openapi/v3/wiremock/harvestresponse/WmHarvestResponseRest.kt
@@ -8,13 +8,19 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.io.*
+import java.net.HttpURLConnection
 import java.net.URL
+import java.net.URLConnection
 
 @RestController
 @RequestMapping(path = ["/api/wm/harvestresponse"])
 class WmHarvestResponseRest {
     private val urlToGetABCMetaData = "https://list.ly/api/v4/meta?url=http%3A%2F%2Fabc.com"
     private val urlToGETGoRESTUsers = "https://gorest.co.in/public/v2/users"
+
+    private val server = "https://grch37.rest.ensembl.org"
+    private val ext = "/vep/human/hgvs"
 
     private val token = System.getenv("GOREST_AUTH_KEY")?: "ACCESS-TOKEN"
 
@@ -43,6 +49,45 @@ class WmHarvestResponseRest {
         }catch (e: Exception){
             return ResponseEntity.status(500).build()
         }
+    }
+
+    @GetMapping(path = ["/grch37Example"])
+    fun getGrch37Example() : ResponseEntity<String> {
+        /*
+            example from https://grch37.rest.ensembl.org/documentation/info/vep_hgvs_post
+
+            used in EMB genome-nexus
+         */
+
+        val url = URL(server + ext)
+
+        val connection = url.openConnection() as HttpURLConnection
+
+        val postBody = "{ \"hgvs_notations\" : [\"AGT:c.803T>C\", \"9:g.22125503G>C\" ] }"
+        connection.requestMethod = "POST"
+        connection.setRequestProperty("Content-Type", "application/json")
+        connection.setRequestProperty("Accept", "application/json")
+        connection.setRequestProperty("Content-Length", postBody.toByteArray().size.toString())
+        connection.useCaches = false
+        connection.doInput = true
+        connection.doOutput = true
+
+        val wr = DataOutputStream(connection.outputStream)
+        wr.writeBytes(postBody)
+        wr.flush()
+        wr.close()
+
+        val responseCode: Int = connection.responseCode
+        val data = connection.inputStream.bufferedReader().use(BufferedReader::readText)
+
+        if (responseCode != 200) {
+            return ResponseEntity.status(400).build()
+        }
+
+        val list = mapper.readValue(data, List::class.java)
+        if (list.size >= 2 && list.any { it is Map<*, *> && it.size > 10 })
+            return ResponseEntity.ok("Found harvested response")
+        return ResponseEntity.ok("Cannot find harvested response")
     }
 
     /*
