6ad3b5aed2ae98574549f456c21c8e513c23e1ca
Man Zhang <m.zhang1126@gmail.com>
2022-11-01 14:44:53 +0100

    fix JsonTaint

--- client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/object/JsonTaint.java
+++ client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/object/JsonTaint.java
@@ -5,8 +5,9 @@ import org.evomaster.client.java.instrumentation.shared.StringSpecializationInfo
 import org.evomaster.client.java.instrumentation.staticstate.ExecutionTracer;
 import org.evomaster.client.java.utils.SimpleLogger;
 
-import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class JsonTaint {
 
@@ -22,7 +23,7 @@ public class JsonTaint {
         StringSpecializationInfo info;
 
         try{
-            if(Collection.class.isAssignableFrom(klass)){
+            if(List.class.isAssignableFrom(klass) || Set.class.isAssignableFrom(klass)){
                 // TODO are there cases in which the content structure would be available? to check
                 info = new StringSpecializationInfo(StringSpecialization.JSON_ARRAY,null);
             } else if (Map.class.isAssignableFrom(klass)){
@@ -30,7 +31,7 @@ public class JsonTaint {
                     might set schema value null for 2-phases taint if needed later
                     the value relates to add 1) tainted map (null) or 2) map (with schema) into string specification
                  */
-                info = new StringSpecializationInfo(StringSpecialization.JSON_MAP, ClassToSchema.getOrDeriveSchemaWithItsRef(klass));
+                info = new StringSpecializationInfo(StringSpecialization.JSON_MAP, null);
             }else {
                 info = new StringSpecializationInfo(StringSpecialization.JSON_OBJECT, ClassToSchema.getOrDeriveSchemaWithItsRef(klass));
             }
