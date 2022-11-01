62d7ff0da7b0574528e2a4be98879f16c946ec46
arcuri82 <arcuri82@gmail.com>
2022-11-01 10:34:44 +0100

    fixed serialization issue

--- client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/JpaConstraint.java
+++ client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/JpaConstraint.java
@@ -1,9 +1,10 @@
 package org.evomaster.client.java.instrumentation;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Objects;
 
-public class JpaConstraint {
+public class JpaConstraint implements Serializable {
 
     private final String tableName;
 
