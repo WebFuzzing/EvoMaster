1ea7f2fee6d019b3c70c980ae461004322a1d85b
Robert Carrier <rcarrier@gozego.com>
2022-11-10 16:32:29 -0600

    SDET-666 Add bash option for url

--- getTestsForPublicApi.sh
+++ getTestsForPublicApi.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-while getopts i:s: option
+while getopts i:s:u: option
 do
   case "${option}"
     in
     i)client_id=${OPTARG};;
     s)client_secret=${OPTARG};;
+    u)url=${OPTARG};;
     *)
   esac
 done
@@ -18,9 +19,9 @@ res_uuid=$(python3 getResidentToken.py)
 resident_token="Bearer $(python3 getAuthToken.py --client_id "$client_id" --client_secret "$client_secret" --user_id "$res_uuid")"
 
 # shellcheck disable=SC2140
-java -jar core/target/evomaster.jar --blackBox true --bbTargetUrl https://public-api.paylease.net/graphql --outputFolder "src/resident_token" --outputFormat JAVA_TESTNG --problemType GRAPHQL --maxTime 2m --ratePerMinute 120 --header0 "Authorization":"$resident_token"
+java -jar core/target/evomaster.jar --blackBox true --bbTargetUrl "$url" --outputFolder "src/resident_token" --outputFormat JAVA_TESTNG --problemType GRAPHQL --maxTime 2m --ratePerMinute 120 --header0 "Authorization":"$resident_token"
 
 client_token="Bearer $(python3 getAuthToken.py --client_id "$client_id" --client_secret "$client_secret")"
 
 # shellcheck disable=SC2140
-java -jar core/target/evomaster.jar --blackBox true --bbTargetUrl https://public-api.paylease.net/graphql  --outputFolder "src/client_token" --outputFormat JAVA_TESTNG --problemType GRAPHQL --maxTime 2m --ratePerMinute 120 --header0 "Authorization":"$client_token"
+java -jar core/target/evomaster.jar --blackBox true --bbTargetUrl "$url"  --outputFolder "src/client_token" --outputFormat JAVA_TESTNG --problemType GRAPHQL --maxTime 2m --ratePerMinute 120 --header0 "Authorization":"$client_token"
