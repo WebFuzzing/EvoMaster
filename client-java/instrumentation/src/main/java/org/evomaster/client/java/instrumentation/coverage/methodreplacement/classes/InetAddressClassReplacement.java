d2cd0854b52bdc404d68c66380fd22abfb6206cf
seran <7030273+seran@users.noreply.github.com>
2022-11-18 15:08:48 +0100

    changes to external service handling

--- client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/coverage/methodreplacement/classes/InetAddressClassReplacement.java
+++ client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/coverage/methodreplacement/classes/InetAddressClassReplacement.java
@@ -15,10 +15,10 @@ import java.net.*;
  * with socket connection, it will first do host lookup with java.net.InetAddress#getByName
  * if it does not exist, UnknownHostException will be thrown,
  * then the `socket.connect` cannot be reached.
- *
+ * <p>
  * in order to make it connected, we could do replacement for 1) collecting host info, and
  * 2) providing an ip address
- *
+ * <p>
  * note that it is not used now, ie, not register it into ReplacementList
  */
 public class InetAddressClassReplacement implements MethodReplacementClass {
@@ -37,17 +37,20 @@ public class InetAddressClassReplacement implements MethodReplacementClass {
         if (ExternalServiceInfoUtils.skipHostnameOrIp(host))
             return InetAddress.getByName(host);
         // FIXME -1 leads a crash, but do we really need the real port info here. might use specified one
-        ExternalServiceInfo remoteHostInfo = new ExternalServiceInfo("none", host, 10000);
-         try{
-             if (ExecutionTracer.hasExternalMapping(remoteHostInfo.signature())){
-                 String ip = ExecutionTracer.getExternalMapping(remoteHostInfo.signature());
-                 return InetAddress.getByName(ip);
-             }
-             return InetAddress.getByName(host);
-         }catch (UnknownHostException e){
-             ExecutionTracer.addExternalServiceHost(remoteHostInfo);
-             throw e;
-         }
+
+        // When using an imaginary host, this method will keep throwing UnknownHostException forever
+        // which won't allow the code to move further
+        ExternalServiceInfo remoteHostInfo = new ExternalServiceInfo("none", host, -1);
+        try {
+            if (ExecutionTracer.hasExternalMapping(remoteHostInfo.signature())) {
+                String ip = ExecutionTracer.getExternalMapping(remoteHostInfo.signature());
+                return InetAddress.getByName(ip);
+            }
+            return InetAddress.getByName(host);
+        } catch (UnknownHostException e) {
+            ExecutionTracer.addExternalServiceHost(remoteHostInfo);
+            throw e;
+        }
     }
 
     @Replacement(
@@ -60,14 +63,14 @@ public class InetAddressClassReplacement implements MethodReplacementClass {
         if (ExternalServiceInfoUtils.skipHostnameOrIp(host))
             return InetAddress.getAllByName(host);
         // FIXME -1 leads a crash, but do we really need the real port info here. might use specified one
-        ExternalServiceInfo remoteHostInfo = new ExternalServiceInfo("none", host, 10000);
-        try{
-            if (ExecutionTracer.hasExternalMapping(remoteHostInfo.signature())){
+        ExternalServiceInfo remoteHostInfo = new ExternalServiceInfo("none", host, -1);
+        try {
+            if (ExecutionTracer.hasExternalMapping(remoteHostInfo.signature())) {
                 String ip = ExecutionTracer.getExternalMapping(remoteHostInfo.signature());
                 return new InetAddress[]{InetAddress.getByName(ip)};
             }
             return InetAddress.getAllByName(host);
-        }catch (UnknownHostException e){
+        } catch (UnknownHostException e) {
             // TODO: Using Inet extract the host information and then add to the collection
             ExecutionTracer.addExternalServiceHost(remoteHostInfo);
             throw e;
--- core/src/main/kotlin/org/evomaster/core/problem/external/service/httpws/HttpWsExternalService.kt
+++ core/src/main/kotlin/org/evomaster/core/problem/external/service/httpws/HttpWsExternalService.kt
@@ -3,7 +3,12 @@ package org.evomaster.core.problem.external.service.httpws
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.MappingBuilder
 import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer
 import com.github.tomakehurst.wiremock.stubbing.StubMapping
+import org.evomaster.core.logging.LoggingUtil
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.util.*
 
 /**
@@ -15,16 +20,55 @@ class HttpWsExternalService(
      * External service information collected from SUT
      */
     private val externalServiceInfo: HttpExternalServiceInfo,
-    /**
-     * Initiated WireMock server for the external service
-     */
-    private val wireMockServer: WireMockServer
 ) {
 
     companion object{
 
         private const val WIREMOCK_DEFAULT_RESPONSE_CODE = 404
         private const val WIREMOCK_DEFAULT_RESPONSE_MESSAGE = "Not Found"
+
+        private val log: Logger = LoggerFactory.getLogger(HttpWsExternalServiceHandler::class.java)
+    }
+
+    /**
+     * Initiated WireMock server for the external service
+     */
+    private val wireMockServer: WireMockServer? = null
+
+    /**
+     * Will initialise WireMock instance on a given IP address for a given port.
+     */
+    fun start(address: String) {
+        if (!externalServiceInfo.isPartial()) {
+            val port = externalServiceInfo.remotePort
+
+            // TODO: Port need to be changed to the remote service port
+            //  In CI also using remote ports as 80 and 443 fails
+            val config =  WireMockConfiguration()
+                .bindAddress(address)
+                .extensions(ResponseTemplateTransformer(false))
+
+            if (!externalServiceInfo.isHttp() && !externalServiceInfo.isHttps())
+                LoggingUtil.uniqueWarn(log, "do not get explicit protocol for address ($address)")
+            val applyHttps = externalServiceInfo.isHttps() || (!externalServiceInfo.isHttp() && externalServiceInfo.isDerivedHttps())
+
+            if (applyHttps){
+                config.httpsPort(port)
+            }else {
+                config.port(port)
+            }
+
+            val wm = WireMockServer(config)
+
+            /*
+                for SUT, its first connection is re-directed to a given ip address,
+                to keep same behavior in generated tests, we do not start the WM accordingly
+             */
+            if (externalServiceInfo !is DefaultHttpExternalServiceInfo){
+                wm.start()
+                wm.stubFor(getDefaultWMMappingBuilder())
+            }
+        }
     }
 
     fun getWMDefaultPriority() = 100
@@ -54,18 +98,18 @@ class HttpWsExternalService(
      * Return the IP address of WireMock instance
      */
     fun getWireMockAddress(): String {
-        return wireMockServer.options.bindAddress()
+        return wireMockServer!!.options.bindAddress()
     }
 
     /**
      * Return the running port of WireMock instance
      */
     fun getWireMockPort(): Int {
-        return wireMockServer.options.portNumber()
+        return wireMockServer!!.options.portNumber()
     }
 
     fun getWireMockServer(): WireMockServer {
-        return wireMockServer
+        return wireMockServer!!
     }
 
     fun getRemoteHostName(): String {
@@ -88,7 +132,7 @@ class HttpWsExternalService(
      * Returns the active stub mappings from the WireMockServer
      */
     fun getStubs(): List<StubMapping> {
-        return wireMockServer.stubMappings
+        return wireMockServer!!.stubMappings
     }
 
 
@@ -102,7 +146,7 @@ class HttpWsExternalService(
      *  reasons. Need to check why.
      */
     fun getAllServedRequests(): List<HttpExternalServiceRequest> {
-        return wireMockServer.allServeEvents.map { it ->
+        return wireMockServer!!.allServeEvents.map { it ->
             HttpExternalServiceRequest(
                 it.id,
                 it.request.method.value(),
@@ -124,7 +168,8 @@ class HttpWsExternalService(
      * requests.
      */
     fun reset() {
-        wireMockServer.resetAll()
+        if (!externalServiceInfo.isPartial())
+            wireMockServer!!.resetAll()
     }
 
     /**
@@ -132,25 +177,31 @@ class HttpWsExternalService(
      * instance.
      */
     fun resetServedRequests() {
-        wireMockServer.resetRequests()
+        if (!externalServiceInfo.isPartial())
+            wireMockServer!!.resetRequests()
     }
 
     /**
      * To stop the WireMock server
      */
     fun stopWireMockServer() {
-        wireMockServer.stop()
+        if (!externalServiceInfo.isPartial())
+            wireMockServer!!.stop()
     }
 
     /**
      * Will remove the stub mapping related to the given id
      */
     fun removeStub(stubId: UUID): Boolean {
-        val stubMapping = wireMockServer.getStubMapping(stubId)
+        val stubMapping = wireMockServer!!.getStubMapping(stubId)
         if (stubMapping.isPresent) {
-            wireMockServer.removeStubMapping(stubMapping.item)
+            wireMockServer!!.removeStubMapping(stubMapping.item)
             return true
         }
         return false
+    }
+
+    fun isActive(): Boolean {
+        return wireMockServer != null
     }
 }
