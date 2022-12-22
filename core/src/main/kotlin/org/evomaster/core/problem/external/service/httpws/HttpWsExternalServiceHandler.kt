c25dbdbbbdcb24fabbec940be61e37063b506cec
seran <7030273+seran@users.noreply.github.com>
2022-12-22 22:36:04 +0100

    exp-3

--- core/src/main/kotlin/org/evomaster/core/problem/external/service/httpws/HttpWsExternalServiceHandler.kt
+++ core/src/main/kotlin/org/evomaster/core/problem/external/service/httpws/HttpWsExternalServiceHandler.kt
@@ -119,16 +119,18 @@ class HttpWsExternalServiceHandler {
 
         if (registered.isNotEmpty()) {
             registered.forEach { (k, e) ->
-                e.updateRemotePort(externalServiceInfo.remotePort)
-                e.startWireMock()
-                /*
-                    Signature should be updated after the port is updated
-                    So the existing element will be removed from the map.
-                    After port information is updated element will be added
-                    to the map with the new key.
-                 */
-                externalServices[e.getSignature()] = e
-                externalServices.remove(k)
+                if (!externalServiceInfo.isPartial()) {
+                    e.updateRemotePort(externalServiceInfo.remotePort)
+                    e.startWireMock()
+                    /*
+                        Signature should be updated after the port is updated
+                        So the existing element will be removed from the map.
+                        After port information is updated element will be added
+                        to the map with the new key.
+                     */
+                    externalServices[e.getSignature()] = e
+                    externalServices.remove(k)
+                }
             }
         } else {
             if (!externalServices.containsKey(externalServiceInfo.signature())) {
