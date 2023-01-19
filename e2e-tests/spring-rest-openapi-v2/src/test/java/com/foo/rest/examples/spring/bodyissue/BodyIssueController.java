e3186293ac85801fb95f2efc21c2b152f8d7734f
arcuri82 <arcuri82@gmail.com>
2023-01-19 15:08:06 +0100

    recreated issue #687 in a E2E test

---
+++ e2e-tests/spring-rest-openapi-v2/src/test/java/com/foo/rest/examples/spring/bodyissue/BodyIssueController.java
@@ -0,0 +1,14 @@
+package com.foo.rest.examples.spring.bodyissue;
+
+import com.foo.rest.examples.spring.SpringController;
+
+/**
+ * Created by arcuri82 on 07-Nov-18.
+ */
+public class BodyIssueController extends SpringController {
+
+    public BodyIssueController() {
+        super(BodyIssueApplication.class);
+    }
+
+}
