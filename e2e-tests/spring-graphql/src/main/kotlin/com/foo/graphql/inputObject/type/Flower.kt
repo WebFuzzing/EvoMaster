0fa098143ec4d3a014212197ea0943e36cdfb20b
asmab <asma.belhadi.17@gmail.com>
2023-02-10 22:51:59 +0100

    Adding input object API.

---
+++ e2e-tests/spring-graphql/src/main/kotlin/com/foo/graphql/inputObject/type/Flower.kt
@@ -0,0 +1,5 @@
+package com.foo.graphql.inputObject.type
+
+data class Flower (
+     var id: Int? = null,
+     var price: Int? = null)
---
+++ e2e-tests/spring-graphql/src/main/kotlin/com/foo/graphql/inputObject/type/Store.kt
@@ -0,0 +1,5 @@
+package com.foo.graphql.inputObject.type
+
+data class Store (
+        var id: Int? = null,
+        var name: String? = null)
