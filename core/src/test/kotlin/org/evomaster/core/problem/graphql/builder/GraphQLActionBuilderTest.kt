d75c7004d71d85ab3cbe1b574eee3955ac77f128
asmab <asma.belhadi.17@gmail.com>
2023-02-15 13:39:18 +0100

    Checking the constructed gene+ Adding temporary unit test.

--- core/src/test/kotlin/org/evomaster/core/problem/graphql/builder/GraphQLActionBuilderTest.kt
+++ core/src/test/kotlin/org/evomaster/core/problem/graphql/builder/GraphQLActionBuilderTest.kt
@@ -302,8 +302,92 @@ class GraphQLActionBuilderTest {
         assertEquals(9, objAgency.fields.size)
         assertTrue(objAgency.fields.any { it is BooleanGene && it.name == "lang" })
         assertTrue(objAgency.fields.any { it is BooleanGene && it.name == "phone" })
+        /**/
+        val stations = actionCluster["stations"] as GraphQLAction
+        assertEquals(5, stations.parameters.size)
+        assertTrue(stations.parameters[4] is GQReturnParam)
+
+        assertTrue(stations.parameters[4].gene is ObjectGene)
+       val objectStop = stations.parameters[4].gene as ObjectGene
+        assertEquals(27, objectStop.fields.size)
+
+        assertTrue(objectStop.fields.any { it.getWrappedGene(TupleGene::class.java)?.name == "stopTimesForPattern" })
+
+        val optStopTimesForPattern = objectStop.fields.first { it.name == "stopTimesForPattern" }
+        val tupleStopTimesForPattern = optStopTimesForPattern.getWrappedGene(TupleGene::class.java)
+        assertEquals(7, tupleStopTimesForPattern?.elements?.size)
+
+        if (tupleStopTimesForPattern !=null) {
+            assertTrue(tupleStopTimesForPattern.elements.any { it is ObjectGene && it.name == "stopTimesForPattern" })
+        }
+
+        val objStopTimesForPattern = tupleStopTimesForPattern?.elements?.get(6)  as ObjectGene
+        assertEquals(17, objStopTimesForPattern.fields.size)
+        assertTrue(objStopTimesForPattern.fields.any { it.getWrappedGene(ObjectGene::class.java)?.name == "trip" })
+
+
+        val optTrip = objStopTimesForPattern.fields.first { it.name == "trip" }
+        val objTrip = optTrip.getWrappedGene(ObjectGene::class.java)
+
+        if(objTrip!=null) {
+            assertEquals(22, objTrip?.fields?.size)
+            assertTrue(objTrip.fields.any { it.getWrappedGene(TupleGene::class.java)?.name == "departureStoptime" })//nbr17
+            val optDepartureStoptime = objTrip.fields.first { it.name == "departureStoptime" }
+            val tupleDepartureStoptime = optDepartureStoptime.getWrappedGene(TupleGene::class.java)
+            assertEquals(2, tupleDepartureStoptime?.elements?.size)
+            assertTrue(tupleDepartureStoptime?.elements?.last() is (CycleObjectGene))
+        }
+
+
     }
 
+    @Test
+    fun digitransitHSLSchemaTestFragment() {
+        val actionCluster = mutableMapOf<String, Action>()
+        val json = GraphQLActionBuilderTest::class.java.getResource("/graphql/artificial/DigitransitHSL(fragment1).json").readText()
+
+        val config = EMConfig()
+        GraphQLActionBuilder.addActionsFromSchema(json, actionCluster, config.treeDepth)
+
+        assertEquals(1, actionCluster.size)
+
+        /**/
+        val stations = actionCluster["stations"] as GraphQLAction
+        assertEquals(2, stations.parameters.size)
+        assertTrue(stations.parameters[1] is GQReturnParam)
+
+        assertTrue(stations.parameters[1].gene is ObjectGene)
+        val objectStop = stations.parameters[1].gene as ObjectGene
+        assertEquals(1, objectStop.fields.size)
+
+        assertTrue(objectStop.fields.any { it.getWrappedGene(TupleGene::class.java)?.name == "stopTimesForPattern" })
+
+        val optStopTimesForPattern = objectStop.fields.first { it.name == "stopTimesForPattern" }
+        val tupleStopTimesForPattern = optStopTimesForPattern.getWrappedGene(TupleGene::class.java)
+        assertEquals(2, tupleStopTimesForPattern?.elements?.size)
+
+        if (tupleStopTimesForPattern !=null) {
+            assertTrue(tupleStopTimesForPattern.elements.any { it is ObjectGene && it.name == "stopTimesForPattern" })
+        }
+
+        val objStopTimesForPattern = tupleStopTimesForPattern?.elements?.get(1)  as ObjectGene
+        assertEquals(1, objStopTimesForPattern.fields.size)
+        assertTrue(objStopTimesForPattern.fields.any { it.getWrappedGene(ObjectGene::class.java)?.name == "trip" })
+
+
+        val optTrip = objStopTimesForPattern.fields.first { it.name == "trip" }
+        val objTrip = optTrip.getWrappedGene(ObjectGene::class.java)
+
+        if(objTrip!=null) {
+            assertEquals(1, objTrip?.fields?.size)
+            assertTrue(objTrip.fields.any { it.getWrappedGene(TupleGene::class.java)?.name == "departureStoptime" })//nbr17
+            val optDepartureStoptime = objTrip.fields.first { it.name == "departureStoptime" }
+            val tupleDepartureStoptime = optDepartureStoptime.getWrappedGene(TupleGene::class.java)
+            assertEquals(2, tupleDepartureStoptime?.elements?.size)
+            assertTrue(tupleDepartureStoptime?.elements?.last() is (CycleObjectGene))
+
+            }
+    }
     @Test
     fun eHRISchemaTest() {
         val actionCluster = mutableMapOf<String, Action>()
