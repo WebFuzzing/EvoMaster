38fef6a88c075c932a3b0900e10b549784e30429
Man Zhang <m.zhang1126@gmail.com>
2023-02-16 17:47:47 +0100

    modify table name

--- e2e-tests/spring-rest-openapi-v2/src/main/java/com/foo/rest/examples/spring/db/crossfks/entities/FooTableEntity.java
+++ e2e-tests/spring-rest-openapi-v2/src/main/java/com/foo/rest/examples/spring/db/crossfks/entities/FooTableEntity.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Table(name = "Foo")
 public class FooTableEntity {
 
     @Id
--- e2e-tests/spring-rest-openapi-v2/src/main/java/com/foo/rest/examples/spring/db/crossfks/entities/RootTableEntity.java
+++ e2e-tests/spring-rest-openapi-v2/src/main/java/com/foo/rest/examples/spring/db/crossfks/entities/RootTableEntity.java
@@ -8,6 +8,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Table(name = "Root")
 public class RootTableEntity {
 
     @Id
