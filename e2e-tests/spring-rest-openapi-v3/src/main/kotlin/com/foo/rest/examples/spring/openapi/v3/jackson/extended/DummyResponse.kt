cbe72d00cea308fe4ac7f1bd097403ccff4bd344
seran <7030273+seran@users.noreply.github.com>
2023-03-28 00:44:04 +0200

    removed useless e2e

--- e2e-tests/spring-rest-openapi-v3/src/main/kotlin/com/foo/rest/examples/spring/openapi/v3/jackson/extended/DummyResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.foo.rest.examples.spring.openapi.v3.jackson.extended
-
-class DummyResponse(
-    val message: String? = null,
-)
--- e2e-tests/spring-rest-openapi-v3/src/test/kotlin/com/foo/rest/examples/spring/openapi/v3/jackson/extended/ExtendedJacksonController.kt
+++
@@ -1,5 +0,0 @@
-package com.foo.rest.examples.spring.openapi.v3.jackson.extended
-
-import com.foo.rest.examples.spring.openapi.v3.SpringController
-
-class ExtendedJacksonController : SpringController(ExtendedJacksonApplication::class.java)
