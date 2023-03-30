ad2e32fa3cfa650d80be5ef074f38e03c2fc6240
arcuri82 <arcuri82@gmail.com>
2023-03-30 15:08:01 +0200

    cleaning

--- client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/coverage/methodreplacement/ThirdPartyMethodReplacementClass.java
+++ client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/coverage/methodreplacement/ThirdPartyMethodReplacementClass.java
@@ -13,7 +13,7 @@ import java.util.*;
  * Third-party libraries might or might not be on the classpath.
  * Furthermore, they MUST NOT be part of EvoMaster.
  * So we have to use reflection to access them at runtime.
- *
+ * <p>
  * There is a problem though :(
  * The replaced methods might have inputs or outputs from the third-party library.
  * To write replacements, we need to create the right method signatures, and those must
@@ -22,28 +22,25 @@ import java.util.*;
  * they will not be included in the uber jar).
  * Unfortunately, this does NOT work, as the classloader that loads the instrumentation might be different from
  * the one used for the SUT. This is the case for example for Spring applications when using External Driver.
- *
+ * <p>
  * The current solution is to use reflection, and have such 3rd-party library NOT on the classpath.
  * They can be in "test" scope when running tests (eg to check validity of string constants), though.
- *
+ * <p>
  * Still, this leaves issue with method signatures.
  * For return types using 3rd-party objects, must put Object as return type, with actual type specified
  * in "castTo". A forced casting is automatically then done at instrumentation time.
  * For input parameters, will need to use the ThirdPartyCast annotation.
- *
+ * <p>
  * There is still the issue of which classloader to use for reflection.
  * For MR of non-static methods, can use classloader of the original caller.
  * For the other cases (eg, static methods and constructors), need to retrieve appropriate classloader from
  * UnitInfoRecorder.
- *
  */
-public abstract class ThirdPartyMethodReplacementClass implements MethodReplacementClass{
+public abstract class ThirdPartyMethodReplacementClass implements MethodReplacementClass {
 
     private static class StateInfo {
 
         public Class<?> targetClass;
-
-       // public boolean triedToLoad = false;
 
         /**
          * Key -> id defined in @Replacement
@@ -61,7 +58,7 @@ public abstract class ThirdPartyMethodReplacementClass implements MethodReplacem
     private final IdentityHashMap<ClassLoader, StateInfo> classInfoPerClassLoader = new IdentityHashMap<>();
 
 
-    protected ThirdPartyMethodReplacementClass(){
+    protected ThirdPartyMethodReplacementClass() {
     }
 
     private void initMethods(ClassLoader loader, StateInfo info) {
@@ -86,23 +83,23 @@ public abstract class ThirdPartyMethodReplacementClass implements MethodReplacem
             Annotation[][] annotations = m.getParameterAnnotations();
 
             int start = 0;
-            if(!r.replacingStatic()){
+            if (!r.replacingStatic()) {
                 start = 1;
             }
 
-            int end = inputs.length-1;
-            if(r.type() == ReplacementType.TRACKER){
+            int end = inputs.length - 1;
+            if (r.type() == ReplacementType.TRACKER) {
                 //no idTemplate at the end
                 end = inputs.length;
             }
 
             Class[] reducedInputs = Arrays.copyOfRange(inputs, start, end);
 
-            for (int i = start; i < end; i++){
+            for (int i = start; i < end; i++) {
                 if (annotations[i].length > 0) {
                     Class<?> klazz = ReplacementUtils.getCastedToThirdParty(annotations[i]);
                     if (klazz != null)
-                        reducedInputs[i-start] = klazz;
+                        reducedInputs[i - start] = klazz;
                 }
             }
 
@@ -122,7 +119,7 @@ public abstract class ThirdPartyMethodReplacementClass implements MethodReplacem
 
             String id = r.id();
 
-            if(info.methods.containsKey(id)){
+            if (info.methods.containsKey(id)) {
                 throw new IllegalStateException("Non-unique id: " + id);
             }
 
@@ -130,7 +127,7 @@ public abstract class ThirdPartyMethodReplacementClass implements MethodReplacem
         }
     }
 
-    private  void initConstructors(ClassLoader loader, StateInfo info) {
+    private void initConstructors(ClassLoader loader, StateInfo info) {
 
         Class<? extends ThirdPartyMethodReplacementClass> subclass = this.getClass();
 
@@ -149,8 +146,8 @@ public abstract class ThirdPartyMethodReplacementClass implements MethodReplacem
 
             int start = 0;
 
-            int end = inputs.length-1;
-            if(r.type() == ReplacementType.TRACKER){
+            int end = inputs.length - 1;
+            if (r.type() == ReplacementType.TRACKER) {
                 //no idTemplate at the end
                 end = inputs.length;
             }
@@ -166,7 +163,7 @@ public abstract class ThirdPartyMethodReplacementClass implements MethodReplacem
 
             String id = r.id();
 
-            if(info.constructors.containsKey(id)){
+            if (info.constructors.containsKey(id)) {
                 throw new IllegalStateException("Non-unique id: " + id);
             }
 
@@ -178,14 +175,13 @@ public abstract class ThirdPartyMethodReplacementClass implements MethodReplacem
     protected abstract String getNameOfThirdPartyTargetClass();
 
     /**
-     *
      * @param singleton a reference to an instance of the subclass. As reflection is expensive,
      *                  we suggest to create it only once, and save it in final static field
-     * @param id    of a replacement method
-     * @return  original method that was replaced
+     * @param id        of a replacement method
+     * @return original method that was replaced
      */
-    public static Method getOriginal(ThirdPartyMethodReplacementClass singleton, String id, Object obj){
-        if(id == null || id.isEmpty()){
+    public static Method getOriginal(ThirdPartyMethodReplacementClass singleton, String id, Object obj) {
+        if (id == null || id.isEmpty()) {
             throw new IllegalArgumentException("Invalid empty id");
         }
 
@@ -193,33 +189,19 @@ public abstract class ThirdPartyMethodReplacementClass implements MethodReplacem
         ClassLoader loader = obj.getClass().getClassLoader();
         StateInfo info = singleton.classInfoPerClassLoader.get(loader);
 
-        if(info == null){
-            /*
-                    This is tricky. We did a method replacement, but the class is not accessible at runtime
-                    from the class loader of the instrumentation... so we try it from the caller
-                    FIXME update comment, after tests/checks
-             */
-//            Class<?> target = singleton.tryLoadingClass(loader);
-//            info = new StateInfo();
-//            info.targetClass = target;
-//            singleton.initMethods(loader, info);
-//            singleton.classInfoPerClassLoader.put(loader, info);
+        if (info == null) {
             info = initializeClassInfo(singleton, loader);
         }
 
-//        if(singleton.methods.isEmpty()){
-//            singleton.initMethods();
-//        }
-        //Method original = singleton.methods.get(id);
         Method original = info.methods.get(id);
-        if(original == null){
+        if (original == null) {
             throw new IllegalArgumentException("No method exists with id: " + id);
         }
         return original;
     }
 
 
-    private static StateInfo initializeClassInfo(ThirdPartyMethodReplacementClass singleton, ClassLoader loader){
+    private static StateInfo initializeClassInfo(ThirdPartyMethodReplacementClass singleton, ClassLoader loader) {
         Class<?> target = singleton.tryLoadingClass(loader);
         StateInfo info = new StateInfo();
         info.targetClass = target;
@@ -230,48 +212,38 @@ public abstract class ThirdPartyMethodReplacementClass implements MethodReplacem
     }
 
     /**
-     *
      * @param singleton a reference to an instance of the subclass. As reflection is expensive,
      *                  we suggest to create it only once, and save it in final static field
-     * @param id    of a replacement method
-     * @return  original constructor that was replaced
+     * @param id        of a replacement method
+     * @return original constructor that was replaced
      */
-    public static Constructor getOriginalConstructor(ThirdPartyMethodReplacementClass singleton, String id){
-        if(id == null || id.isEmpty()){
+    public static Constructor getOriginalConstructor(ThirdPartyMethodReplacementClass singleton, String id) {
+        if (id == null || id.isEmpty()) {
             throw new IllegalArgumentException("Invalid empty id");
         }
-
-      //  if(singleton.getTargetClass()==null){
 
             /*
                 we do not have access to the caller directly here, so we need to use what registered
                 in ExecutionTracer
              */
 
-            String callerName = ExecutionTracer.getLastCallerClass();
-            if(callerName == null){
-                //this would be clearly a bug...
-                throw new IllegalStateException("No access to last caller class");
-            }
-            //TODO what if more than 1 available ???
-            //might have to store last one in use for caller class
-            ClassLoader loader = UnitsInfoRecorder.getInstance().getClassLoaders(callerName).get(0);
-
-           // singleton.tryLoadingClass(loader);
-        //}
+        String callerName = ExecutionTracer.getLastCallerClass();
+        if (callerName == null) {
+            //this would be clearly a bug...
+            throw new IllegalStateException("No access to last caller class");
+        }
+        //TODO what if more than 1 available ???
+        //might have to store last one in use for caller class
+        ClassLoader loader = UnitsInfoRecorder.getInstance().getClassLoaders(callerName).get(0);
 
         StateInfo info = singleton.classInfoPerClassLoader.get(loader);
 
-        if(info == null){
+        if (info == null) {
             info = initializeClassInfo(singleton, loader);
         }
 
-//        if(singletoninfo.constructors.isEmpty()){
-//            singleton.initConstructors();
-//        }
-        //Constructor original = singleton.constructors.get(id);
         Constructor original = info.constructors.get(id);
-        if(original == null){
+        if (original == null) {
             throw new IllegalArgumentException("No constructor exists with id: " + id);
         }
         return original;
@@ -286,7 +258,7 @@ public abstract class ThirdPartyMethodReplacementClass implements MethodReplacem
     }
 
     @Override
-    public final Class<?> getTargetClass(){
+    public final Class<?> getTargetClass() {
         throw new IllegalStateException("This method should never be called on a third-party replacement");
     }
 
@@ -300,34 +272,6 @@ public abstract class ThirdPartyMethodReplacementClass implements MethodReplacem
             //it is not a bug/error
             return null;
         }
-
-        //this leads to infinite recursion
-//        StateInfo info = classInfoPerClassLoader.get(loader);
-//        if(info == null){
-//            info = initializeClassInfo(this, loader);
-//        }
-//        return info.targetClass;
-
-//        if(targetClass != null){
-//            return targetClass;
-//        }
-//
-//        /*
-//            If not present, try to load it via reflection based on the class name.
-//            But try only once
-//         */
-//        if(!triedToLoad){
-//            triedToLoad = true;
-//
-//            try{
-//                targetClass = Class.forName(getTargetClassName());
-//            }catch (Exception e){
-//                //this can happen if the third-party library is missing.
-//                //it is not a bug/error
-//            }
-//        }
-//
-//        return targetClass;
     }
 
     @Override
