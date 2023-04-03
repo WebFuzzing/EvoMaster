263bb5523b0692b811d57738551461eec2763a79
Man Zhang <m.zhang1126@gmail.com>
2023-04-03 12:56:39 +0200

    update unit test

--- core/src/test/kotlin/org/evomaster/core/problem/external/service/ExternalHarvesterStrategyTest.kt
+++ core/src/test/kotlin/org/evomaster/core/problem/external/service/ExternalHarvesterStrategyTest.kt
@@ -1,28 +1,61 @@
 package org.evomaster.core.problem.external.service
 
+import com.google.inject.AbstractModule
+import com.google.inject.Injector
+import com.google.inject.Provides
+import com.google.inject.Singleton
+import com.netflix.governator.guice.LifecycleInjector
+import org.evomaster.core.BaseModule
 import org.evomaster.core.EMConfig
 import org.evomaster.core.problem.externalservice.httpws.HttpExternalServiceRequest
 import org.evomaster.core.problem.externalservice.httpws.param.HttpWsResponseParam
 import org.evomaster.core.problem.externalservice.httpws.service.HarvestActualHttpWsResponseHandler
-import org.evomaster.core.search.service.Randomness
+import org.evomaster.core.remote.service.RemoteController
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.util.*
 
-class ExternalHarvesterStrategyUnitTest {
 
-    val randomness = Randomness()
+class ExternalHarvesterStrategyTest {
 
-    val remoteController = DummyController()
+    private lateinit var config: EMConfig
+    private lateinit var externalHarvestActualHttpWsResponseHandler: HarvestActualHttpWsResponseHandler
+
+    @BeforeEach
+    fun init(){
+
+        val injector: Injector = LifecycleInjector.builder()
+            .withModules(FakeModule(), BaseModule())
+            .build().createInjector()
+
+
+        config = injector.getInstance(EMConfig::class.java)
+        externalHarvestActualHttpWsResponseHandler = injector.getInstance(HarvestActualHttpWsResponseHandler::class.java)
+
+    }
+
+
+    private class FakeModule : AbstractModule() {
+
+        @Provides
+        @Singleton
+        fun getRemoteController(): RemoteController {
+            return DummyController()
+        }
+
+        override fun configure() {
+            bind(HarvestActualHttpWsResponseHandler::class.java)
+                .asEagerSingleton()
+        }
+    }
 
     @Test
     fun testExactStrategy() {
-        val config = EMConfig()
         config.externalRequestResponseSelectionStrategy = EMConfig.ExternalRequestResponseSelectionStrategy.EXACT
         config.probOfHarvestingResponsesFromActualExternalServices = 1.0
         config.probOfMutatingResponsesBasedOnActualResponse = 2.0
 
-        val externalHarvestActualHttpWsResponseHandler = HarvestActualHttpWsResponseHandler(config, remoteController, randomness)
         externalHarvestActualHttpWsResponseHandler.initialize()
 
         val requests = mutableListOf<HttpExternalServiceRequest>()
@@ -47,12 +80,10 @@ class ExternalHarvesterStrategyUnitTest {
 
     @Test
     fun testClosestStrategy() {
-        val config = EMConfig()
         config.externalRequestResponseSelectionStrategy = EMConfig.ExternalRequestResponseSelectionStrategy.CLOSEST
         config.probOfHarvestingResponsesFromActualExternalServices = 1.0
         config.probOfMutatingResponsesBasedOnActualResponse = 2.0
 
-        val externalHarvestActualHttpWsResponseHandler = HarvestActualHttpWsResponseHandler(config, remoteController, randomness)
         externalHarvestActualHttpWsResponseHandler.initialize()
 
         val requests = mutableListOf<HttpExternalServiceRequest>()
