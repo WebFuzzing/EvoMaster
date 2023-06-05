dddf02ffcdefd429b2ad921c2c6e089af88193f1
arcuri82 <arcuri82@gmail.com>
2023-06-05 14:02:08 +0200

    added support for JDK 17

--- client-java/controller/src/main/java/org/evomaster/client/java/controller/InstrumentedSutStarter.java
+++ client-java/controller/src/main/java/org/evomaster/client/java/controller/InstrumentedSutStarter.java
@@ -27,6 +27,14 @@ public class InstrumentedSutStarter {
         //AgentLoader.loadAgentClass(InstrumentingAgent.class.getName(), "foobar_packagenameshouldnotexist.");
     }
 
+
+    /**
+     *   Annoying settings needed for JDK 17 :(
+     *
+     *   Update docs/jdks.md if this changes
+     */
+    public static final String JDK_17_JVM_OPTIONS = "--add-opens java.base/java.util.regex=ALL-UNNAMED --add-opens java.base/java.net=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED";
+
     private static boolean alreadyLoaded = false;
 
     private final SutController sutController;
@@ -42,8 +50,7 @@ public class InstrumentedSutStarter {
                         "\nIf you are using JDK 11 or above, are you sure you added the following JVM option?" +
                         "\n -Djdk.attach.allowAttachSelf=true " +
                         "\nAlso, if you are using JDK 17 or above, you also need the following:" +
-                        // Update docs/jdks.md if this changes
-                        "\n--add-opens java.base/java.util.regex=ALL-UNNAMED --add-opens java.base/java.net=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED"+
+                        "\n"+JDK_17_JVM_OPTIONS+
                         "\nSee documentation at https://github.com/EMResearch/EvoMaster/blob/master/docs/jdks.md", e);
             }
         }
