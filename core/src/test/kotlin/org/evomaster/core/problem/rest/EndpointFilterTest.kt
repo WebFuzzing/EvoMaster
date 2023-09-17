9513d6f5ff08dd7180cb461d75b712864d2029dc
cryptotiger <dumanonur@gmail.com>
2023-09-17 22:50:06 +0200

    This test class contains tests for ensuring that endpointFocus and endpointPrefix parameters are received correctly.

--- core/src/test/kotlin/org/evomaster/core/problem/rest/EndpointFilterTest.kt
+++ core/src/test/kotlin/org/evomaster/core/problem/rest/EndpointFilterTest.kt
@@ -1,6 +1,5 @@
 package org.evomaster.core.problem.rest
 
-import org.evomaster.client.java.instrumentation.coverage.methodreplacement.classes.LocalDateClassReplacement
 import org.evomaster.core.EMConfig
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
