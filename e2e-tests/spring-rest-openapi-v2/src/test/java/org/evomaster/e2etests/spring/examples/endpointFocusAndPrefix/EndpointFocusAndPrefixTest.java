995d0ea8b2985e7ea3bfeb62c8c993263da19b43
cryptotiger <dumanonur@gmail.com>
2023-09-17 22:46:55 +0200

    This test class contains tests for endpointFocus and endpointPrefix parameters of Evomaster.

---
+++ e2e-tests/spring-rest-openapi-v2/src/test/java/org/evomaster/e2etests/spring/examples/endpointFocusAndPrefix/EndpointFocusAndPrefixTest.java
@@ -0,0 +1,125 @@
+package org.evomaster.e2etests.spring.examples.endpointFocusAndPrefix;
+
+import com.foo.rest.examples.spring.endpointFocusAndPrefix.EndpointFocusAndPrefixController;
+import org.evomaster.client.java.instrumentation.shared.ClassName;
+import org.evomaster.core.problem.rest.RestIndividual;
+import org.evomaster.core.problem.rest.OpenApiAccess;
+import org.evomaster.core.search.Solution;
+import org.evomaster.e2etests.spring.examples.SpringTestBase;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import io.swagger.v3.oas.models.OpenAPI;
+
+
+import java.util.List;
+
+
+public class EndpointFocusAndPrefixTest extends SpringTestBase {
+
+    private final String swaggerURLSample = "https://petstore.swagger.io/v2/swagger.json";
+    private OpenAPI swagger = OpenApiAccess.INSTANCE.getOpenAPIFromURL(swaggerURLSample);
+
+
+    @BeforeAll
+    /* An empty method since the test is going to use petstore API on
+       So not initiating controller for this test
+     */
+    public static void initClass() throws Exception {
+
+        SpringTestBase.initClass(new EndpointFocusAndPrefixController());
+
+    }
+
+    /*
+    Black box test with existing endpointFocus
+     */
+    @Test
+    public void testRunBlackBoxWithFocus() throws Throwable {
+
+
+
+        String outputFolder = "EndpointFocus";
+        String endpointFocus = "/pet";
+
+
+        //List<String> args = getArgsWithCompilation(
+         //       10,
+          //      outputFolder,
+           //     ClassName.get("org.foo.BlackBoxConstant"),
+           //     true);
+
+
+        List<String> args = getArgsWithCompilation(
+                10,
+                outputFolder,
+                ClassName.get("org.foo.EndpointFocus"),
+                true);
+        // program arguments for EvoMaster
+        args.add("--blackBox");
+        args.add("true");
+        args.add("--bbSwaggerUrl");
+        args.add(swaggerURLSample);
+        // endpointFocus
+        args.add("--endpointFocus");
+        args.add(endpointFocus);
+
+        boolean hasAtLeastOneTest = true;
+
+        Solution<RestIndividual> solution = initAndRun(args);
+
+        String pathToSearch = "/v2" + endpointFocus;
+
+        assertAllSolutionsHavePathFocus(solution, pathToSearch);
+
+        compile(outputFolder);
+
+    }
+
+    /*
+    If an endpointFocus which does not exist in the swagger is provided,
+    the program has to throw an IllegalArgumentException and stop
+     */
+    @Test
+    public void testRunBlackBoxWithPrefix() throws Throwable {
+
+
+
+        String outputFolder = "EndpointPrefix";
+        String endpointPrefix = "/pet";
+
+
+        //List<String> args = getArgsWithCompilation(
+        //       10,
+        //      outputFolder,
+        //     ClassName.get("org.foo.BlackBoxConstant"),
+        //     true);
+
+
+        List<String> args = getArgsWithCompilation(
+                10,
+                outputFolder,
+                ClassName.get("org.foo.EndpointPrefix"),
+                true);
+        // program arguments for EvoMaster
+        args.add("--blackBox");
+        args.add("true");
+        args.add("--bbSwaggerUrl");
+        args.add(swaggerURLSample);
+        // endpointFocus
+        args.add("--endpointPrefix");
+        args.add(endpointPrefix);
+
+        boolean hasAtLeastOneTest = true;
+
+        Solution<RestIndividual> solution = initAndRun(args);
+
+
+        String pathToSearch = "/v2" + endpointPrefix;
+
+        assertAllSolutionsHavePathPrefix(solution, pathToSearch);
+
+        //compile(outputFolder);
+
+    }
+}
