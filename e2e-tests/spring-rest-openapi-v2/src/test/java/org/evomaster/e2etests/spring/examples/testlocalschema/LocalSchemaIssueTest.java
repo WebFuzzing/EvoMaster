f4db8ec8fca5e843f23ea645cb65141434261b1f
Onur D <dumanonur@gmail.com>
2023-10-05 10:03:32 +0200

    Removed the empty test case.

--- e2e-tests/spring-rest-openapi-v2/src/test/java/org/evomaster/e2etests/spring/examples/testlocalschema/LocalSchemaIssueTest.java
+++
@@ -1,8 +0,0 @@
-package org.evomaster.e2etests.spring.examples.testlocalschema;
-
-import org.evomaster.e2etests.spring.examples.SpringTestBase;
-
-public class LocalSchemaIssueTest extends SpringTestBase {
-
-
-}
