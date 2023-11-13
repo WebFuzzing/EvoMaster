094eb7f5b37ed86dc7d545544273f25e6d8ce2fd
ZhangMan <m.zhang1126@gmail.com>
2023-11-13 15:07:34 +0800

    CoveredStatisticsBySeededTests with tests

--- core/src/main/kotlin/org/evomaster/core/search/service/Archive.kt
+++ core/src/main/kotlin/org/evomaster/core/search/service/Archive.kt
@@ -82,9 +82,10 @@ class Archive<T> where T : Individual {
      */
     private var lastChosen: Int? = null
 
-    data class CoveredStatisticsBySeededTests(val coveredTargets: List<Int>)
+    data class CoveredStatisticsBySeededTests<G> (val coveredTargets: List<Int>, val uniquePopulationsDuringSeeding : List<EvaluatedIndividual<G>>) where G: Individual
 
-    private var coveredStatisticsBySeededTests : CoveredStatisticsBySeededTests? = null
+    private var coveredStatisticsBySeededTests : CoveredStatisticsBySeededTests<T>? = null
+
 
     /**
      * Kill all populations.
@@ -103,7 +104,6 @@ class Archive<T> where T : Individual {
     }
 
 
-
     fun archiveCoveredStatisticsBySeededTests(){
         if (coveredStatisticsBySeededTests != null){
             throw IllegalStateException("`archiveCoveredStatisticsBySeededTests` can only be performed once")
@@ -111,8 +111,10 @@ class Archive<T> where T : Individual {
 
         val current = extractSolution()
         coveredStatisticsBySeededTests = CoveredStatisticsBySeededTests(
-            coveredTargets = current.overall.getViewOfData().filter { it.value.distance == FitnessValue.MAX_VALUE }.keys.toList()
+            coveredTargets = current.overall.getViewOfData().filter { it.value.distance == FitnessValue.MAX_VALUE }.keys.toList(),
+            if (config.exportTestCasesDuringSeeding) current.individuals.map { it.copy() } else emptyList()
         )
+
     }
 
     fun anyTargetsCoveredSeededTests () = coveredStatisticsBySeededTests != null && coveredStatisticsBySeededTests!!.coveredTargets.isNotEmpty()
