bb7acc5e5ec9d3654269b77881eb3e5f4ea7f62c
Juan Pablo Galeotti <jgaleotti@gmail.com>
2023-11-22 17:35:57 -0300

    add string specialization for ObjectId(String hexString)

---
+++ client-java/instrumentation/src/test/java/com/foo/somedifferentpackage/examples/bson/ObjectIdBuilderImp.java
@@ -0,0 +1,15 @@
+package com.foo.somedifferentpackage.examples.bson;
+
+import org.bson.types.ObjectId;
+import org.evomaster.client.java.instrumentation.example.bson.ObjectIdBuilder;
+
+public class ObjectIdBuilderImp implements ObjectIdBuilder {
+
+    @Override
+    public ObjectId buildNewObjectId(String hexString) {
+        ObjectId newObjectId = new ObjectId(hexString);
+        return newObjectId;
+    }
+
+
+}
