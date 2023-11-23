6d4558e79604ef8bebf2e0cabcfc99ff5d7141a5
Juan Pablo Galeotti <jgaleotti@gmail.com>
2023-11-23 09:47:52 -0300

    fixes to remove Code Analysis warnings

--- e2e-tests/spring-rest-mongo/src/test/java/org/evomaster/e2etests/spring/rest/mongo/foo/MongoEMObjectIdWithMRTest.java
+++ e2e-tests/spring-rest-mongo/src/test/java/org/evomaster/e2etests/spring/rest/mongo/foo/MongoEMObjectIdWithMRTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MongoEMObjectIdWithMRTest extends RestTestBase {
 
