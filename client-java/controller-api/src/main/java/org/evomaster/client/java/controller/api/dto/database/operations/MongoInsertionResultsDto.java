0995e96de1c57b6ea05ab24ad7df33b92c69de0b
hghianni <28987681+hghianni@users.noreply.github.com>
2023-11-25 14:09:50 -0300

    Fix issue with test case writer when Mongo insertions fail

--- client-java/controller-api/src/main/java/org/evomaster/client/java/controller/api/dto/database/operations/MongoInsertionResultsDto.java
+++ client-java/controller-api/src/main/java/org/evomaster/client/java/controller/api/dto/database/operations/MongoInsertionResultsDto.java
@@ -1,7 +1,10 @@
 package org.evomaster.client.java.controller.api.dto.database.operations;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MongoInsertionResultsDto {
     /**
@@ -9,4 +12,28 @@ public class MongoInsertionResultsDto {
      * executed successfully
      */
     public List<Boolean> executionResults = new ArrayList<>();
+    public Integer failedInsertionIndex = -1;
+
+    public Boolean insertionFailed() {
+        return failedInsertionIndex != -1;
+    }
+
+    public void handleFailedInsertion(List<MongoInsertionDto> insertions, Exception e) {
+        failedInsertionIndex = findFailedInsertion(e);
+
+        if(insertionFailed()){
+            List<Boolean> results = new ArrayList<>(Collections.nCopies(insertions.size(), false));
+
+            for (int i = 0; i < failedInsertionIndex; i++) {
+                results.set(i, true);
+            }
+
+            executionResults = results;
+        }
+    }
+
+    private static int findFailedInsertion(Exception e) {
+        Matcher matcher = Pattern.compile("index (\\d+)").matcher(e.getMessage());
+        return matcher.find() ? Integer.parseInt(matcher.group(1)) : -1;
+    }
 }
