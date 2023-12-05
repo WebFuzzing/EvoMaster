970ef5ae2764b4f66919e640f485b8773d700677
Onur D <dumanonur@gmail.com>
2023-12-05 23:55:55 +0100

    Fixed the following issues in the simple security application:
    - removed POST
    - I have modified the code in a way that it gives 404 when the resource is not there.
    - I changed the implementation in a way that there are only these endpoints, GET, PUT, and DELETE.
    -  I changed the implementation of the PUT method in a way that the input parameters are retrieved from the body of the method, not as request parameters.
    - I made modifications to the security configuration so that authentication is done with Spring security. Authorization is done in endpoints.

--- e2e-tests/spring-rest-openapi-v2/src/main/java/com/foo/rest/examples/spring/security/accesscontrol/deleteput/SimpleAccessControlRest.java
+++ e2e-tests/spring-rest-openapi-v2/src/main/java/com/foo/rest/examples/spring/security/accesscontrol/deleteput/SimpleAccessControlRest.java
@@ -3,13 +3,12 @@ package com.foo.rest.examples.spring.security.accesscontrol.deleteput;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
 
 /*
 Vulnerable application in which only authorized users can create or delete resources but
@@ -17,11 +16,7 @@ any user can modify resources.
 
 This application was developed to satisfy the request:
 
-delete this file once added example
-
-each E2E will have its own subpackage here
-
-example, simple with only 2 endpoints: PUT and DELETE on
+An example application with 2 endpoints: PUT and DELETE on the same resource
 
 /api/{x}
 
@@ -32,88 +27,54 @@ example, simple with only 2 endpoints: PUT and DELETE on
 public class SimpleAccessControlRest {
 
     // owners of each resource
-    private HashMap<String, String> resourceOwners= new HashMap<String, String>();
+    private final Map<String, String> resourceOwners= new HashMap<>();
 
     // set of resources
-    private HashMap<String, ResourceDto> resources = new HashMap<String, ResourceDto>();
-
-
-    //
-
+    private final Map<String, ResourceDto> resources = new HashMap<>();
 
     @GetMapping(value = "/{x}")
-    public ResourceDto getResource(@PathVariable("x") String x) {
+    public ResponseEntity getResource(@PathVariable("x") String x) {
 
-        return resources.get(x);
-    }
-
-    @PostMapping(value = "/{x}")
-    public ResponseEntity createResource(@PathVariable("x") String x, @RequestParam String stringValue, @RequestParam int integerValue, @RequestParam boolean booleanValue ) {
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        // anonymous users cannot create resources
-        if (authentication.getPrincipal().equals("anonymousUser")) {
-
-            return new ResponseEntity<>("Only authenticated users can create resources ", HttpStatus.UNAUTHORIZED);
+        if (resources.containsKey(x)) {
+            return ResponseEntity.status(HttpStatus.OK).body(resources.get(x));
         }
-        // the user is not anonymous
         else {
-
-            String creatorUser = authentication.getName();
-
-            ResourceDto newResource = new ResourceDto();
-            newResource.stringValue = stringValue;
-            newResource.integerValue = integerValue;
-            newResource.booleanValue = booleanValue;
-
-            // if the resource exists and the user sending this request is the resource owner, overwrite the resource
-            if (resources.containsKey(x)) {
-
-                if (resourceOwners.get(x).equals(creatorUser)) {
-
-                    resources.put(x, newResource);
-                    return new ResponseEntity<>("Resource has been successfully recreated by its owner ",
-                            HttpStatus.OK);
-                }
-                else {
-                    return new ResponseEntity<>("Only the owner can recreate the resource ", HttpStatus.UNAUTHORIZED);
-                }
-            }
-            else {
-
-                // if the user has role CREATOR, create the resource
-                if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_CREATOR"))) {
-                    resources.put(x, newResource);
-                    resourceOwners.put(x, creatorUser);
-
-                    return new ResponseEntity<>("New resource has been successfully created ",
-                            HttpStatus.OK);
-                }
-                else {
-                    return new ResponseEntity<>("Only users with creator role can create resources ",
-                            HttpStatus.UNAUTHORIZED);
-                }
-
-            }
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Could not find the resource " +
+                    "with the key: " + x);
         }
     }
 
     // put is to update an existing resource, this endpoint is vulnerable in the sense that
     // it does not check the owner of the resource before updating it
     @PutMapping(value = "/{x}")
-    public ResponseEntity modifyResource(@PathVariable("x") String x, @RequestParam String stringValue, @RequestParam int integerValue, @RequestParam boolean booleanValue ) {
+    public ResponseEntity modifyResource(@PathVariable("x") String x, @RequestBody ResourceDto newResource) {
 
-        ResourceDto newResource = new ResourceDto();
-        newResource.stringValue = stringValue;
-        newResource.integerValue = integerValue;
-        newResource.booleanValue = booleanValue;
+        // Authorization is not checked here, but if the resource does not exist,
+        // the caller is assigned as the owner of the resource
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        resources.put(x, newResource);
+        if (!resources.containsKey(x)) {
 
-        return new ResponseEntity<>("Modified the resource successfully ",
-                HttpStatus.OK);
+            if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_CREATOR"))) {
+                resourceOwners.put(x, authentication.getName());
+                resources.put(x, newResource);
 
+                return new ResponseEntity<>("Created the resource successfully ",
+                        HttpStatus.OK);
+
+            } else {
+                return new ResponseEntity<>("Only users with the role CREATOR can create resources ",
+                        HttpStatus.UNAUTHORIZED);
+            }
+        }
+        else {
+
+            // no authorization check for an existing resource, which is a security issue.
+            resources.put(x, newResource);
+
+            return new ResponseEntity<>("Modified the existing resource successfully ",
+                    HttpStatus.OK);
+        }
     }
 
     // delete endpoint checks the owner of the resource to make sure the resource cannot be deleted
@@ -121,30 +82,23 @@ public class SimpleAccessControlRest {
     public ResponseEntity deleteResource(@PathVariable("x") String x) {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
 
-        // anonymous users cannot delete resources
-        if (authentication.getPrincipal().equals("anonymousUser")) {
-
-            return new ResponseEntity<>("Only authenticated users can delete resources ", HttpStatus.UNAUTHORIZED);
+        if (!resources.containsKey(x)) {
+            return new ResponseEntity<>("The resource does not exist ", HttpStatus.NOT_FOUND);
         } else {
 
-            if (!resources.containsKey(x)) {
-                return new ResponseEntity<>("The resource does not exist ", HttpStatus.INTERNAL_SERVER_ERROR);
-            } else {
+            // only the owner of the resource can delete the resource
+            if (resourceOwners.get(x).equals(authentication.getName())) {
+                resources.remove(x);
+                resourceOwners.remove(x);
 
-                // only the owner of the resource can delete the resource
-                if (resourceOwners.get(x).equals(authentication.getName())) {
-                    resources.remove(x);
-                    resourceOwners.remove(x);
-
-                    return new ResponseEntity<>("Resource has been successfully deleted ",
+                return new ResponseEntity<>("Resource has been successfully deleted ",
                             HttpStatus.OK);
-                } else {
-                    return new ResponseEntity<>("The user is not authorized to delete the resource ",
-                            HttpStatus.UNAUTHORIZED);
+            } else {
+                return new ResponseEntity<>("Only the resource owner is authorized to delete the resource ",
+                        HttpStatus.FORBIDDEN);
                 }
-            }
         }
+
     }
 }
--- e2e-tests/spring-rest-openapi-v2/src/main/java/com/foo/rest/examples/spring/security/accesscontrol/deleteput/SimpleAccessControlWebSecurityConfig.java
+++ e2e-tests/spring-rest-openapi-v2/src/main/java/com/foo/rest/examples/spring/security/accesscontrol/deleteput/SimpleAccessControlWebSecurityConfig.java
@@ -2,19 +2,14 @@ package com.foo.rest.examples.spring.security.accesscontrol.deleteput;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -23,12 +18,16 @@ public class SimpleAccessControlWebSecurityConfig extends WebSecurityConfigurerA
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/v2/api-docs").permitAll()
-                .antMatchers("/api/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/{x}").permitAll()
+                .antMatchers(HttpMethod.PUT, "/api/{x}").hasAnyRole("CREATOR", "CONSUMER")
+                .antMatchers(HttpMethod.DELETE, "/api/{x}").hasRole("CREATOR")
+                .anyRequest().authenticated()
                 .and()
                 .httpBasic()
                 .and()
                 .csrf().disable();
     }
+
 
     @Bean
     @Override
@@ -45,7 +44,6 @@ public class SimpleAccessControlWebSecurityConfig extends WebSecurityConfigurerA
                         .password("consumer1_password")
                         .roles("CONSUMER")
                         .build();
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
 
         UserDetails user3 =
                 User.withDefaultPasswordEncoder()
@@ -54,9 +52,25 @@ public class SimpleAccessControlWebSecurityConfig extends WebSecurityConfigurerA
                         .roles("CONSUMER")
                         .build();
 
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
         manager.createUser(user);
         manager.createUser(user2);
         manager.createUser(user3);
         return manager;
     }
+
+    /*
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth)  throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("creator").password("creator_password").roles("CREATOR").and()
+                .withUser("consumer1").password("consumer1_password").roles("CONSUMER").and()
+                .withUser("consumer2").password("consumer2_password").roles("CONSUMER");
+    }
+
+    */
+
 }
