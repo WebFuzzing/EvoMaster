75c3feb6f6e820323026652fe1b6206655993ca5
Juan Pablo Galeotti <jgaleotti@gmail.com>
2024-01-26 17:47:07 -0300

    bugfix: new @Regex annotation for externalServiceIP

--- core/src/main/kotlin/org/evomaster/core/EMConfig.kt
+++ core/src/main/kotlin/org/evomaster/core/EMConfig.kt
@@ -2017,7 +2017,7 @@ class EMConfig {
 
     @Cfg("User provided external service IP.")
     @Experimental
-    @Regex("^127\\.((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){2}([3-9]|[0-9][0-9]|[0-2][0-5][0-5]?)\$")
+    @Regex("^127(\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}\$")
     var externalServiceIP : String = "127.0.0.3"
 
     @Experimental
