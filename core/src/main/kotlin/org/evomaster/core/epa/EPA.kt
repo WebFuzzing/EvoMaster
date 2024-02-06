e8a742480cccdee2d29f5c934da6603c610264ce
Chiara <31589958+CTarzia@users.noreply.github.com>
2024-02-05 21:11:25 -0300

    move .dot generation from epa to epawriter

--- core/src/main/kotlin/org/evomaster/core/epa/EPA.kt
+++ core/src/main/kotlin/org/evomaster/core/epa/EPA.kt
@@ -7,13 +7,6 @@ class EPA {
 
     val adjacencyMap = mutableMapOf<Vertex, MutableList<Edge>>()
 
-    private val graphHeader = "digraph {\n" +
-            "beautify=true\n" +
-            "graph [pad=\"3\", nodesep=\"4\", ranksep=\"5\"]\n" +
-            "node [ margin=0.4 fontname=Helvetica ]\n" +
-            "edge [fontname=Courier fontsize=14]\n" +
-            "init [shape=box]\n"
-
     fun createOrGetVertex(enabledEndpoints: RestActions, isInitial: Boolean = false): Vertex {
         val enabledEndpointsString = enabledEndpoints.toStringForEPA()
         var vertex: Vertex? = getVertex(enabledEndpointsString)
@@ -33,41 +26,6 @@ class EPA {
             adjacencyMap[source]?.add(edge)
         }
         edge.addRestActionIfNecessary(restAction)
-    }
-
-    fun toDOT(): String {
-        val sb = StringBuilder()
-        sb.append(graphHeader)
-        adjacencyMap.forEach { (vertex, edges) ->
-            if (vertex.isInitial) {
-                sb.append(String.format("init -> \"%s\"\n", vertex.enabledEndpoints))
-            }
-            edges.forEach {
-                val edgeLabel = it.restActions.stream()
-                    .map { a -> a.toString() }
-                    .reduce { s: String, a: String -> String.format("%s, \\n%s", s, a) }
-                if (edgeLabel.isPresent) {
-                    sb.append(
-                        String.format(
-                            "\"%s\" -> \"%s\" [labeldistance=\"0.5\" label=\"%s\"]\n",
-                            it.source.enabledEndpoints,
-                            it.destination.enabledEndpoints,
-                            edgeLabel.get()
-                        )
-                    )
-                } else {
-                    sb.append(
-                        String.format(
-                            "\"%s\" -> \"%s\" \n",
-                            it.source.enabledEndpoints,
-                            it.destination.enabledEndpoints
-                        )
-                    )
-                }
-            }
-        }
-        sb.append("}")
-        return sb.toString()
     }
 
     private fun getVertex(enabledEndpoints: String): Vertex? {
--- core/src/main/kotlin/org/evomaster/core/output/service/EpaWriter.kt
+++ core/src/main/kotlin/org/evomaster/core/output/service/EpaWriter.kt
@@ -9,6 +9,13 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 class EpaWriter {
+    private val graphHeader = "digraph {\n" +
+            "beautify=true\n" +
+            "graph [pad=\"3\", nodesep=\"4\", ranksep=\"5\"]\n" +
+            "node [ margin=0.4 fontname=Helvetica ]\n" +
+            "edge [fontname=Courier fontsize=14]\n" +
+            "init [shape=box]\n"
+
     fun writeEPA(solution: Solution<*>, epaFile: String) {
         val epa = EPA()
         for (i in solution.individuals) {
@@ -35,20 +42,47 @@ class EpaWriter {
     }
 
     private fun writeToFile(epa: EPA, epaFile: String) {
-        var path = Paths.get(epaFile).toAbsolutePath()
+        val path = Paths.get(epaFile).toAbsolutePath()
 
         Files.createDirectories(path.parent)
         Files.deleteIfExists(path)
         Files.createFile(path)
 
-        path.toFile().appendText(epa.toDOT())
+        path.toFile().appendText(toDOT(epa))
+    }
 
-        path = Paths.get(epaFile+".txt").toAbsolutePath()
-
-        Files.createDirectories(path.parent)
-        Files.deleteIfExists(path)
-        Files.createFile(path)
-
-        path.toFile().appendText(epa.adjacencyMap.toString())
+    private fun toDOT(epa: EPA): String {
+        val sb = StringBuilder()
+        sb.append(graphHeader)
+        epa.adjacencyMap.forEach { (vertex, edges) ->
+            if (vertex.isInitial) {
+                sb.append(String.format("init -> \"%s\"\n", vertex.enabledEndpoints))
+            }
+            edges.forEach {
+                val edgeLabel = it.restActions.stream()
+                    .map { a -> a.toString() }
+                    .reduce { s: String, a: String -> String.format("%s, \\n%s", s, a) }
+                if (edgeLabel.isPresent) {
+                    sb.append(
+                        String.format(
+                            "\"%s\" -> \"%s\" [labeldistance=\"0.5\" label=\"%s\"]\n",
+                            it.source.enabledEndpoints,
+                            it.destination.enabledEndpoints,
+                            edgeLabel.get()
+                        )
+                    )
+                } else {
+                    sb.append(
+                        String.format(
+                            "\"%s\" -> \"%s\" \n",
+                            it.source.enabledEndpoints,
+                            it.destination.enabledEndpoints
+                        )
+                    )
+                }
+            }
+        }
+        sb.append("}")
+        return sb.toString()
     }
 }
