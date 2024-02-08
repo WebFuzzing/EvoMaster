dccc89f124930a39b0bd89652199854b2c2fbd71
Agustina Aldasoro <agusaldasoro@gmail.com>
2024-02-08 13:55:26 -0300

    Support for more than one variable

--- solver/src/test/java/Smt2WriterTest.java
+++ solver/src/test/java/Smt2WriterTest.java
@@ -1,7 +1,5 @@
 import org.evomaster.client.java.controller.api.dto.database.schema.TableCheckExpressionDto;
-import org.evomaster.client.java.sql.internal.constraint.DbTableCheckExpression;
 import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -24,23 +22,24 @@ public class Smt2WriterTest {
         assertEquals(expectedSmt2(">", "100"), text);
     }
 
-    // TODO: Add support for multiple check constraints
     @Test
-    @Disabled
     public void productGreaterPriceAndStock() {
         Smt2Writer writer = new Smt2Writer();
-        boolean succeed = writer.addTableCheckExpression(CheckExpressionFrom("(\"PRICE\">1000 AND \"STOCK\">5)"));
+        boolean succeed = writer.addTableCheckExpression(CheckExpressionFrom("(\"PRICE\">1000)"));
+        succeed = succeed && writer.addTableCheckExpression(CheckExpressionFrom("(\"STOCK\">=5)"));
 
         assertTrue(succeed);
 
         String text = writer.asText();
 
         String expected = "(set-logic QF_LIA)\n" +
-                "(declare-const price Int)\n" +
-                "(declare-const stock Int)\n" +
-                "(assert (and (> price 1000) (> stock 5)))\n" +
+                "(declare-const PRICE Int)\n" +
+                "(declare-const STOCK Int)\n" +
+                "(assert (> PRICE 1000))\n" +
+                "(assert (>= STOCK 5))\n" +
                 "(check-sat)\n" +
-                "(get-value (price stock))\n";
+                "(get-value (PRICE))\n" +
+                "(get-value (STOCK))\n";
 
         assertEquals(expected, text);
     }
