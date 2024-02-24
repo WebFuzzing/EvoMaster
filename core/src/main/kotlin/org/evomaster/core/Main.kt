e14adcb937c3da36cd6493ca7e242992d45292d3
Chiara <31589958+CTarzia@users.noreply.github.com>
2024-02-24 12:21:57 -0300

    undo changes

--- core/src/main/kotlin/org/evomaster/core/Main.kt
+++ core/src/main/kotlin/org/evomaster/core/Main.kt
@@ -176,10 +176,6 @@ class Main {
 
             var solution = run(injector, controllerInfo)
 
-            if (config.epaCalculation) {
-                LoggingUtil.getInfoLogger().info("Going to write EPA to ${config.epaFile}.")
-                writeEPA(solution, config.epaFile)
-            }
             //save data regarding the search phase
             writeOverallProcessData(injector)
             writeDependencies(injector)
@@ -230,6 +226,11 @@ class Main {
             writeCoveredTargets(injector, solution)
             writeTests(injector, solution, controllerInfo)
             writeStatistics(injector, solution) //FIXME if other phases after search, might get skewed data on 100% snapshots...
+
+            if (config.epaCalculation) {
+                LoggingUtil.getInfoLogger().info("Going to write EPA to ${config.epaFile}.")
+                writeEPA(solution, config.epaFile)
+            }
 
             val statistics = injector.getInstance(Statistics::class.java)
             val data = statistics.getData(solution)
