1f7893692e1b09b0a51fa59b47da1287a1c3b6b7
Onur D <dumanonur@gmail.com>
2024-03-08 16:22:21 +0100

    Code cleanup and removing unused imports.

--- e2e-tests/spring-rest-openapi-v2/src/test/java/org/evomaster/e2etests/spring/examples/authenticatedswaggeraccesstest/UnauthenticatedSwaggerAccessManualTest.java
+++ e2e-tests/spring-rest-openapi-v2/src/test/java/org/evomaster/e2etests/spring/examples/authenticatedswaggeraccesstest/UnauthenticatedSwaggerAccessManualTest.java
@@ -3,9 +3,6 @@ package org.evomaster.e2etests.spring.examples.authenticatedswaggeraccesstest;
 import com.foo.rest.examples.spring.unauthenticatedswaggeraccesscontroller.UnauthenticatedSwaggerAccessController;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Paths;
-import org.evomaster.client.java.controller.api.dto.auth.AuthenticationDto;
-import org.evomaster.core.problem.httpws.auth.AuthenticationHeader;
-import org.evomaster.core.problem.httpws.auth.HttpWsAuthenticationInfo;
 import org.evomaster.core.problem.httpws.auth.HttpWsNoAuth;
 import org.evomaster.core.problem.rest.OpenApiAccess;
 import org.evomaster.e2etests.spring.examples.SpringTestBase;
@@ -13,33 +10,25 @@ import org.junit.Assert;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class UnauthenticatedSwaggerAccessManualTest extends SpringTestBase {
-
-    private static UnauthenticatedSwaggerAccessController controller;
 
     @BeforeAll
     public static void initClass() throws Exception {
 
-        controller = new UnauthenticatedSwaggerAccessController();
+        UnauthenticatedSwaggerAccessController controller = new UnauthenticatedSwaggerAccessController();
         SpringTestBase.initClass(controller);
     }
 
     /**
      * Since the swagger endpoint is authenticated, it cannot be retrieved using no authentication object
-     * @throws Throwable
      */
     @Test
-    public void accessSwaggerUnauthenticatedShouldSucceed() throws Throwable {
+    public void accessSwaggerUnauthenticatedShouldSucceed() {
 
         // get all paths from the swagger
         OpenAPI swagger = OpenApiAccess.INSTANCE.getOpenAPIFromURL(baseUrlOfSut + "/v2/api-docs", new HttpWsNoAuth());
 
-        // api paths
-        Paths apiPaths = swagger.getPaths();
-
+        // api paths should not be null
         Assert.assertNotNull(swagger.getPaths());
 
     }
