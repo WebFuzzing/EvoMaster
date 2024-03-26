56878206b4dc3558c69fe6ccc9567c994fbf0760
Man Zhang <man.mzhang@gmail.com>
2024-03-26 23:05:18 +0800

    modify createIndividual

--- core-it/src/test/kotlin/org/evomaster/core/problem/rest/IntegrationTestRestBase.kt
+++ core-it/src/test/kotlin/org/evomaster/core/problem/rest/IntegrationTestRestBase.kt
@@ -3,6 +3,7 @@ package org.evomaster.core.problem.rest
 import com.google.inject.Injector
 import org.evomaster.core.problem.enterprise.SampleType
 import org.evomaster.core.problem.rest.service.AbstractRestFitness
+import org.evomaster.core.problem.rest.service.AbstractRestSampler
 import org.evomaster.core.search.EvaluatedIndividual
 import org.evomaster.core.search.service.SearchGlobalState
 import org.evomaster.core.seeding.service.rest.PirToRest
@@ -28,10 +29,18 @@ abstract class IntegrationTestRestBase : RestTestBase() {
 
     fun createIndividual(actions: List<RestCallAction>): EvaluatedIndividual<RestIndividual> {
 
-        val searchGlobalState = injector.getInstance(SearchGlobalState::class.java)
+//        val searchGlobalState = injector.getInstance(SearchGlobalState::class.java)
 
-        val ind = RestIndividual(actions.toMutableList(), SampleType.SEEDED)
-        ind.doGlobalInitialize(searchGlobalState)
+//        val ind = RestIndividual(actions.toMutableList(), SampleType.SEEDED)
+//        ind.doGlobalInitialize(searchGlobalState)
+
+        val sampler = injector.getInstance(AbstractRestSampler::class.java)
+        /*
+            the method `createIndividual` can be overridden
+            in this case, the sampler is an instance of ResourceSampler,
+            then check its implementation in ResourceSampler.createIndividual(...)
+         */
+        val ind = sampler.createIndividual(SampleType.SEEDED, actions.toMutableList())
 
         val ff = injector.getInstance(AbstractRestFitness::class.java)
         val ei = ff.calculateCoverage(ind)!!
