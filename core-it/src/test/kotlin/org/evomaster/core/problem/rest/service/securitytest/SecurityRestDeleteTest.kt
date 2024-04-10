9a38e9626beab047ce694f17654a1a2ec00ae06e
arcuri82 <arcuri82@gmail.com>
2024-04-10 14:45:36 +0200

    template IT for delete auth

---
+++ core-it/src/test/kotlin/org/evomaster/core/problem/rest/service/securitytest/SecurityRestDeleteTest.kt
@@ -0,0 +1,23 @@
+package org.evomaster.core.problem.rest.service.securitytest
+
+import org.evomaster.core.problem.rest.IntegrationTestRestBase
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+
+class SecurityRestDeleteTest : IntegrationTestRestBase() {
+
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun init() {
+           // initClass(PathStatusController())
+        }
+    }
+
+
+    @Test
+    fun testDeletePut(){
+
+        //TODO
+    }
+}
