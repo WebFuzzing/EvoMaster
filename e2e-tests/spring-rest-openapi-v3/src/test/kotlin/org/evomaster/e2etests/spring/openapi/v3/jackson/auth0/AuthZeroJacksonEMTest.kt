ad862c6a1af8dd1967408000eda3451dfaf1d5f4
seran <7030273+seran@users.noreply.github.com>
2024-05-10 19:26:51 +0200

    missing import

--- e2e-tests/spring-rest-openapi-v3/src/test/kotlin/org/evomaster/e2etests/spring/openapi/v3/jackson/auth0/AuthZeroJacksonEMTest.kt
+++ e2e-tests/spring-rest-openapi-v3/src/test/kotlin/org/evomaster/e2etests/spring/openapi/v3/jackson/auth0/AuthZeroJacksonEMTest.kt
@@ -5,6 +5,7 @@ import org.evomaster.core.problem.rest.HttpVerb
 import org.evomaster.e2etests.spring.openapi.v3.SpringTestBase
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class AuthZeroJacksonEMTest: SpringTestBase() {
