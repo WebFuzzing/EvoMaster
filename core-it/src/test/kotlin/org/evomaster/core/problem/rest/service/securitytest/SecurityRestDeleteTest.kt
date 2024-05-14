150166ef55aa4cb0c0313ca46b28656db64834f8
Onur D <dumanonur@gmail.com>
2024-05-14 19:41:57 +0200

    Disable security integration test for now, just put initclass to the initialization.

--- core-it/src/test/kotlin/org/evomaster/core/problem/rest/service/securitytest/SecurityRestDeleteTest.kt
+++ core-it/src/test/kotlin/org/evomaster/core/problem/rest/service/securitytest/SecurityRestDeleteTest.kt
@@ -1,6 +1,8 @@
 package org.evomaster.core.problem.rest.service.securitytest
 
+import bar.examples.it.spring.pathstatus.PathStatusController
 import org.evomaster.core.problem.rest.IntegrationTestRestBase
+import org.junit.Ignore
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
@@ -11,7 +13,7 @@ class SecurityRestDeleteTest : IntegrationTestRestBase() {
         @BeforeAll
         @JvmStatic
         fun init() {
-           // initClass(PathStatusController())
+            initClass(PathStatusController())
         }
     }
 
