6d670537a9a417d2f4c7042a6c0d9e8b440afb15
Onur D <dumanonur@gmail.com>
2024-07-03 13:59:24 +0300

    First try for crreating individual using parameters.

--- core/src/main/kotlin/org/evomaster/core/seeding/service/rest/PirToRest.kt
+++ core/src/main/kotlin/org/evomaster/core/seeding/service/rest/PirToRest.kt
@@ -1,11 +1,13 @@
 package org.evomaster.core.seeding.service.rest
 
 import com.google.inject.Inject
+import org.evomaster.core.problem.api.param.Param
 import org.evomaster.core.problem.enterprise.auth.AuthSettings
 import org.evomaster.core.problem.rest.HttpVerb
 import org.evomaster.core.problem.rest.RestCallAction
 import org.evomaster.core.problem.rest.param.PathParam
 import org.evomaster.core.problem.rest.service.AbstractRestSampler
+import org.evomaster.core.search.gene.Gene
 import org.evomaster.core.search.service.Randomness
 import org.evomaster.core.seeding.service.PirToIndividual
 import org.slf4j.Logger
@@ -76,6 +78,54 @@ class PirToRest: PirToIndividual(){
                 }
             }
         }
+
+        return x
+    }
+
+    fun fromVerbPathWithQueryParameters(verb: String, path: String, params: MutableMap<String, Gene>) : RestCallAction?{
+
+        val v = try{HttpVerb.valueOf(verb.uppercase())}
+        catch (e: IllegalArgumentException){
+            log.warn("Unrecognized http verb: $verb")
+            return null
+        }
+
+        val candidates = actionDefinitions.filter { it.verb == v }
+            .filter { it.path.matches(path) }
+
+        if(candidates.isEmpty()){
+            log.warn("No match for endpoint path: $path")
+            return null
+        }
+
+        if(candidates.size > 1){
+            log.warn("Ambiguity issue, as ${candidates.size} endpoints are a match for: $path")
+        }
+
+        val x = candidates[0].copy() as RestCallAction
+        x.doInitialize(randomness)
+
+        x.parameters.forEach { p ->
+            when(p){
+                is PathParam -> {
+                    val toSeed = x.path.getKeyValues(path)?.get(p.name)!!
+                    val isSet = p.gene.setFromStringValue(toSeed)
+                    if(!isSet){
+                        log.warn("Failed to update path parameter ${p.name} with value: $toSeed")
+                        return null
+                    }
+                }
+                else -> {
+                    //TODO other cases
+                }
+            }
+        }
+
+        for (p in params) {
+
+            x.addParam( Param(p, params.get(p)));
+        }
+        //x.parameters = params;
 
         return x
     }
