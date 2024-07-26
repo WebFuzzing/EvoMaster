267e3fa8ec0bbb9350faf9dd9c5dafc48cec0d35
Man Zhang <man.mzhang@gmail.com>
2024-07-26 17:06:00 +0800

    add methods to extract info

--- client-java/sql-dto/src/main/java/org/evomaster/client/java/controller/api/dto/database/operations/InsertionDto.java
+++ client-java/sql-dto/src/main/java/org/evomaster/client/java/controller/api/dto/database/operations/InsertionDto.java
@@ -2,6 +2,8 @@ package org.evomaster.client.java.controller.api.dto.database.operations;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class InsertionDto {
 
@@ -17,4 +19,22 @@ public class InsertionDto {
     public String targetTable;
 
     public List<InsertionEntryDto> data = new ArrayList<>();
+
+    /**
+     *
+     * @param filter specifies which column should be returned, null means all columns should be returned
+     * @return name of columns based on specified filter
+     */
+    public List<String> extractColumnNames(Set<String> filter){
+        return data.stream().filter(i-> (filter == null) || filter.stream().anyMatch(f-> i.variableName.equalsIgnoreCase(f))).map(i-> i.variableName).collect(Collectors.toList());
+    }
+
+    /**
+     *
+     * @param filter specifies which column should be returned, null means all columns should be returned
+     * @return printable value of columns based on specified filter
+     */
+    public List<String> extractColumnPrintableValues(Set<String> filter){
+        return data.stream().filter(i-> (filter == null) || filter.stream().anyMatch(f-> i.variableName.equalsIgnoreCase(f))).map(i-> i.printableValue).collect(Collectors.toList());
+    }
 }
