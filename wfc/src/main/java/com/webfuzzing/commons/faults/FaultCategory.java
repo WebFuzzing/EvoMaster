4746bffca101daa911a7a52d60420c49d9dede16
arcuri82 <arcuri82@gmail.com>
2024-08-22 23:25:54 +0200

    unit test for unique codes

--- wfc/src/main/java/com/webfuzzing/commons/faults/FaultCategory.java
+++ wfc/src/main/java/com/webfuzzing/commons/faults/FaultCategory.java
@@ -2,14 +2,19 @@ package com.webfuzzing.commons.faults;
 
 public enum FaultCategory {
 
+    /*
+        TODO
+        code label are still up to discussion and re-arrangement...
+     */
+
     //1xx: HTTP
     HTTP_STATUS_500(100, "HTTP Status 500"),
     HTTP_INVALID_PAYLOAD_SYNTAX(101, "Invalid Payload Syntax"),
     HTTP_INVALID_LOCATION(102, "Invalid Location HTTP Header"),
 
     //2xx: REST/OpenAPI
-    OPENAPI_UNDECLARED_STATUS(200, "Undeclared HTTP Status Code"),
-    INVALID_RESPONSE(201, "Invalid HTTP Response Object"),
+    OPENAPI_UNDECLARED_STATUS(200, "Undeclared Returned HTTP Status Code In The Schema"),
+    OPENAPI_INVALID_RESPONSE(201, "Invalid HTTP Response Object According To The Schema"),
 
     //3xx: GraphQL
     GQL_ERROR_FIELD(301, "Error Field"),
