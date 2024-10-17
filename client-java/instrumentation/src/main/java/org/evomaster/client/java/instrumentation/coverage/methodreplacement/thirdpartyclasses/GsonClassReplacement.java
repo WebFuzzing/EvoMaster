193cd722531ee935dd1b4a5a7d5c88199ff8a981
seran <7030273+seran@users.noreply.github.com>
2024-10-17 12:46:47 +0200

    wip: Gson method replacements

--- client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/coverage/methodreplacement/thirdpartyclasses/GsonClassReplacement.java
+++ client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/coverage/methodreplacement/thirdpartyclasses/GsonClassReplacement.java
@@ -8,8 +8,12 @@ import org.evomaster.client.java.instrumentation.coverage.methodreplacement.Usag
 import org.evomaster.client.java.instrumentation.object.ClassToSchema;
 
 
+import java.io.IOException;
+import java.io.Reader;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.nio.CharBuffer;
 
 public class GsonClassReplacement extends ThirdPartyMethodReplacementClass {
 
@@ -27,24 +31,120 @@ public class GsonClassReplacement extends ThirdPartyMethodReplacementClass {
             id = "fromJson_string_class",
             usageFilter = UsageFilter.ANY,
             category = ReplacementCategory.BASE)
-    public static Object fromJson(Object caller, String json, Class<?> classOfT){
+    public static Object fromJson(Object caller, String json, Class<?> classOfT) {
 
-        if(caller == null){
+        if (caller == null) {
             throw new NullPointerException();
         }
 
         ClassToSchema.registerSchemaIfNeeded(classOfT);
-        JsonTaint.handlePossibleJsonTaint(json,classOfT);
+        JsonTaint.handlePossibleJsonTaint(json, classOfT);
 
         Method original = getOriginal(singleton, "fromJson_string_class", caller);
 
         try {
             return original.invoke(caller, json, classOfT);
-        } catch (IllegalAccessException e){
+        } catch (IllegalAccessException e) {
             throw new RuntimeException(e);// ah, the beauty of Java...
-        } catch (InvocationTargetException e){
+        } catch (InvocationTargetException e) {
             throw (RuntimeException) e.getCause();
         }
+    }
+
+    @Replacement(
+            replacingStatic = false,
+            type = ReplacementType.TRACKER,
+            id = "fromJson_string_type",
+            usageFilter = UsageFilter.ANY,
+            category = ReplacementCategory.BASE
+    )
+    public static Object fromJson(Object caller, String json, Type typeOfT) {
+        if (caller == null) {
+            throw new NullPointerException();
+        }
+
+        Class<?> klass = (Class<?>) typeOfT;
+
+        ClassToSchema.registerSchemaIfNeeded(klass);
+        JsonTaint.handlePossibleJsonTaint(json, klass);
+
+        Method original = getOriginal(singleton, "fromJson_string_type", caller);
+
+        try {
+            return original.invoke(caller, json, typeOfT);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw (RuntimeException) e.getCause();
+        }
+    }
+
+    @Replacement(
+            replacingStatic = false,
+            type = ReplacementType.TRACKER,
+            id = "fromJson_reader_class",
+            usageFilter = UsageFilter.ANY,
+            category = ReplacementCategory.BASE
+    )
+    public static Object fromJson(Object caller, Reader json, Class<?> classOfT) {
+        if (caller == null) {
+            throw new NullPointerException();
+        }
+
+        ClassToSchema.registerSchemaIfNeeded(classOfT);
+        JsonTaint.handlePossibleJsonTaint(getStringFromReader(json), classOfT);
+
+        Method original = getOriginal(singleton, "fromJson_reader_class", caller);
+
+        try {
+            return original.invoke(caller, json, classOfT);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw (RuntimeException) e.getCause();
+        }
+    }
+
+    @Replacement(
+            replacingStatic = false,
+            type = ReplacementType.TRACKER,
+            id = "fromJson_reader_type",
+            usageFilter = UsageFilter.ANY,
+            category = ReplacementCategory.BASE
+    )
+    public static Object fromJson(Object caller, Reader json, Type typeOfT) {
+        if (caller == null) {
+            throw new NullPointerException();
+        }
+
+        Class<?> klass = (Class<?>) typeOfT;
+        ClassToSchema.registerSchemaIfNeeded(klass);
+        JsonTaint.handlePossibleJsonTaint(getStringFromReader(json), klass);
+
+        Method original = getOriginal(singleton, "fromJson_reader_type", caller);
+
+        try {
+            return original.invoke(caller, json, klass);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw (RuntimeException) e.getCause();
+        }
+    }
+
+    private static String getStringFromReader(Reader reader) {
+        String s = "";
+        int character;
+
+        try {
+            while ((character = reader.read()) != -1) {
+                s += (char) character;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return s;
     }
 
 }
---
+++ client-java/instrumentation/src/test/java/org/evomaster/client/java/instrumentation/coverage/methodreplacement/thirdpartyclasses/GsonReplacementTest.java
@@ -0,0 +1,101 @@
+package org.evomaster.client.java.instrumentation.coverage.methodreplacement.thirdpartyclasses;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.evomaster.client.java.instrumentation.AdditionalInfo;
+import org.evomaster.client.java.instrumentation.staticstate.ExecutionTracer;
+import org.evomaster.client.java.instrumentation.staticstate.UnitsInfoRecorder;
+import org.junit.Test;
+
+import java.io.Reader;
+import java.io.StringReader;
+import java.lang.reflect.Type;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GsonReplacementTest {
+
+    @Test
+    public void testFromJsonClassOfT() throws Throwable {
+        String json = "{\n\"color\": \"red\"\n}";
+
+        Gson gson = new Gson();
+        GsonClassReplacement.fromJson(gson, json, GsonTestDto.class);
+
+        Map<String, String> parsedDto = UnitsInfoRecorder.getInstance().getParsedDtos();
+        List<AdditionalInfo> additionalInfoList = ExecutionTracer.exposeAdditionalInfoList();
+
+        Set<String> infoList = new HashSet<>();
+        additionalInfoList.forEach(info -> {
+            infoList.addAll(info.getParsedDtoNamesView());
+        });
+        assertTrue(parsedDto.containsKey(GsonTestDto.class.getName()));
+        assertTrue(infoList.contains(GsonTestDto.class.getName()));
+
+    }
+
+    @Test
+    public void testFromJsonType() throws Throwable {
+        String json = "{\n\"color\": \"red\"\n}";
+
+        Gson gson = new Gson();
+        Type dtoType = new TypeToken<GsonTestDto>() {}.getType();
+        GsonClassReplacement.fromJson(gson, json, dtoType);
+
+        Map<String, String> parsedDto = UnitsInfoRecorder.getInstance().getParsedDtos();
+        List<AdditionalInfo> additionalInfoList = ExecutionTracer.exposeAdditionalInfoList();
+
+        Set<String> infoList = new HashSet<>();
+        additionalInfoList.forEach(info -> {
+            infoList.addAll(info.getParsedDtoNamesView());
+        });
+        assertTrue(parsedDto.containsKey(GsonTestDto.class.getName()));
+        assertTrue(infoList.contains(GsonTestDto.class.getName()));
+    }
+
+    @Test
+    public void testFromJsonReaderClassOfT() throws Throwable {
+        String json = "{\n\"color\": \"red\"\n}";
+
+        Reader reader = new StringReader(json);
+
+        Gson gson = new Gson();
+
+        GsonClassReplacement.fromJson(gson, reader, GsonTestDto.class);
+
+        Map<String, String> parsedDto = UnitsInfoRecorder.getInstance().getParsedDtos();
+        List<AdditionalInfo> additionalInfoList = ExecutionTracer.exposeAdditionalInfoList();
+
+        Set<String> infoList = new HashSet<>();
+        additionalInfoList.forEach(info -> {
+            infoList.addAll(info.getParsedDtoNamesView());
+        });
+        assertTrue(parsedDto.containsKey(GsonTestDto.class.getName()));
+        assertTrue(infoList.contains(GsonTestDto.class.getName()));
+    }
+
+    @Test
+    public void testFromJsonReaderType() throws Throwable {
+        String json = "{\n\"color\": \"red\"\n}";
+
+        Reader reader = new StringReader(json);
+
+        Gson gson = new Gson();
+        Type dtoType = new TypeToken<GsonTestDto>() {}.getType();
+        GsonClassReplacement.fromJson(gson, reader, dtoType);
+
+        Map<String, String> parsedDto = UnitsInfoRecorder.getInstance().getParsedDtos();
+        List<AdditionalInfo> additionalInfoList = ExecutionTracer.exposeAdditionalInfoList();
+
+        Set<String> infoList = new HashSet<>();
+        additionalInfoList.forEach(info -> {
+            infoList.addAll(info.getParsedDtoNamesView());
+        });
+        assertTrue(parsedDto.containsKey(GsonTestDto.class.getName()));
+        assertTrue(infoList.contains(GsonTestDto.class.getName()));
+    }
+}
