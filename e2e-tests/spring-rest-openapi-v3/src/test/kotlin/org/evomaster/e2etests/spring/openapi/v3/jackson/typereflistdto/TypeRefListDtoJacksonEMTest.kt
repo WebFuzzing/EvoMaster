cbbaf5da39e53bb7a4e1508806961e2be9eede4b
arcuri82 <arcuri82@gmail.com>
2024-11-01 15:26:59 +0100

    handling of TypeRef for lists

--- e2e-tests/spring-rest-openapi-v3/src/test/kotlin/org/evomaster/e2etests/spring/openapi/v3/jackson/typereflistdto/TypeRefListDtoJacksonEMTest.kt
+++ e2e-tests/spring-rest-openapi-v3/src/test/kotlin/org/evomaster/e2etests/spring/openapi/v3/jackson/typereflistdto/TypeRefListDtoJacksonEMTest.kt
@@ -1,8 +1,10 @@
-package org.evomaster.e2etests.spring.openapi.v3.jackson.mapdto
+package org.evomaster.e2etests.spring.openapi.v3.jackson.typereflistdto
 
 import com.foo.rest.examples.spring.openapi.v3.jackson.mapdouble.MapDoubleJacksonController
 import com.foo.rest.examples.spring.openapi.v3.jackson.mapdto.MapDtoJacksonController
 import com.foo.rest.examples.spring.openapi.v3.jackson.maplistint.MapListIntJacksonController
+import com.foo.rest.examples.spring.openapi.v3.jackson.typereflistdto.TypeRefListDtoJacksonController
+import com.foo.rest.examples.spring.openapi.v3.jackson.typereflistint.TypeRefListIntJacksonController
 import org.evomaster.core.problem.rest.HttpVerb
 import org.evomaster.e2etests.spring.openapi.v3.SpringTestBase
 import org.junit.jupiter.api.Assertions
@@ -10,22 +12,21 @@ import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
-class MapDtoJacksonEMTest : SpringTestBase() {
+class TypeRefListDtoJacksonEMTest : SpringTestBase() {
 
     companion object {
         @BeforeAll
         @JvmStatic
         fun init() {
-            initClass(MapDtoJacksonController())
+            initClass(TypeRefListDtoJacksonController())
         }
     }
 
-    @Disabled("We don't handle this case yet... also, how common would it be???")
     @Test
     fun basicEMTest() {
         runTestHandlingFlakyAndCompilation(
-            "MapDtoJacksonEM",
-            2000
+            "TypeRefListDtoJacksonEM",
+            500
         ) { args: List<String> ->
 
             setOption(args, "taintForceSelectionOfGenesWithSpecialization", "true")
@@ -33,7 +34,7 @@ class MapDtoJacksonEMTest : SpringTestBase() {
             val solution = initAndRun(args)
 
             Assertions.assertTrue(solution.individuals.size >= 1)
-            assertHasAtLeastOne(solution, HttpVerb.POST, 200, "/api/jackson/mapdto", "Working")
+            assertHasAtLeastOne(solution, HttpVerb.POST, 200, "/api/jackson/typereflistdto", "Working")
         }
     }
 }
--- e2e-tests/spring-rest-openapi-v3/src/test/kotlin/org/evomaster/e2etests/spring/openapi/v3/jackson/typereflistint/TypeRefListIntJacksonEMTest.kt
+++ e2e-tests/spring-rest-openapi-v3/src/test/kotlin/org/evomaster/e2etests/spring/openapi/v3/jackson/typereflistint/TypeRefListIntJacksonEMTest.kt
@@ -1,8 +1,9 @@
-package org.evomaster.e2etests.spring.openapi.v3.jackson.mapdto
+package org.evomaster.e2etests.spring.openapi.v3.jackson.typereflistint
 
 import com.foo.rest.examples.spring.openapi.v3.jackson.mapdouble.MapDoubleJacksonController
 import com.foo.rest.examples.spring.openapi.v3.jackson.mapdto.MapDtoJacksonController
 import com.foo.rest.examples.spring.openapi.v3.jackson.maplistint.MapListIntJacksonController
+import com.foo.rest.examples.spring.openapi.v3.jackson.typereflistint.TypeRefListIntJacksonController
 import org.evomaster.core.problem.rest.HttpVerb
 import org.evomaster.e2etests.spring.openapi.v3.SpringTestBase
 import org.junit.jupiter.api.Assertions
@@ -10,22 +11,21 @@ import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
-class MapDtoJacksonEMTest : SpringTestBase() {
+class TypeRefListIntJacksonEMTest : SpringTestBase() {
 
     companion object {
         @BeforeAll
         @JvmStatic
         fun init() {
-            initClass(MapDtoJacksonController())
+            initClass(TypeRefListIntJacksonController())
         }
     }
 
-    @Disabled("We don't handle this case yet... also, how common would it be???")
     @Test
     fun basicEMTest() {
         runTestHandlingFlakyAndCompilation(
-            "MapDtoJacksonEM",
-            2000
+            "TypeRefListIntJacksonEM",
+            500
         ) { args: List<String> ->
 
             setOption(args, "taintForceSelectionOfGenesWithSpecialization", "true")
@@ -33,7 +33,7 @@ class MapDtoJacksonEMTest : SpringTestBase() {
             val solution = initAndRun(args)
 
             Assertions.assertTrue(solution.individuals.size >= 1)
-            assertHasAtLeastOne(solution, HttpVerb.POST, 200, "/api/jackson/mapdto", "Working")
+            assertHasAtLeastOne(solution, HttpVerb.POST, 200, "/api/jackson/typereflistint", "Working")
         }
     }
 }
