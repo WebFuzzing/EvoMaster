76dd89c99877717da981532781adb36ab85f6578
Man Zhang <man.mzhang@gmail.com>
2024-11-05 00:27:52 +0800

    add ScheduleTaskAction

---
+++ core/src/main/kotlin/org/evomaster/core/problem/scheduletask/ScheduleTaskAction.kt
@@ -0,0 +1,28 @@
+package org.evomaster.core.problem.scheduletask
+
+import org.evomaster.core.problem.api.param.Param
+import org.evomaster.core.search.action.EnvironmentAction
+import org.evomaster.core.search.gene.Gene
+
+class ScheduleTaskAction(
+    val taskId: String,
+    parameters: MutableList<Param>,
+): EnvironmentAction(parameters) {
+
+    val parameters : List<Param>
+        get() { return children as List<Param>}
+
+    override fun getName(): String {
+        return taskId
+    }
+
+    override fun seeTopGenes(): List<out Gene> {
+        return parameters.flatMap { it.seeGenes() }
+    }
+
+
+    override fun copyContent(): ScheduleTaskAction {
+        val p = parameters.asSequence().map(Param::copy).toMutableList()
+        return ScheduleTaskAction(taskId, p)
+    }
+}
