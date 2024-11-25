953409ec2c8a87e4af1c84c05cb1583071687c69
Agustina Aldasoro <agusaldasoro@users.noreply.github.com>
2024-11-24 22:56:58 -0300

    Update core/src/main/kotlin/org/evomaster/core/problem/api/service/ApiWsStructureMutator.kt

--- core/src/main/kotlin/org/evomaster/core/problem/api/service/ApiWsStructureMutator.kt
+++ core/src/main/kotlin/org/evomaster/core/problem/api/service/ApiWsStructureMutator.kt
@@ -47,9 +47,7 @@ abstract class ApiWsStructureMutator : StructureMutator() {
     @Inject
     protected lateinit var harvestResponseHandler: HarvestActualHttpWsResponseHandler
 
-    // TODO: This should only be initialized when config.generateSqlDataWithDSE is enabled
-    // TODO: Also, z3solver.close() should be invoked
-    // when the application is shutting down to stop the Docker container and clean the tmp folder
+    // TODO: z3solver.close() should be invoked when the application is shutting down to stop the Docker container and clean the tmp folder
     @Inject
     protected lateinit var z3Solver: SMTLibZ3DbConstraintSolver
 
