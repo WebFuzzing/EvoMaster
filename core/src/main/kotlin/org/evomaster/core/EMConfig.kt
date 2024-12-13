2ad6fd2a5ce6924f3335e183d068c22cb3491884
arcuri82 <arcuri82@gmail.com>
2024-12-13 22:26:42 +0100

    enabling test comments. added info on found faults

--- core/src/main/kotlin/org/evomaster/core/EMConfig.kt
+++ core/src/main/kotlin/org/evomaster/core/EMConfig.kt
@@ -2398,9 +2398,8 @@ class EMConfig {
             " instead of OFF.")
     val probabilityOfOnVsOffInAllOptionals = 0.8
 
-    @Experimental
     @Cfg("Add summary comments on each test")
-    var addTestComments = false
+    var addTestComments = true
 
     @Min(1.0)
     @Cfg("Max length for test comments. Needed when enumerating some names/values, making comments too long to be" +
