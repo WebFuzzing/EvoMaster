4897586968d69756b96f17440e2b6995e8b549d8
Man Zhang <man.mzhang@gmail.com>
2025-01-24 08:27:25 +0800

    add comments for dto

--- client-java/controller-api/src/main/java/org/evomaster/client/java/controller/api/dto/problem/rpc/ScheduleTaskInvocationsDto.java
+++ client-java/controller-api/src/main/java/org/evomaster/client/java/controller/api/dto/problem/rpc/ScheduleTaskInvocationsDto.java
@@ -3,8 +3,11 @@ package org.evomaster.client.java.controller.api.dto.problem.rpc;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ScheduleTaskInvocationsDto {
 
+/**
+ * to reduce times of making http calls, all schedule tasks in a test will be handled one time.
+ */
+public class ScheduleTaskInvocationsDto {
 
     public List<ScheduleTaskInvocationDto> tasks = new ArrayList<>();
 }
--- client-java/controller-api/src/main/java/org/evomaster/client/java/controller/api/dto/problem/rpc/ScheduleTaskInvocationsResult.java
+++ client-java/controller-api/src/main/java/org/evomaster/client/java/controller/api/dto/problem/rpc/ScheduleTaskInvocationsResult.java
@@ -3,6 +3,9 @@ package org.evomaster.client.java.controller.api.dto.problem.rpc;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * this dto is used to save info on a sequence of invoked schedule tasks
+ */
 public class ScheduleTaskInvocationsResult {
 
     public List<ScheduleTaskInvocationResultDto> results = new ArrayList<>();
