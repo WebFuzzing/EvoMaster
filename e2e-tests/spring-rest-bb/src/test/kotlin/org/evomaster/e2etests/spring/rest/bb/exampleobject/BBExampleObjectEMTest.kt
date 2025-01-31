cf642397194fd908e5eda50f26123e4ab0389e74
arcuri82 <arcuri82@gmail.com>
2025-01-31 22:45:39 +0100

    explicit testing targets for examples

--- e2e-tests/spring-rest-bb/src/test/kotlin/org/evomaster/e2etests/spring/rest/bb/exampleobject/BBExampleObjectEMTest.kt
+++ e2e-tests/spring-rest-bb/src/test/kotlin/org/evomaster/e2etests/spring/rest/bb/exampleobject/BBExampleObjectEMTest.kt
@@ -4,6 +4,7 @@ import com.foo.rest.examples.bb.exampleobject.BBExampleObjectController
 import com.foo.rest.examples.bb.examplevalues.BBExamplesController
 import org.evomaster.core.output.OutputFormat
 import org.evomaster.core.problem.rest.HttpVerb
+import org.evomaster.core.problem.rest.param.BodyParam
 import org.evomaster.e2etests.spring.rest.bb.SpringTestBase
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertTrue
@@ -40,6 +41,22 @@ class BBExampleObjectEMTest : SpringTestBase() {
 
             assertTrue(solution.individuals.size >= 1)
             assertHasAtLeastOne(solution, HttpVerb.POST, 200, "/api/bbexampleobject", "OK")
+
+            val bodies = solution.individuals
+                .asSequence()
+                .map { it.individual }
+                .flatMap { it.seeMainExecutableActions() }
+                .flatMap { it.parameters }
+                .filterIsInstance<BodyParam>()
+                .map { it.primaryGene().getValueAsRawString() }
+                .toList()
+
+            assertTrue(bodies.any { it.contains("Foo") }, "Missing Foo")
+            assertTrue(bodies.any { it.contains("Y0") }, "Missing Y0")
+            assertTrue(bodies.any { it.contains("Y1") }, "Missing Y1")
+            assertTrue(bodies.any { it.contains("Y2") }, "Missing Y2")
+            assertTrue(bodies.any { it.contains("Y3") }, "Missing Y3")
+            assertTrue(bodies.any { it.contains("Y4") }, "Missing Y4")
         }
     }
 }
