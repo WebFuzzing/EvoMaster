43a4e122fcb0e749b1c5083717ae20b6cb4f7b12
Omur <omur@erciyes.edu.tr>
2025-04-06 22:42:25 +0200

    fixing assertions

--- e2e-tests/spring-rest-openapi-v3/src/test/kotlin/org/evomaster/e2etests/spring/openapi/v3/security/pathconflict/PathConflictDeleteEMTest.kt
+++ e2e-tests/spring-rest-openapi-v3/src/test/kotlin/org/evomaster/e2etests/spring/openapi/v3/security/pathconflict/PathConflictDeleteEMTest.kt
@@ -1,6 +1,7 @@
 package org.evomaster.e2etests.spring.openapi.v3.security.pathconflict
 
 import com.foo.rest.examples.spring.openapi.v3.security.pathconflict.PathConflictDeleteController
+import org.evomaster.core.problem.rest.HttpVerb
 import org.evomaster.e2etests.spring.openapi.v3.SpringTestBase
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeAll
@@ -32,15 +33,11 @@ class PathConflictDeleteEMTest : SpringTestBase(){
 
             assertTrue(solution.individuals.size >= 1)
 
-//            assertHasAtLeastOne(solution, HttpVerb.PUT, 201, "/api/resources/{id}", null)
-//            assertHasAtLeastOne(solution, HttpVerb.GET, 200, "/api/resources/{id}", null)
-//            assertHasAtLeastOne(solution, HttpVerb.GET, 403, "/api/resources/{id}", null)
-//            assertHasAtLeastOne(solution, HttpVerb.POST, 401, "/api/resources/", null)
-//
-//
-//            val faults = DetectedFaultUtils.getDetectedFaultCategories(solution)
-//            assertEquals(1, faults.size)
-//            assertEquals(FaultCategory.SECURITY_NOT_RECOGNIZED_AUTHENTICATED, faults.first())
+            assertHasAtLeastOne(solution, HttpVerb.POST, 201, "/api/articles", null)
+            assertHasAtLeastOne(solution, HttpVerb.GET, 200, "/api/articles/{id}", null)
+            assertHasAtLeastOne(solution, HttpVerb.GET, 401, "/api/articles/{id}", null)
+            assertHasAtLeastOne(solution, HttpVerb.POST, 201, "/api/articles/{id}/comments", null)
+
         }
     }
 }
