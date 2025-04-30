69a6d7c857add265a0444397dfece888b2e5fc9c
ivakertusha <iva.kertusha@gmail.com>
2025-04-30 11:26:07 +0200

    different user action types

--- core/src/main/kotlin/org/evomaster/core/problem/webfrontend/UserActionType.kt
+++ core/src/main/kotlin/org/evomaster/core/problem/webfrontend/UserActionType.kt
@@ -15,4 +15,17 @@ enum class UserActionType {
      * Some dropdown <select> elements might allow to select more than one element
      */
     SELECT_MULTI,
+
+    DOUBLE_CLICK,
+    RIGHT_CLICK,
+    HOVER_OVER_ELEMENT,
+    VERTICAL_SCROLLING,
+    HORIZONTAL_SCROLLING,
+    DRAG_AND_DROP,
+
+    CHECK_CHECKBOX,
+    UNCHECK_CHECKBOX,
+    CHOOSE_RADIO_BUTTON,
+    SELECT,
+    UPLOAD_FILE,
 }
