306c1b3dcf4c670464302447e356bb4df746793b
seran <7030273+seran@users.noreply.github.com>
2025-05-14 13:44:45 +0200

    clean-up

--- client-java/instrumentation-shared/src/main/java/org/evomaster/client/java/instrumentation/shared/SecuritySharedUtils.java
+++ client-java/instrumentation-shared/src/main/java/org/evomaster/client/java/instrumentation/shared/SecuritySharedUtils.java
@@ -3,7 +3,7 @@ package org.evomaster.client.java.instrumentation.shared;
 public class SecuritySharedUtils {
 
     /**
-     * WireMock server to verify SSRF requests
+     * Reserved IP address for SSRF verifier.
      */
     public static final String RESERVED_VULNERABILITY_VERIFIER = "127.0.0.4";
 }
