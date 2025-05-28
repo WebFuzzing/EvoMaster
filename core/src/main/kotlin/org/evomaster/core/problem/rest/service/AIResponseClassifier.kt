d2986d98af9ca4dc053ca908b4b62180a4a06726
Mohsen Taheri Shalmani <taherimohsen2000@gmail.com>
2025-05-28 19:11:15 +0200

    Added testAIModel
    I added testAIModel model to make the procedure clear

--- core/src/main/kotlin/org/evomaster/core/problem/rest/service/AIResponseClassifier.kt
+++ core/src/main/kotlin/org/evomaster/core/problem/rest/service/AIResponseClassifier.kt
@@ -3,6 +3,9 @@ package org.evomaster.core.problem.rest.service
 import org.evomaster.core.problem.rest.StatusGroup
 import org.evomaster.core.problem.rest.data.RestCallAction
 import org.evomaster.core.problem.rest.data.RestCallResult
+import org.junit.Test
+import kotlin.random.Random
+
 
 class AIResponseClassifier {
 
@@ -14,6 +17,19 @@ class AIResponseClassifier {
 
         //TODO
         return StatusGroup.G_2xx
+
+    }
+
+    // return the probability of acceptance based on the classifier
+    fun probValidity(input: RestCallAction) : Double{
+
+        // TODO
+        // this part must be fixed based on the gaussian model
+        // for now we just generate a random number assuming the model
+        // provided that
+        val prob2xx = Random.nextDouble(0.0, 1.0)
+        return prob2xx
+
     }
 
 }
