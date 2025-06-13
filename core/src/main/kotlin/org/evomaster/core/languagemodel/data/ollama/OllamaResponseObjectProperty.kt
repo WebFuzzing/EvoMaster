3c0f1b38a71266e10a9fc1ee0c18e1d31593651d
seran <7030273+seran@users.noreply.github.com>
2025-06-13 13:19:42 +0200

    clean-up

--- core/src/main/kotlin/org/evomaster/core/languagemodel/data/ollama/OllamaResponseObjectProperty.kt
+++
@@ -1,8 +0,0 @@
-package org.evomaster.core.languagemodel.data.ollama
-
-class OllamaResponseObjectProperty(
-    override val type: String,
-    val properties: Map<String, OllamaResponseProperty>
-) : OllamaResponseProperty(type) {
-
-}
