01be02e3baaa598abd40c9e6bc8742aacc169775
seran <7030273+seran@users.noreply.github.com>
2025-06-17 09:43:22 +0200

    java docs

--- core/src/main/kotlin/org/evomaster/core/languagemodel/data/ollama/OllamaResponseFormat.kt
+++ core/src/main/kotlin/org/evomaster/core/languagemodel/data/ollama/OllamaResponseFormat.kt
@@ -1,10 +1,22 @@
 package org.evomaster.core.languagemodel.data.ollama
 
+/**
+ * Represents Ollama custom response format request.
+ */
 class OllamaResponseFormat (
     val type: String,
 
+    /**
+     * Represents the response format properties.
+     * Key holds the name of the properties and value holds the property
+     * type as [OllamaResponseProperty].
+     * For array type use [OllamaResponseArrayProperty].
+     */
     val properties: Map<String, OllamaResponseProperty>,
 
+    /**
+     * List to hold required fields on the response.
+     */
     val required: List<String>,
 ) {
 }
