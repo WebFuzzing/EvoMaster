9a4cb2ebd62c2300363f4ce53ed02e8e9017a935
Alexander Szyrej <alexander.szyrej@gmail.com>
2025-07-10 15:41:38 -0300

    removed instrumented tests

--- client-java/instrumentation/src/test/java/org/evomaster/client/java/instrumentation/example/redis/RedisAsyncCommandsOperations.java
+++
@@ -1,7 +0,0 @@
-package org.evomaster.client.java.instrumentation.example.redis;
-
-public interface RedisAsyncCommandsOperations {
-    Object get(String key);
-    Object hget(String key, String hashKey);
-    Object hgetall(String key);
-}
--- client-java/instrumentation/src/test/java/org/evomaster/client/java/instrumentation/example/redis/RedisCommandsOperations.java
+++
@@ -1,9 +0,0 @@
-package org.evomaster.client.java.instrumentation.example.redis;
-
-import java.util.Map;
-
-public interface RedisCommandsOperations {
-    String get(String key);
-    String hget(String key, String hashKey);
-    Map<String,String> hgetall(String key);
-}
