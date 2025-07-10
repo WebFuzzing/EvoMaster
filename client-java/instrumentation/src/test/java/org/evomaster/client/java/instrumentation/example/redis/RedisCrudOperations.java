440790518f3f365c5aeda9a050e268f2cfbdbbbb
Alexander Szyrej <alexander.szyrej@gmail.com>
2025-07-10 15:41:38 -0300

    removed instrumented tests

--- client-java/instrumentation/src/test/java/org/evomaster/client/java/instrumentation/example/redis/RedisCrudOperations.java
+++
@@ -1,7 +0,0 @@
-package org.evomaster.client.java.instrumentation.example.redis;
-
-import java.util.Optional;
-
-public interface RedisCrudOperations {
-    Optional<RedisEntity> findById(String id);
-}
