bce1aae6b5297ac880de1a3664398664ff78c11f
seran <7030273+seran@users.noreply.github.com>
2025-08-27 17:46:18 +0530

    minor change

--- core/src/main/kotlin/org/evomaster/core/search/gene/uri/UrlHttpGene.kt
+++ core/src/main/kotlin/org/evomaster/core/search/gene/uri/UrlHttpGene.kt
@@ -110,7 +110,7 @@ class UrlHttpGene(
             host.setValueBasedOn(url.host)
             port.setValueBasedOn(url.port.toString())
             // This to make the String similar to what is expected in ArrayGene
-            val pathValues = url.path.replace("/", ", ")
+            val pathValues = url.path.drop(1).replace("/", ", ")
             path.setValueBasedOn(pathValues)
             // TODO: handle path
             path.setFromStringValue(url.path)
