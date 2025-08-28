edd29ad31cbe05b2f1de4b64c5a030efde17efa3
seran <7030273+seran@users.noreply.github.com>
2025-08-28 19:40:54 +0530

    fix

--- core/src/test/kotlin/org/evomaster/core/search/gene/ArrayGeneTest.kt
+++ core/src/test/kotlin/org/evomaster/core/search/gene/ArrayGeneTest.kt
@@ -117,7 +117,7 @@ class ArrayGeneTest {
 
         assertEquals(2, gene.getViewOfChildren().size)
 
-        gene.setValueBasedOn("baz,qux,quux")
+        gene.setValueBasedOn("baz, qux, quux")
 
         assertEquals(3, gene.getViewOfChildren().size)
         assertEquals("baz", gene.getViewOfChildren()[0].getValueAsRawString())
