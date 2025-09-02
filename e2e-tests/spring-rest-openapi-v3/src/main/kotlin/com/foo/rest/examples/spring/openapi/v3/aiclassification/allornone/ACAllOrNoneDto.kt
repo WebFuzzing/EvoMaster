78a5298f25fbb07f6887e18eb9206a9501cbf6c4
arcuri82 <arcuri82@gmail.com>
2025-09-02 14:37:37 +0200

    test for allornone

---
+++ e2e-tests/spring-rest-openapi-v3/src/main/kotlin/com/foo/rest/examples/spring/openapi/v3/aiclassification/allornone/ACAllOrNoneDto.kt
@@ -0,0 +1,16 @@
+package com.foo.rest.examples.spring.openapi.v3.aiclassification.allornone
+
+
+class ACAllOrNoneDto(
+
+    var a : Boolean? = null,
+
+    var b : String? = null,
+
+    var c : Long? = null,
+
+    var d : ACAllOrNoneEnum? = null,
+
+    var e : List<String>? = null,
+
+    )
---
+++ e2e-tests/spring-rest-openapi-v3/src/test/kotlin/org/evomaster/e2etests/spring/openapi/v3/aiclassification/allornone/ACAllOrNoneEMTest.kt
@@ -0,0 +1,68 @@
+package org.evomaster.e2etests.spring.openapi.v3.aiclassification.allornone
+
+import com.foo.rest.examples.spring.openapi.v3.aiclassification.allornone.ACAllOrNoneController
+import com.foo.rest.examples.spring.openapi.v3.aiclassification.basic.ACBasicController
+import com.foo.rest.examples.spring.openapi.v3.aiclassification.onlyone.ACOnlyOneController
+import com.foo.rest.examples.spring.openapi.v3.aiclassification.or.ACOrController
+import com.foo.rest.examples.spring.openapi.v3.aiclassification.required.ACRequiredController
+import com.google.inject.Injector
+import org.evomaster.core.EMConfig.AIResponseClassifierModel
+import org.evomaster.core.problem.rest.data.HttpVerb
+import org.evomaster.core.problem.rest.service.AIResponseClassifier
+import org.evomaster.core.seeding.service.rest.PirToRest
+import org.evomaster.e2etests.spring.openapi.v3.aiclassification.AIClassificationEMTestBase
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+
+
+class ACAllOrNoneEMTest : AIClassificationEMTestBase() {
+
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun init() {
+            initClass(ACAllOrNoneController())
+        }
+    }
+
+   // @Disabled
+    @Test
+    fun testRunDeterministic(){
+        testRunEM(AIResponseClassifierModel.DETERMINISTIC)
+    }
+
+    @Disabled
+    @Test
+    fun testRunGaussian(){
+        testRunEM(AIResponseClassifierModel.GAUSSIAN)
+    }
+
+    private fun testRunEM(model: AIResponseClassifierModel) {
+
+        runTestHandlingFlakyAndCompilation(
+            "ACAllOrNoneEM",
+            500
+        ) { args: MutableList<String> ->
+
+            args.add("--aiModelForResponseClassification")
+            args.add("$model")
+
+            val (injector, solution) = initAndDebug(args)
+
+            assertTrue(solution.individuals.size >= 1)
+            assertHasAtLeastOne(solution, HttpVerb.GET, 200, "/api/allornone", "OK")
+            assertHasAtLeastOne(solution, HttpVerb.GET, 400, "/api/allornone", null)
+            assertHasAtLeastOne(solution, HttpVerb.POST, 201, "/api/allornone", "OK")
+            assertHasAtLeastOne(solution, HttpVerb.POST, 400, "/api/allornone", null)
+
+            val ptr = injector.getInstance(PirToRest::class.java)
+
+            //TODO need to deal with body payload in PTR
+
+            //verifyModel(injector) //TODO
+        }
+    }
+}
