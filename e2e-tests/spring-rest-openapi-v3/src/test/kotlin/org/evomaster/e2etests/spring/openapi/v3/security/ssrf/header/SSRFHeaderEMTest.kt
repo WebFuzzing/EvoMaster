ff73d649e68fb80786d81739c7f28d565c584420
seran <7030273+seran@users.noreply.github.com>
2025-09-03 13:44:49 +0530

    minor change

--- e2e-tests/spring-rest-openapi-v3/src/test/kotlin/org/evomaster/e2etests/spring/openapi/v3/security/ssrf/header/SSRFHeaderEMTest.kt
+++ e2e-tests/spring-rest-openapi-v3/src/test/kotlin/org/evomaster/e2etests/spring/openapi/v3/security/ssrf/header/SSRFHeaderEMTest.kt
@@ -22,7 +22,7 @@ class SSRFHeaderEMTest: SpringTestBase() {
     @Test
     fun testSSRFHeader() {
         runTestHandlingFlakyAndCompilation(
-            "SSRFEMTest",
+            "SSRFHeaderEMTest",
             80,
         ) { args: MutableList<String> ->
 
@@ -38,7 +38,7 @@ class SSRFHeaderEMTest: SpringTestBase() {
 
             Assertions.assertTrue(solution.individuals.isNotEmpty())
 
-            // TODO: Need to modify this to test for executed calls inside [SSRFAnalyser]
+            // TODO: Need to modify this to test to check for SSRF faults
             assertHasAtLeastOne(solution, HttpVerb.GET, 200, "/api/header", "OK")
         }
     }
