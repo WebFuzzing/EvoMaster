4c0461be5e9169e8ea6430c9940e05cdd7361bbf
seran <7030273+seran@users.noreply.github.com>
2025-09-04 15:32:43 +0530

    comments

--- core/src/main/kotlin/org/evomaster/core/problem/security/data/ActionStubMapping.kt
+++ core/src/main/kotlin/org/evomaster/core/problem/security/data/ActionStubMapping.kt
@@ -1,5 +1,9 @@
 package org.evomaster.core.problem.security.data
 
+/**
+ * [ActionStubMapping] represents the information about [Action] name, WireMock [stub],
+ * [port], and the full [url] for the callback link.
+ */
 class ActionStubMapping (
     val id: Long,
     val actionName: String,
