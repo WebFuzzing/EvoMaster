4681043ce2989cd2623df7e2a9ffc0f7b2ebd4c3
Philip Garrett <garrett.phg@gmail.com>
2025-10-05 16:43:08 -0300

    Fixed null find of BodyParam in DTOWriter

--- core/src/main/kotlin/org/evomaster/core/output/dto/DtoWriter.kt
+++ core/src/main/kotlin/org/evomaster/core/output/dto/DtoWriter.kt
@@ -56,7 +56,7 @@ class DtoWriter {
     private fun calculateDtos(actionDefinitions: List<Action>) {
         actionDefinitions.forEach { action ->
             action.getViewOfChildren().find { it is BodyParam }
-            .let {
+            ?.let {
                 val primaryGene = (it as BodyParam).primaryGene()
                 val choiceGene = primaryGene.getWrappedGene(ChoiceGene::class.java)
                 if (choiceGene != null) {
