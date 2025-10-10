ed511c82f92eeb4463203c1f1b1b243898ac1871
Alexander Szyrej <alexander.szyrej@gmail.com>
2025-10-09 21:57:35 -0300

    Redis handler and Redis heuristic calculator, plus tests.

---
+++ client-java/controller/src/test/java/org/evomaster/client/java/controller/internal/db/RedisDistanceWithMetricsTest.java
@@ -0,0 +1,23 @@
+package org.evomaster.client.java.controller.internal.db;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RedisDistanceWithMetricsTest {
+
+    @Test
+    public void testNegativeRedisDistance() {
+        assertThrows(IllegalArgumentException.class, () ->
+            new RedisDistanceWithMetrics(-1.0, 0)
+        );
+    }
+
+    @Test
+    public void testNegativeNumberOfDocuments() {
+        assertThrows(IllegalArgumentException.class, () ->
+            new RedisDistanceWithMetrics(1.0, -1)
+        );
+    }
+
+}
