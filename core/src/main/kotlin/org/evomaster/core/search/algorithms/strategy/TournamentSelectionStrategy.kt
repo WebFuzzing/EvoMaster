08e98b047052a6b63b7191a9ab65554557f9e9c5
fcastagna_meli <franco.castagna@mercadolibre.com>
2025-10-15 10:27:15 -0300

    add tournament selection strategy javadocs

--- core/src/main/kotlin/org/evomaster/core/search/algorithms/strategy/TournamentSelectionStrategy.kt
+++ core/src/main/kotlin/org/evomaster/core/search/algorithms/strategy/TournamentSelectionStrategy.kt
@@ -4,6 +4,13 @@ import org.evomaster.core.search.Individual
 import org.evomaster.core.search.algorithms.wts.WtsEvalIndividual
 import org.evomaster.core.search.service.Randomness
 
+/**
+ * Tournament selection strategy for GA suites.
+ *
+ * Behavior:
+ * - Randomly samples [tournamentSize] distinct individuals from [population].
+ * - Computes their fitness with [score] and returns the one with the highest value.
+ */
 class TournamentSelectionStrategy : SelectionStrategy {
     override fun <T : Individual> select(
         population: List<WtsEvalIndividual<T>>,
