c8e29f3b179a119d7cc2eb2f345bed23c46791a9
arcuri82 <arcuri82@gmail.com>
2025-10-31 21:23:58 +0100

    adding more info to test assertions

--- core-tests/integration-tests/core-driver-it/src/test/kotlin/com/foo/base/BaseIT.kt
+++ core-tests/integration-tests/core-driver-it/src/test/kotlin/com/foo/base/BaseIT.kt
@@ -59,13 +59,17 @@ class BaseIT {
 
     @Test
     fun testSearchCommands(){
-        remote.startANewSearch()
-        remote.startSUT()
+        val startedNewSearch = remote.startANewSearch()
+        assertTrue(startedNewSearch, "Failed to start new search")
 
-        remote.registerNewAction(ActionDto().apply { index = 0 })
+        val startedSUT = remote.startSUT()
+        assertTrue(startedSUT, "Failed to start SUT")
+
+        val actionRegistered = remote.registerNewAction(ActionDto().apply { index = 0 })
+        assertTrue(actionRegistered, "Failed to register action")
 
         val results = remote.getTestResults()
-        assertNotNull(results)
+        assertNotNull(results, "Failed to get results")
         assertEquals(0, results!!.targets.size)
     }
 
@@ -73,30 +77,30 @@ class BaseIT {
     fun testRestart(){
 
         //make sure it is started
-        assertTrue(remote.startSUT())
+        assertTrue(remote.startSUT(), "Failed to restart SUT")
         var info = remote.getSutInfo()
-        assertNotNull(info)
+        assertNotNull(info, "Failed to get SUT info")
 
         //stop it
-        assertTrue(remote.stopSUT())
+        assertTrue(remote.stopSUT(), "Failed to stop SUT")
         info = remote.getSutInfo()
-        assertNull(info)
+        assertNull(info, "Failed to get SUT info after stop")
 
 
         //start it again
         driver.sutPort++ //let's try to avoid issue with TCP port taking too long to be released
-        assertTrue(remote.startSUT())
+        assertTrue(remote.startSUT(), "Failed to re-start SUT")
         info = remote.getSutInfo()
-        assertNotNull(info)
+        assertNotNull(info, "Failed to get SUT info after re-start")
 
         //stop it
-        assertTrue(remote.stopSUT())
+        assertTrue(remote.stopSUT(), "Failed to re-stop SUT")
         info = remote.getSutInfo()
-        assertNull(info)
+        assertNull(info, "Failed to get SUT info after re-stop")
 
         //start it again
         driver.sutPort++
-        assertTrue(remote.startSUT())
+        assertTrue(remote.startSUT(), "Failed to re-start SUT in 3rd time")
         info = remote.getSutInfo()
         assertNotNull(info)
     }
