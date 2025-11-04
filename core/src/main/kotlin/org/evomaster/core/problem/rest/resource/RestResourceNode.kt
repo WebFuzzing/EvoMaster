053aa3b12dd5c30d34a5178768ea343e97225d6f
arcuri82 <arcuri82@gmail.com>
2025-11-04 15:01:54 +0100

    added more use of forceNewTaints()

--- core/src/main/kotlin/org/evomaster/core/problem/rest/resource/RestResourceNode.kt
+++ core/src/main/kotlin/org/evomaster/core/problem/rest/resource/RestResourceNode.kt
@@ -408,6 +408,7 @@ open class RestResourceNode(
         } else {
             copy.doInitialize(randomness)
         }
+        copy.forceNewTaints()
 
         val template = templates[copy.verb.toString()]
                 ?: throw IllegalArgumentException("${copy.verb} is not one of templates of ${this.path}")
@@ -565,7 +566,7 @@ open class RestResourceNode(
     private fun templateSelected(callsTemplate: CallsTemplate){
         templates.getValue(callsTemplate.template).times += 1
     }
-    
+
     private fun selectTemplate(predicate: (CallsTemplate) -> Boolean, randomness: Randomness, chosen : Map<String, CallsTemplate>?=null, chooseLessVisit : Boolean = false) : CallsTemplate?{
         val ts = if(chosen == null) templates.filter { predicate(it.value) } else chosen.filter { predicate(it.value) }
         if(ts.isEmpty())
