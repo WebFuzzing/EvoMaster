c6a6c616ad488fce1cbab57032d14255445df82d
Mohsen Taheri Shalmani <taherimohsen2000@gmail.com>
2025-12-15 15:45:09 +0100

    Update EMconfig

--- core/src/main/kotlin/org/evomaster/core/EMConfig.kt
+++ core/src/main/kotlin/org/evomaster/core/EMConfig.kt
@@ -1448,7 +1448,7 @@ class EMConfig {
     @Cfg("Determines whether the AI response classifier skips model updates when the response " +
             "indicates a server-side error with status code 500. Such responses can be considered non-informative, " +
             "as they can be caused by both valid and invalid requests.")
-    var skipAIModelUpdateWhenResponseIs500 = true
+    var skipAIModelUpdateWhenResponseIs500 = false
 
     @Cfg("Output a JSON file representing statistics of the fuzzing session, written in the WFC Report format." +
             " This also includes a index.html web application to visualize such data.")
