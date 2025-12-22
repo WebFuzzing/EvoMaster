6c60a6445e9d803f4d7981ff760c11179b7c02bb
Omur <omur@erciyes.edu.tr>
2025-12-22 19:44:07 +0300

    simulate sleep

--- core-tests/e2e-tests/spring/spring-rest-bb/src/main/kotlin/com/foo/rest/examples/bb/sqli/BBQuerySQLiApplication.kt
+++ core-tests/e2e-tests/spring/spring-rest-bb/src/main/kotlin/com/foo/rest/examples/bb/sqli/BBQuerySQLiApplication.kt
@@ -2,28 +2,16 @@ package com.foo.rest.examples.bb.sqli
 
 import io.swagger.v3.oas.annotations.Operation
 import org.evomaster.e2etests.utils.CoveredTargets
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import java.sql.Connection
-import javax.annotation.PostConstruct
-import javax.sql.DataSource
 
 
 @SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 @RequestMapping(path = ["/api/sqli/query"])
 open class BBQuerySQLiApplication {
-
-    @Autowired
-    private lateinit var dataSource: DataSource
-
-    @Autowired
-    private lateinit var userRepository: UserRepository
-
-    private var connection: Connection? = null
 
     companion object {
         @JvmStatic
@@ -32,27 +20,13 @@ open class BBQuerySQLiApplication {
         }
     }
 
-    @PostConstruct
-    fun init() {
-        connection = dataSource.connection
-        initializeTestData()
-    }
-
-    private fun initializeTestData() {
-        if (userRepository.count() == 0L) {
-            userRepository.save(UserEntity(null, "admin", "admin123"))
-            userRepository.save(UserEntity(null, "user1", "password1"))
-        }
-    }
-
     /**
      * Safe endpoint - No SQL Injection vulnerability
      */
     @GetMapping("/safe")
     @Operation(summary = "Safe Query - No SQL Injection")
-    fun getSafeUsers(): ResponseEntity<List<UserDto>> {
-        val users = userRepository.findAll().map { UserDto(it.id, it.username) }
-        return ResponseEntity.ok(users)
+    fun getSafeUsers(): ResponseEntity<String?> {
+        return ResponseEntity.ok("OK")
     }
 
     /**
@@ -62,15 +36,12 @@ open class BBQuerySQLiApplication {
     @Operation(summary = "SQL Injection - Query Parameter")
     fun timeBasedQuery(@RequestParam username: String): ResponseEntity<String> {
         CoveredTargets.cover("sqli")
-
-        return try {
-            val stmt = connection?.createStatement()
-            val rs = stmt?.executeQuery("SELECT COUNT(*) as cnt FROM users WHERE username = '$username'")
-            val count = if (rs?.next() == true) rs.getInt("cnt") else 0
-            ResponseEntity.ok("COUNT: $count")
-        } catch (e: Exception) {
-            ResponseEntity.status(500).body("ERROR: ${e.message}")
+        if(username.contains("\' OR (select pg_sleep(5.50)) IS NULL-- -")) {
+            //simulating delay
+            Thread.sleep(5500)
         }
+
+        return ResponseEntity.ok("OK")
     }
 
 }
