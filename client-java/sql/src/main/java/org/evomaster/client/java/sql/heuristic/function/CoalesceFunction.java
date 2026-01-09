c69573a35c02415e035bd37f7b2d2397e047e69b
Juan Pablo Galeotti <jgaleotti@gmail.com>
2026-01-09 15:22:04 -0300

    Add some explanantion and tests for CoalesceFunction

--- client-java/sql/src/main/java/org/evomaster/client/java/sql/heuristic/function/CoalesceFunction.java
+++ client-java/sql/src/main/java/org/evomaster/client/java/sql/heuristic/function/CoalesceFunction.java
@@ -1,5 +1,8 @@
 package org.evomaster.client.java.sql.heuristic.function;
 
+/**
+ * Represents the SQL COALESCE() function, which returns the first non-null argument.
+ */
 public class CoalesceFunction extends SqlFunction {
 
     public CoalesceFunction() {
@@ -12,6 +15,18 @@ public class CoalesceFunction extends SqlFunction {
             throw new IllegalArgumentException("COALESCE() function requires at least one argument");
         }
 
+        final Object arg = returnFirstNonNullArgument(arguments);
+        return arg;
+    }
+
+    /**
+     * Returns the first non-null argument from <code>arguments</code>.
+     * If none of them is non-null, returns null.
+     *
+     * @param arguments
+     * @return
+     */
+    private static Object returnFirstNonNullArgument(Object[] arguments) {
         for (Object arg : arguments) {
             if (arg != null) {
                 return arg;
