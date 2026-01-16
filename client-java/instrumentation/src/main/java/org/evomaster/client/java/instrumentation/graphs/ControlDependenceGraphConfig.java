6f8ebebd557edcb35b9ffc4f55bde7a3d9e448ec
francastagna <fcastagna@dc.uba.ar>
2026-01-15 21:12:24 -0300

    replaced all System.out.println() calls with SimpleLogger calls in ControlDependenceGraphConfig

--- client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/graphs/ControlDependenceGraphConfig.java
+++ client-java/instrumentation/src/main/java/org/evomaster/client/java/instrumentation/graphs/ControlDependenceGraphConfig.java
@@ -1,5 +1,7 @@
 package org.evomaster.client.java.instrumentation.graphs;
 
+import org.evomaster.client.java.utils.SimpleLogger;
+
 /**
  * Runtime configuration for control-dependence graph instrumentation features.
  * Populated via the agent control channel.
@@ -9,30 +11,23 @@ public class ControlDependenceGraphConfig {
     private static volatile boolean enableGraphs = false;
     private static volatile boolean writeCfg = false;
 
-    
     public static boolean isGraphsEnabled() {
-        System.out.println("enableGraphs: " + enableGraphs);
+        SimpleLogger.debug("enableGraphs: " + enableGraphs);
         return enableGraphs;
     }
 
     public static void setEnableGraphs(boolean value) {
-        System.out.println("----------------------------------------------");
-        System.out.println("Setting enableGraphs to " + value);
-        System.out.println("----------------------------------------------");
+        SimpleLogger.info("Setting enableGraphs to " + value);
         enableGraphs = value;
     }
 
     public static boolean isWriteCfgEnabled() {
-        System.out.println("----------------------------------------------");
-        System.out.println("writeCfg: " + writeCfg);
-        System.out.println("----------------------------------------------");
+        SimpleLogger.debug("writeCfg: " + writeCfg);
         return writeCfg;
     }
 
     public static void setWriteCfgEnabled(boolean value) {
-        System.out.println("----------------------------------------------");
-        System.out.println("Setting writeCfg to " + value);
-        System.out.println("----------------------------------------------");
+        SimpleLogger.info("Setting writeCfg to " + value);
         writeCfg = value;
     }
 }
