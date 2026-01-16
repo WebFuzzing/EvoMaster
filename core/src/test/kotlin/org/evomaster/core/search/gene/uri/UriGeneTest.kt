8dfcdd55b511a7f85e984040cfa6df4a8b3abc4d
seran <7030273+seran@users.noreply.github.com>
2026-01-16 20:30:20 +0100

    test disabled

--- core/src/test/kotlin/org/evomaster/core/search/gene/uri/UriGeneTest.kt
+++ core/src/test/kotlin/org/evomaster/core/search/gene/uri/UriGeneTest.kt
@@ -1,18 +1,16 @@
 package org.evomaster.core.search.gene.uri
 
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class UriGeneTest {
 
+    @Disabled("Work in progress")
     @Test
-    fun testSetValueBasedOn() {
+    fun testSetValueBasedOnForUriDataGene() {
         val gene = UriGene("data:content/type;base64,")
 
-//        assertEquals("http://0.0.0.0:0/", gene.getValueAsRawString())
         assertTrue(gene.unsafeSetFromStringValue("data:text/plain;charset=UTF-8;base64,R0lGODdh"))
-
-        assertEquals("127.0.0.1", gene.getValueAsRawString())
     }
 }
