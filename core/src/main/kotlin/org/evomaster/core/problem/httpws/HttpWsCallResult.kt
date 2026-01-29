b4bf238b9f9329e16482c935b667433cb8a96246
Man Zhang <man.mzhang@gmail.com>
2026-01-29 16:30:14 +0800

    put back setFlakiness in HttpWsCallResult.kt

--- core/src/main/kotlin/org/evomaster/core/problem/httpws/HttpWsCallResult.kt
+++ core/src/main/kotlin/org/evomaster/core/problem/httpws/HttpWsCallResult.kt
@@ -154,5 +154,25 @@ abstract class HttpWsCallResult : EnterpriseActionResult {
     fun setFlakyBodyType(type: MediaType) = addResultValue(FLAKY_BODY_TYPE, type.toString())
     fun getFlakyBodyType() : MediaType? = getResultValue(FLAKY_BODY_TYPE)?.let { MediaType.valueOf(it) }
 
+    fun setFlakiness(previous: HttpWsCallResult){
+        val pStatusCode = previous.getStatusCode()
+        if (pStatusCode != null && pStatusCode != getStatusCode()) {
+            setFlakyStatusCode(pStatusCode)
+        }
 
+        val pBody = previous.getBody()
+        if (pBody != null && pBody != getBody()) {
+            setFlakyBody(pBody)
+        }
+
+        val pBodyType = previous.getBodyType()
+        if (pBodyType != null && pBodyType != getBodyType()) {
+            setFlakyBodyType(pBodyType)
+        }
+
+        val pMessage = previous.getErrorMessage()
+        if (pMessage != null && pMessage != getErrorMessage()) {
+            setFlakyErrorMessage(pMessage)
+        }
+    }
 }
