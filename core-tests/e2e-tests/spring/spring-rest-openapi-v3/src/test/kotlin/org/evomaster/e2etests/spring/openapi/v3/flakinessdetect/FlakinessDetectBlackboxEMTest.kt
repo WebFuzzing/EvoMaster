2e160a1ebc1cfe163b4ebf5b70c7e194a076dcba
Man Zhang <man.mzhang@gmail.com>
2026-02-05 18:02:18 +0800

    add bb e2e test

---
+++ core-tests/e2e-tests/spring/spring-rest-openapi-v3/src/test/kotlin/org/evomaster/e2etests/spring/openapi/v3/flakinessdetect/FlakinessDetectBlackboxEMTest.kt
@@ -0,0 +1,47 @@
+package org.evomaster.e2etests.spring.openapi.v3.flakinessdetect
+
+import com.foo.rest.examples.spring.openapi.v3.flakinessdetect.FlakinessDetectController
+import org.evomaster.e2etests.spring.openapi.v3.SpringTestBase
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import java.nio.file.Files
+import java.nio.file.Paths
+
+class FlakinessDetectBlackboxEMTest : SpringTestBase() {
+
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun init() {
+            initClass(FlakinessDetectController())
+        }
+    }
+
+
+    @Test
+    fun testRunEM() {
+        defaultSeed = 43
+
+        runTestHandlingFlakyAndCompilation(
+            "FlakinessDetectBlackboxEM",
+            "org.foo.FlakinessDetectBlackboxEM",
+            100
+        ) { args: MutableList<String> ->
+
+            val executedMainActionToFile = "target/em-tests/FlakinessDetectBlackboxEM/org/foo/FlakinessDetectBlackboxEM.kt"
+
+            setOption(args, "handleFlakiness", "true")
+            setOption(args, "blackBox", "true")
+            setOption(args, "bbTargetUrl", baseUrlOfSut)
+            setOption(args, "bbSwaggerUrl", "$baseUrlOfSut/v3/api-docs")
+
+
+            val solution = initAndRun(args)
+
+            val size = Files.readAllLines(Paths.get(executedMainActionToFile)).count { !it.contains("Flaky") && it.isNotBlank() }
+            assertTrue(size >= 3)
+        }
+    }
+}
