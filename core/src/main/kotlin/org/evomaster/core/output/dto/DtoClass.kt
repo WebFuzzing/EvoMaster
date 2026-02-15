2e2d74b8fd4c34735f566bd057b658266703039f
Philip Garrett <garrett.phg@gmail.com>
2026-02-15 10:03:35 -0300

    Remove commented code

--- core/src/main/kotlin/org/evomaster/core/output/dto/DtoClass.kt
+++ core/src/main/kotlin/org/evomaster/core/output/dto/DtoClass.kt
@@ -3,11 +3,8 @@ package org.evomaster.core.output.dto
 class DtoClass(
     val name: String,
     val fieldsMap: MutableMap<String, DtoField> = mutableMapOf(),
-//    var hasAdditionalProperties: Boolean = false,
     var additionalPropertiesDtoName: String? = null
 ) {
-
-//    private lateinit var additionalPropertiesDtoName: String
 
     fun addField(fieldName: String, field: DtoField) {
         if (!fieldsMap.containsKey(fieldName)) {
