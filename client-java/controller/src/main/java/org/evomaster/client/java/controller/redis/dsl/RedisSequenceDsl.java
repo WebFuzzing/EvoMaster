85f5bdf7237daf066c85678964451d05ec88ab13
Alexander Szyrej <alexander.szyrej@gmail.com>
2026-03-30 17:20:59 -0300

    removed dtos() from RedisSequenceDsl

--- client-java/controller/src/main/java/org/evomaster/client/java/controller/redis/dsl/RedisSequenceDsl.java
+++ client-java/controller/src/main/java/org/evomaster/client/java/controller/redis/dsl/RedisSequenceDsl.java
@@ -1,8 +1,5 @@
 package org.evomaster.client.java.controller.redis.dsl;
 
-import org.evomaster.client.java.controller.api.dto.database.operations.RedisInsertionDto;
-import java.util.List;
-
 public interface RedisSequenceDsl {
 
     /**
@@ -13,12 +10,4 @@ public interface RedisSequenceDsl {
      * @return a statement object on which the sequence can be continued or closed
      */
     RedisStatementDsl set(String key, String value);
-
-    /**
-     * Build the DTOs (Data Transfer Object) from this DSL,
-     * closing it (i.e., not usable any longer).
-     *
-     * @return a list of DTOs representing all the insertion Redis commands defined in this DSL.
-     */
-    List<RedisInsertionDto> dtos();
 }
