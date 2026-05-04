1785fc8c71707a1e1a19d5b2f5eaf120d8ebbca5
arcuri82 <arcuri82@gmail.com>
2026-05-04 22:08:38 +0200

    fixing app-image for all main OSs

--- makeExecutable.sh
+++ makeExecutable.sh
@@ -45,24 +45,13 @@ $JPACKAGE --main-jar $JAR --input $BUILD --dest $RELEASE --name evomaster \
   --java-options "--add-opens java.base/java.net=ALL-UNNAMED" \
   --java-options "--add-opens java.base/java.util=ALL-UNNAMED"
 # In theory, should not need --add-opens here, as in theory handled inside manifest of uber jar.
-# Unfortunately, it does not work when unpacking a DMG distribution without installing it.
+# Unfortunately, it does not work when unpacking an app-image distribution without installing it.
 # After pulling enough hair understanding WTF was going on, an easy workaround was just to force the
 # --add-opens here as well.
 # IMPORTANT: must be kept in sync with maven-shade-plugin in pom.xml and JdkIssue
 
-if [ "$TAG" == "WINDOWS" ]; then
-  echo TODO WINDOWS
-elif [ "$TAG" == "OSX" ]; then
-  # Need to convert, as no way in f*****g Mac you can install programmatically :(
-  hdiutil convert release/evomaster-*.dmg -format UDTO -o evomaster.cdr
-  hdiutil attach evomaster.cdr -nobrowse -noverify -noautoopen
-  rm -fr pypi-distribution/src/evomaster/dmg
-  cp -r /Volumes/evomaster pypi-distribution/src/evomaster/dmg
-  hdiutil detach /Volumes/evomaster
-  rm evomaster.cdr
-elif [ "$TAG" == "DEBIAN" ]; then
-  echo TODO DEBIAN
-else
-    echo Unrecognized tag "$TAG"
-    exit 1
+if [ "$TAG" == "APP" ]; then
+  rm -fr pypi-distribution/src/evomaster/release
+  cp -r release pypi-distribution/src/evomaster/release
 fi
+
--- pypi-distribution/src/evomaster/__main__.py
+++ pypi-distribution/src/evomaster/__main__.py
@@ -4,14 +4,25 @@ import subprocess
 
 
 def main():
-    print("Work in Progress - The release of WebFuzzing/EvoMaster on PyPi will hopefully happen soon in 2026")
-
     base = os.path.dirname(__file__)
 
-    exe = os.path.join(base, "dmg","evomaster.app", "Contents", "MacOS", "evomaster")
+    exe = None
 
-    if not os.path.exists(exe):
-        print("Error: could not find EvoMaster executable at:", exe, file=sys.stderr)
+    osx = os.path.join(base, "release","evomaster.app", "Contents", "MacOS", "evomaster")
+    if os.path.exists(osx):
+        exe = osx
+
+    windows = os.path.join(base, "release","evomaster", "evomaster.exe")
+    if os.path.exists(windows):
+        exe = windows
+
+    linux = os.path.join(base, "release","evomaster", "bin", "evomaster")
+    if os.path.exists(linux):
+        exe = linux
+
+    if exe is None:
+        print("Error: could not find EvoMaster executable. This mean the release of EvoMaster failed for your OS."
+              " This is a critical issue. Please report this bug on evomaster.org", file=sys.stderr)
         sys.exit(1)
 
     cmd = [exe] + sys.argv[1:]
