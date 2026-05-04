23fbc9575a2667530a7894520bd9c01fb29ddbb2
arcuri82 <arcuri82@gmail.com>
2026-05-04 11:05:20 +0200

    fixed script entry point

--- pypi-distribution/src/evomaster/__main__.py
+++ pypi-distribution/src/evomaster/__main__.py
@@ -2,7 +2,6 @@ import sys
 
 def main():
     print("Work in Progress - The release of WebFuzzing/EvoMaster on PyPi will hopefully happen soon in 2026")
-    print(f"Command executed: {' '.join(sys.argv[1:])}")
     sys.exit(0)
 
 if __name__ == "__main__":
