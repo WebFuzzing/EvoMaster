118d200cd1edaadb6ca58d2e81f7134478316bd9
arcuri82 <arcuri82@gmail.com>
2026-05-04 20:42:57 +0200

    giving up on GraalVM. trying to pack JRE directly from installers

--- makeExecutable.sh
+++ makeExecutable.sh
@@ -38,4 +38,28 @@ COPYRIGHT="Copyright 2016-$YEAR EvoMaster Team"
 VENDOR="EvoMaster Team"
 
 $JPACKAGE --main-jar $JAR --input $BUILD --dest $RELEASE --name evomaster \
-  --copyright "$COPYRIGHT" --license-file ./LICENSE --vendor "$VENDOR" --app-version $VERSION $OS
+  --copyright "$COPYRIGHT" --license-file ./LICENSE --vendor "$VENDOR" --app-version $VERSION $OS \
+  --java-options "--add-opens java.base/java.net=ALL-UNNAMED" \
+  --java-options "--add-opens java.base/java.util=ALL-UNNAMED"
+# In theory, should not need --add-opens here, as in theory handled inside manifest of uber jar.
+# Unfortunately, it does not work when unpacking a DMG distribution without installing it.
+# After pulling enough hair understanding WTF was going on, an easy workaround was just to force the
+# --add-opens here as well.
+# IMPORTANT: must be kept in sync with maven-shade-plugin in pom.xml and JdkIssue
+
+if [ "$TAG" == "WINDOWS" ]; then
+  echo TODO WINDOWS
+elif [ "$TAG" == "OSX" ]; then
+  # Need to convert, as no way in f*****g Mac you can install programmatically :(
+  hdiutil convert release/evomaster-*.dmg -format UDTO -o evomaster.cdr
+  hdiutil attach evomaster.cdr -nobrowse -noverify -noautoopen
+  rm -fr pypi-distribution/src/evomaster/dmg
+  cp -r /Volumes/evomaster pypi-distribution/src/evomaster/dmg
+  hdiutil detach /Volumes/evomaster
+  rm evomaster.cdr
+elif [ "$TAG" == "DEBIAN" ]; then
+  echo TODO DEBIAN
+else
+    echo Unrecognized tag "$TAG"
+    exit 1
+fi
--- pypi-distribution/src/evomaster/__main__.py
+++ pypi-distribution/src/evomaster/__main__.py
@@ -1,8 +1,24 @@
 import sys
+import os
+import subprocess
+
 
 def main():
     print("Work in Progress - The release of WebFuzzing/EvoMaster on PyPi will hopefully happen soon in 2026")
-    sys.exit(0)
+
+    base = os.path.dirname(__file__)
+
+    exe = os.path.join(base, "dmg","evomaster.app", "Contents", "MacOS", "evomaster")
+
+    if not os.path.exists(exe):
+        print("Error: could not find EvoMaster executable at:", exe, file=sys.stderr)
+        sys.exit(1)
+
+    cmd = [exe] + sys.argv[1:]
+
+    # Forward exit code
+    result = subprocess.run(cmd)
+    sys.exit(result.returncode)
 
 if __name__ == "__main__":
     main()
