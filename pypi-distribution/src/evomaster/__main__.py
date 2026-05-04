4f764b5a3101059e33c4f689b9ec5acdc7fbeb23
arcuri82 <arcuri82@gmail.com>
2026-05-04 10:09:12 +0200

    starting with Pypi distribution

---
+++ pypi-distribution/src/evomaster/__main__.py
@@ -0,0 +1,9 @@
+import sys
+
+def main():
+    print("Work in Progress - The release of WebFuzzing/EvoMaster on PyPi will hopefully happen soon in 2026")
+    print(f"Command executed: {' '.join(sys.argv[1:])}")
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
