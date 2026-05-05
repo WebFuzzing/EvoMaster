0417faa92a4675214b8fd48265e2617a72d38bde
arcuri82 <arcuri82@gmail.com>
2026-05-05 12:47:10 +0200

    ... i dig myself into...

--- pypi-distribution/src/evomaster/_dummy.c
+++ pypi-distribution/src/evomaster/_dummy.c
@@ -1,4 +1,18 @@
-// Dummy file to force binary wheel
-void _dummy_function(void) {
-    // Does absolutely nothing
+// Dummy file to force binary wheel... but still it has compile properly
+#include <Python.h>
+
+static PyMethodDef Methods[] = {
+    {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef module = {
+    PyModuleDef_HEAD_INIT,
+    "foo",
+    NULL,
+    -1,
+    Methods
+};
+
+PyMODINIT_FUNC PyInit_foo(void) {
+    return PyModule_Create(&module);
 }
