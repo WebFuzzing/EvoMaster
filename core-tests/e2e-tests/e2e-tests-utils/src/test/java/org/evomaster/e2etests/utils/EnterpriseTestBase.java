7606da5101aae68e16f6495ca70a66cfdad34bae
arcuri82 <arcuri82@gmail.com>
2026-05-12 13:52:41 +0200

    fixed default in E2E tests

--- core-tests/e2e-tests/e2e-tests-utils/src/test/java/org/evomaster/e2etests/utils/EnterpriseTestBase.java
+++ core-tests/e2e-tests/e2e-tests-utils/src/test/java/org/evomaster/e2etests/utils/EnterpriseTestBase.java
@@ -459,6 +459,7 @@ public abstract class EnterpriseTestBase {
         boolean active = code % 2 == 0;
 
         return new ArrayList<>(Arrays.asList(
+                "--blackBox", "false",
                 "--createTests", "" + createTests,
                 "--seed", "" + defaultSeed,
                 "--useTimeInFeedbackSampling" , "false",
