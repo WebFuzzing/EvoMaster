cd7c2c081ed3ff04478b5c06ce9c317a875cda23
lmasroca <lmasroca@gmail.com>
2026-05-16 18:25:37 -0300

    Excluded SqlPrimaryKeyGene from appearing inside ArrayGenes in samples, as they will be invalid most of the time.

--- core/src/test/kotlin/org/evomaster/core/search/gene/GeneSamplerForTests.kt
+++ core/src/test/kotlin/org/evomaster/core/search/gene/GeneSamplerForTests.kt
@@ -321,6 +321,7 @@ object GeneSamplerForTests {
                 .filter { !it.isAbstract }
                 .filter { it.java != CycleObjectGene::class.java && it.java !== LimitObjectGene::class.java }
                 .filter { it.java != ArrayGene::class.java && it.java != SqlMultidimensionalArrayGene::class.java }
+                .filter{ it.java != SqlPrimaryKeyGene::class.java }
         // TODO might filter out some more genes here
     }
 
