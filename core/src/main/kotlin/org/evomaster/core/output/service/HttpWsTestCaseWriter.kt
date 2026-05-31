f53a33d82ba01d8f5d3659b2bed75874c72af52c
Philip Garrett <garrett.phg@gmail.com>
2026-05-31 13:04:45 -0300

    Relax SSL Validations for generated tests in python and javascript

--- core/src/main/kotlin/org/evomaster/core/output/service/HttpWsTestCaseWriter.kt
+++ core/src/main/kotlin/org/evomaster/core/output/service/HttpWsTestCaseWriter.kt
@@ -869,6 +869,7 @@ abstract class HttpWsTestCaseWriter : ApiTestCaseWriter() {
                 lines.append(", cookies=${CookieWriter.cookiesName(elc)}")
             }
             appendBodyArgument(call)
+//        lines.append(", verify=False")
         }
     }
 
