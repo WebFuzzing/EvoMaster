ccd0ec77504f9cfd3c38a31f3a32e608dd39383a
guido-rodriguez_sfemu <guido.rodriguez@salesforce.com>
2026-06-04 21:59:38 -0300

    Setting up MCP Problem Type Skeleton

---
+++ core/src/main/kotlin/org/evomaster/core/problem/mcp/McpAction.kt
@@ -0,0 +1,18 @@
+package org.evomaster.core.problem.mcp
+
+import org.evomaster.core.problem.api.param.Param
+import org.evomaster.core.search.action.MainAction
+import org.evomaster.core.search.gene.Gene
+
+abstract class McpAction(
+    val id: String,
+    parameters: MutableList<Param>
+) : MainAction(false, parameters) {
+
+    val parameters: List<Param>
+        get() = children as List<Param>
+
+    override fun getName(): String = id
+
+    override fun seeTopGenes(): List<Gene> = parameters.flatMap { it.seeGenes() }
+}
