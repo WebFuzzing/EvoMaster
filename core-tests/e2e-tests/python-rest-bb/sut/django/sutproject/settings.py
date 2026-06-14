b577468a90681ab0ad069a322a0113b26cb7a131
Omur Sahin <omur@erciyes.edu.tr>
2026-06-14 23:37:17 +0300

    renderer class

--- core-tests/e2e-tests/python-rest-bb/sut/django/sutproject/settings.py
+++ core-tests/e2e-tests/python-rest-bb/sut/django/sutproject/settings.py
@@ -32,6 +32,7 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
 }
 
 SPECTACULAR_SETTINGS = {
