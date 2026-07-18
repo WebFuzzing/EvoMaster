8c46846b90026de67537eee67a618fe67e05f74e
hocoscy <cy_shen@outlook.com>
2026-07-18 20:49:57 +0800

    fix REST path surrogate handling and monitor initialization

--- core/src/main/kotlin/org/evomaster/core/problem/rest/data/RestPath.kt
+++ core/src/main/kotlin/org/evomaster/core/problem/rest/data/RestPath.kt
@@ -354,7 +354,15 @@ class RestPath(path: String) {
         val data = dynamicResolutionOnlyPathData(params, mapOf())
         assert(data.size == 1)
         assert(!data[0].second)
-        return data[0].first
+        return data[0].first.map { c ->
+        // The URI calls in dynamicResolutionOnlyPathData do not encode non-ASCII characters in path segments.
+            if (c.code > 127) {
+                // non-ASCII
+                encode(c.toString())
+            } else {
+                c.toString()
+            }
+        }.joinToString( "")
     }
 
 
@@ -413,7 +421,7 @@ class RestPath(path: String) {
                             why not using URI also for Query part???
                             it seems unclear how to properly build it as a single string...
                          */
-                        val entry = URI(null, null, path.toString(), null, null).toASCIIString()
+                        val entry = URI(null, null, path.toString(), null, null).rawPath
                         data.add(Pair(entry, false))
                         path.setLength(0) // clear it
                         data.add(Pair(variable, true))
@@ -446,7 +454,7 @@ class RestPath(path: String) {
         }
 
         if(path.isNotEmpty()){
-            val entry = URI(null, null, path.toString(), null, null).toASCIIString()
+            val entry = URI(null, null, path.toString(), null, null).rawPath
             data.add(Pair(entry, false))
         }
 
