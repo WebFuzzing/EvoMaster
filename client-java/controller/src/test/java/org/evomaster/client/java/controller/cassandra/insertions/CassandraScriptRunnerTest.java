45e9da07170d3205bca55482c2e3c9625dc9c55e
Gonzalo Tomas Guerrero <gtguerrero1@gmail.com>
2026-08-12 00:25:51 -0300

    Add constants in test class

--- client-java/controller/src/test/java/org/evomaster/client/java/controller/cassandra/insertions/CassandraScriptRunnerTest.java
+++ client-java/controller/src/test/java/org/evomaster/client/java/controller/cassandra/insertions/CassandraScriptRunnerTest.java
@@ -21,7 +21,10 @@ public class CassandraScriptRunnerTest {
 
     private static CqlSession connection;
     private static final int CASSANDRA_PORT = 9042;
-    private static final GenericContainer<?> cassandra = new GenericContainer<>("cassandra:4.1")
+    private static final String CASSANDRA_IMAGE = "cassandra";
+    private static final String CASSANDRA_VERSION = "4.1";
+
+    private static final GenericContainer<?> cassandra = new GenericContainer<>(CASSANDRA_IMAGE + ":" + CASSANDRA_VERSION)
             .withExposedPorts(CASSANDRA_PORT)
             .waitingFor(Wait.forLogMessage(".*Starting listening for CQL clients.*", 1)
                     .withStartupTimeout(Duration.ofMinutes(2)));
