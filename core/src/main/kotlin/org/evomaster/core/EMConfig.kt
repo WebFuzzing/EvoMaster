c5bff91667902f70dc7feab85b1ebdcd10eefaeb
Mohsen Taheri Shalmani <taherimohsen2000@gmail.com>
2026-08-18 19:14:15 +0200

    Reduce MaxRepair

--- core/src/main/kotlin/org/evomaster/core/EMConfig.kt
+++ core/src/main/kotlin/org/evomaster/core/EMConfig.kt
@@ -1654,7 +1654,7 @@ class EMConfig {
     @Cfg("Models used to learn input constraints and predict the response status before issuing a request. " +
             "Supports both single-model and ensemble configurations. " +
             "Ensemble model is a combination of a comma-separated list, e.g., GLM,NN,KDE.")
-    var aiModelForResponseClassification: Set<AIResponseClassifierModel> = setOf(AIResponseClassifierModel.NONE)
+    var aiModelForResponseClassification: Set<AIResponseClassifierModel> = setOf(AIResponseClassifierModel.GAUSSIAN)
 
     @Experimental
     @Cfg("Learning rate controlling the step size during parameter updates in classifiers. " +
@@ -1707,7 +1707,7 @@ class EMConfig {
     @Min(1.0)
     @Cfg("When the Response Classifier determines an action is going to fail, specify how many attempts will" +
             " be tried at fixing it.")
-    var maxRepairAttemptsInResponseClassification = 100
+    var maxRepairAttemptsInResponseClassification = 2
 
     enum class AIClassificationRepairActivation{
         /*
