497fcd9119f8f37280fd6fb31ea1239230e1194b
Gonzalo Tomas Guerrero <gtguerrero1@gmail.com>
2026-08-23 20:49:18 -0300

    Remove previousInsertionDtos from dsl

--- client-java/controller/src/main/java/org/evomaster/client/java/controller/cassandra/dsl/CassandraDsl.java
+++ client-java/controller/src/main/java/org/evomaster/client/java/controller/cassandra/dsl/CassandraDsl.java
@@ -4,8 +4,8 @@ import org.evomaster.client.java.controller.cassandra.insertions.model.Cassandra
 import org.evomaster.client.java.controller.cassandra.insertions.model.CassandraInsertionEntryDto;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * DSL (Domain Specific Language) for insertion operations on
@@ -15,31 +15,13 @@ public class CassandraDsl implements CassandraSequenceDsl, CassandraStatementDsl
 
     private List<CassandraInsertionDto> list = new ArrayList<>();
 
-    private final List<CassandraInsertionDto> previousInsertionDtos = new ArrayList<>();
-
-    private CassandraDsl() {
-    }
-
-    private CassandraDsl(List<CassandraInsertionDto>... previous) {
-        if (previous != null && previous.length > 0) {
-            Arrays.stream(previous).forEach(previousInsertionDtos::addAll);
-        }
-    }
+    private CassandraDsl() {}
 
     /**
      * @return a DSL object to create Cassandra operations
      */
     public static CassandraSequenceDsl cassandra() {
         return new CassandraDsl();
-    }
-
-    /**
-     * @param previous a DSL object which is executed in the front of this
-     * @return a DSL object to create Cassandra operations
-     */
-    @SafeVarargs
-    public static CassandraSequenceDsl cassandra(List<CassandraInsertionDto>... previous) {
-        return new CassandraDsl(previous);
     }
 
     @Override
@@ -66,6 +48,7 @@ public class CassandraDsl implements CassandraSequenceDsl, CassandraStatementDsl
 
     @Override
     public CassandraStatementDsl d(String columnName, String printableValue) {
+        Objects.requireNonNull(printableValue);
 
         checkDsl();
 
--- client-java/controller/src/test/java/org/evomaster/client/java/controller/cassandra/dsl/CassandraDslTest.java
+++ client-java/controller/src/test/java/org/evomaster/client/java/controller/cassandra/dsl/CassandraDslTest.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CassandraDslTest {
 
