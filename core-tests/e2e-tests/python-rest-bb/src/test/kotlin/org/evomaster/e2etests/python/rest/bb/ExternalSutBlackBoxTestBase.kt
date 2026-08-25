5f344be66b04453f194cbae67b6414500af042d6
May Kristin Ugelstad <may.kristin.ugelstad@aize.io>
2026-08-25 12:37:20 +0200

    Use Playwright runner for JS_PLAYWRIGHT in Python BB, enforce npm ci and add Playwright deps/script

--- core-tests/e2e-tests/python-rest-bb/src/test/kotlin/org/evomaster/e2etests/python/rest/bb/ExternalSutBlackBoxTestBase.kt
+++ core-tests/e2e-tests/python-rest-bb/src/test/kotlin/org/evomaster/e2etests/python/rest/bb/ExternalSutBlackBoxTestBase.kt
@@ -109,7 +109,8 @@ abstract class ExternalSutBlackBoxTestBase : RestTestBase() {
 
     private fun runGeneratedTests(outputFormat: OutputFormat, outputFolderName: String) {
         when {
-            outputFormat.isJavaScript() -> BlackBoxUtils.runNpmTests(BlackBoxUtils.relativePath(outputFolderName))
+            // For Playwright output, invoke Playwright runner instead of Jest
+            outputFormat.isJavaScript() -> BlackBoxUtils.runNpmTests(BlackBoxUtils.relativePath(outputFolderName), outputFormat.isPlaywright())
             outputFormat.isPython() -> BlackBoxUtils.runPythonTests(BlackBoxUtils.relativePath(outputFolderName))
             outputFormat.isJava() -> BlackBoxUtils.runJavaTests(outputFolderName)
             outputFormat.isKotlin() -> BlackBoxUtils.runKotlinTests(outputFolderName)
