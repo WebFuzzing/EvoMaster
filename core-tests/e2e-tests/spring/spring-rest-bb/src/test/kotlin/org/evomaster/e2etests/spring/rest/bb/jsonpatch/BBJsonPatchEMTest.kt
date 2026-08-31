00141902c9867ff264c27f453e74cdc4bbcf2e26
ROMINA JULIETA SUÁREZ <romi_de_munro@hotmail.com>
2026-08-30 23:06:09 -0300

    Fix for convention in tests

--- core-tests/e2e-tests/spring/spring-rest-bb/src/test/kotlin/org/evomaster/e2etests/spring/rest/bb/jsonpatch/BBJsonPatchEMTest.kt
+++ core-tests/e2e-tests/spring/spring-rest-bb/src/test/kotlin/org/evomaster/e2etests/spring/rest/bb/jsonpatch/BBJsonPatchEMTest.kt
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EnumSource
 
-class BBJsonPatchTest : SpringTestBase() {
+class BBJsonPatchEMTest : SpringTestBase() {
 
     companion object {
         @BeforeAll
--- core-tests/e2e-tests/spring/spring-rest-bb/src/test/kotlin/org/evomaster/e2etests/spring/rest/bb/xml/BBXMLEMTest.kt
+++ core-tests/e2e-tests/spring/spring-rest-bb/src/test/kotlin/org/evomaster/e2etests/spring/rest/bb/xml/BBXMLEMTest.kt
@@ -14,7 +14,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EnumSource
 
-class BBXMLTest : SpringTestBase() {
+class BBXMLEMTest : SpringTestBase() {
 
     companion object {
 
